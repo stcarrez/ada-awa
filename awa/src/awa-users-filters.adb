9828708568084b10dc94347958604eebef287fe3
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-06-08 21:53:27 +0000

    Define a servlet filter that verifies the access key - check the access key and obtain the user + session - create the user principal - redirect the user if the access key identification failed

--- awa/src/awa-users-filters.adb
+++ awa/src/awa-users-filters.adb
@@ -28,6 +28,7 @@ with ASF.Sessions;
 with ASF.Cookies;
 with ASF.Principals;
 with AWA.Users.Principals;
+with AWA.Users.Models;
 package body AWA.Users.Filters is
 
    use Util.Log;
@@ -35,6 +36,16 @@ package body AWA.Users.Filters is
    --  The logger
    Log : constant Loggers.Logger := Loggers.Create ("AWA.Users.Filters");
 
+
+   procedure Set_Session_Principal (Request : in out ASF.Requests.Request'Class;
+                                    User    : in AWA.Users.Models.User_Ref;
+                                    Sess    : in AWA.Users.Models.Session_Ref) is
+      Principal : constant Principals.Principal_Access := Principals.Create (User, Sess);
+      Session   : ASF.Sessions.Session := Request.Get_Session (Create => True);
+      Name      : constant String := "User: " & User.Get_Name;
+   begin
+      Session.Set_Principal (Principal.all'Access);
+   end Set_Session_Principal;
 
    procedure Authenticate (F        : in Auth_Filter;
                            Request  : in out ASF.Requests.Request'Class;
@@ -45,5 +56,46 @@ package body AWA.Users.Filters is
    begin
       null;
    end Authenticate;
+
+   --  ------------------------------
+   --  Filter a request which contains an access key and verify that the
+   --  key is valid and identifies a user.  Once the user is known, create
+   --  a session and setup the user principal.
+   --
+   --  If the access key is missing or invalid, redirect to the
+   --  <b>Invalid_Key_URI</b> associated with the filter.
+   --  ------------------------------
+   procedure Do_Filter (F        : in Verify_Filter;
+                        Request  : in out ASF.Requests.Request'Class;
+                        Response : in out ASF.Responses.Response'Class;
+                        Chain    : in out ASF.Servlets.Filter_Chain) is
+      Key     : constant String := Request.Get_Parameter ("key");
+      Manager : AWA.Users.Services.User_Service_Access := AWA.Users.Module.Get_User_Manager;
+      User    : AWA.Users.Models.User_Ref;
+      Session : AWA.Users.Models.Session_Ref;
+   begin
+      Log.Info ("Verify access key {0}", Key);
+
+      Manager.Verify_User (Key     => Key,
+                           User    => User,
+                           IpAddr  => "",
+                           Session => Session);
+
+      Set_Session_Principal (Request, User, Session);
+
+      --  Request is authorized, proceed to the next filter.
+      ASF.Servlets.Do_Filter (Chain    => Chain,
+                              Request  => Request,
+                              Response => Response);
+
+   exception
+      when AWA.Users.Services.Not_Found =>
+         declare
+            URI : constant String := Ada.Strings.Unbounded.To_String (F.Invalid_Key_URI);
+         begin
+            Log.Info ("Invalid access key {0}, redirecting to {1}", Key, URI);
+            Response.Send_Redirect (Location => URI);
+         end;
+   end Do_Filter;
 
 end AWA.Users.Filters;
--- awa/src/awa-users-filters.ads
+++ awa/src/awa-users-filters.ads
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------
---  awa-users-beans -- ASF Beans for user module
+--  awa-users-filters -- Specific filters for authentication and key verification
 --  Copyright (C) 2011 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
@@ -16,10 +16,14 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
+with Ada.Strings.Unbounded;
+
 with ASF.Requests;
 with ASF.Responses;
 with ASF.Sessions;
 with ASF.Principals;
+with ASF.Filters;
+with ASF.Servlets;
 with Security.Filters;
 package AWA.Users.Filters is
 
@@ -32,5 +36,24 @@ package AWA.Users.Filters is
                            Session  : in ASF.Sessions.Session;
                            Auth_Id  : in String;
                            Principal : out ASF.Principals.Principal_Access);
+
+   type Verify_Filter is new ASF.Filters.Filter with private;
+
+   --  Filter a request which contains an access key and verify that the
+   --  key is valid and identifies a user.  Once the user is known, create
+   --  a session and setup the user principal.
+   --
+   --  If the access key is missing or invalid, redirect to the
+   --  <b>Invalid_Key_URI</b> associated with the filter.
+   procedure Do_Filter (F        : in Verify_Filter;
+                        Request  : in out ASF.Requests.Request'Class;
+                        Response : in out ASF.Responses.Response'Class;
+                        Chain    : in out ASF.Servlets.Filter_Chain);
+
+private
+
+   type Verify_Filter is new ASF.Filters.Filter with record
+      Invalid_Key_URI : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
 
 end AWA.Users.Filters;
