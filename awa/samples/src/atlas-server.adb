97a90a43d6d7514a9a20f8f54afd6fc3954a0893
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-07-14 06:59:08 +0000

    Add a log to indicate the main URL to connect to

--- awa/samples/src/atlas-server.adb
+++ awa/samples/src/atlas-server.adb
@@ -18,15 +18,22 @@
 with Ada.Text_IO;
 with Ada.Exceptions;
 with ASF.Server.Web;
+
+with Util.Log.Loggers;
+
 with Atlas.Applications;
 procedure Atlas.Server is
    CONTEXT_PATH : constant String := "/atlas";
    App     : constant Atlas.Applications.Application_Access := new Atlas.Applications.Application;
 
+   Log     : Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Atlas.Server");
    WS      : ASF.Server.Web.AWS_Container;
 begin
    Atlas.Applications.Initialize (App);
    WS.Register_Application (CONTEXT_PATH, App.all'Access);
+
+   Log.Info ("Connect you browser to: http://localhost:8080/atlas/index.html");
+
    WS.Start;
    delay 365.0 * 24.0 * 3600.0;
    App.Close;
