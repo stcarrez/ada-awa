27eaaff61d7031011c57e89d2634b3da6f1fe33f
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-08-09 21:36:42 +0000

    New input component - Provide a UIInput component that overrides the <h:inputText> to generate   a ui input with a title and the error message if necessary.

---
+++ awa/src/awa-components-inputs.adb
@@ -0,0 +1,98 @@
+-----------------------------------------------------------------------
+--  awa-components-inputs -- AWA input field component
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+with ASF.Components.Html.Messages;
+with ASF.Applications.Messages.Vectors;
+
+with Util.Beans.Objects;
+package body AWA.Components.Inputs is
+
+   use Ada.Strings.Unbounded;
+
+   --  ------------------------------
+   --  Render the input field title.
+   --  ------------------------------
+   procedure Render_Title (UI      : in UIInput;
+                           Writer  : in ResponseWriter_Access;
+                           Context : in out Faces_Context'Class) is
+      Title : constant Util.Beans.Objects.Object := UI.Get_Attribute (Context, "title");
+   begin
+      Writer.Start_Element ("dt");
+      Writer.Write (Title);
+      Writer.End_Element ("dt");
+   end Render_Title;
+
+   --  ------------------------------
+   --  Render the input component.  Starts the DL/DD list and write the input
+   --  component with the possible associated error message.
+   --  ------------------------------
+   overriding
+   procedure Encode_Begin (UI      : in UIInput;
+                           Context : in out Faces_Context'Class) is
+   begin
+      if not UI.Is_Rendered (Context) then
+         return;
+      end if;
+      declare
+         Id       : constant String := To_String (UI.Get_Client_Id);
+         Writer   : constant ResponseWriter_Access := Context.Get_Response_Writer;
+         Messages : constant ASF.Applications.Messages.Vectors.Cursor := Context.Get_Messages (Id);
+         Style    : constant String := UI.Get_Attribute ("styleClass", Context);
+      begin
+         Writer.Start_Element ("dl");
+         if ASF.Applications.Messages.Vectors.Has_Element (Messages) then
+            Writer.Write_Attribute ("class", Style & " awa-error");
+         elsif Style'Length > 0 then
+            Writer.Write_Attribute ("class", Style);
+         end if;
+         UI.Render_Title (Writer, Context);
+         Writer.Start_Element ("dd");
+         UI.Render_Input (Context);
+      end;
+   end Encode_Begin;
+
+   --  ------------------------------
+   --  Render the end of the input component.  Closes the DL/DD list.
+   --  ------------------------------
+   overriding
+   procedure Encode_End (UI      : in UIInput;
+                         Context : in out Faces_Context'Class) is
+      use ASF.Components.Html.Messages;
+   begin
+      if not UI.Is_Rendered (Context) then
+         return;
+      end if;
+
+      --  Render the error message associated with the input field.
+      declare
+         Id       : constant String := To_String (UI.Get_Client_Id);
+         Writer   : constant ResponseWriter_Access := Context.Get_Response_Writer;
+         Messages : constant ASF.Applications.Messages.Vectors.Cursor := Context.Get_Messages (Id);
+      begin
+         if ASF.Applications.Messages.Vectors.Has_Element (Messages) then
+            Write_Message (UI, ASF.Applications.Messages.Vectors.Element (Messages),
+                           SPAN_NO_STYLE, False, True, Context);
+         end if;
+         Writer.End_Element ("dd");
+         Writer.End_Element ("dl");
+      end;
+   end Encode_End;
+
+end AWA.Components.Inputs;
---
+++ awa/src/awa-components-inputs.ads
@@ -0,0 +1,61 @@
+-----------------------------------------------------------------------
+--  awa-components-inputs -- AWA input field component
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Components.Html.Forms;
+with ASF.Contexts.Faces;
+with ASF.Contexts.Writer;
+
+package AWA.Components.Inputs is
+
+   use ASF.Contexts.Faces;
+   use ASF.Contexts.Writer;
+
+   --  ------------------------------
+   --  Input component
+   --  ------------------------------
+   --  The AWA input component overrides the ASF input component to build a compact component
+   --  that displays a label, the input field and the associated error message if necessary.
+   --
+   --  The generated HTML looks like:
+   --
+   --  <dl class='... awa-error'>
+   --    <dt>title <i>required</i></dt>
+   --    <dd><input type='text' ...> <em/>
+   --        <span class='...'>message</span>
+   --    </dd>
+   --  </dl>
+   type UIInput is new ASF.Components.Html.Forms.UIInput with null record;
+   type UIInput_Access is access all UIInput'Class;
+
+   --  Render the input field title.
+   procedure Render_Title (UI      : in UIInput;
+                           Writer  : in ResponseWriter_Access;
+                           Context : in out Faces_Context'Class);
+
+   --  Render the input component.  Starts the DL/DD list and write the input
+   --  component with the possible associated error message.
+   overriding
+   procedure Encode_Begin (UI      : in UIInput;
+                           Context : in out Faces_Context'Class);
+
+   --  Render the end of the input component.  Closes the DL/DD list.
+   overriding
+   procedure Encode_End (UI      : in UIInput;
+                         Context : in out Faces_Context'Class);
+
+end AWA.Components.Inputs;
