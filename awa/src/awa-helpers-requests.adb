51db5fc0838dc38efa6868f55f7b5d9bba9808e3
Stephane Carrez <Stephane.Carrez@gmail.com>
2011-12-19 07:14:47 +0000

    New helper package to get information from the current request - New function Get_Parameter to retrieve an identifier from a request parameter

---
+++ awa/src/awa-helpers-requests.adb
@@ -0,0 +1,40 @@
+-----------------------------------------------------------------------
+--  awa-helpers-requests -- Helpers for AWA applications
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Contexts.Faces;
+package body AWA.Helpers.Requests is
+
+   --  ------------------------------
+   --  Get the parameter identified by the given name and return it as an identifier.
+   --  Returns NO_IDENTIFIER if the parameter does not exist or is not valid.
+   --  ------------------------------
+   function Get_Parameter (Name : in String) return ADO.Identifier is
+      Ctx  : constant ASF.Contexts.Faces.Faces_Context_Access := ASF.Contexts.Faces.Current;
+      P    : constant String := Ctx.Get_Parameter (Name);
+   begin
+      if P = "" then
+         return ADO.NO_IDENTIFIER;
+      else
+         return ADO.Identifier'Value (P);
+      end if;
+   exception
+      when others =>
+         return ADO.NO_IDENTIFIER;
+   end Get_Parameter;
+
+end AWA.Helpers.Requests;
---
+++ awa/src/awa-helpers-requests.ads
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------
+--  awa-helpers-requests -- Helpers for AWA applications
+--  Copyright (C) 2011 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ADO;
+package AWA.Helpers.Requests is
+
+   --  Get the parameter identified by the given name and return it as an identifier.
+   --  Returns NO_IDENTIFIER if the parameter does not exist or is not valid.
+   function Get_Parameter (Name : in String) return ADO.Identifier;
+
+end AWA.Helpers.Requests;
