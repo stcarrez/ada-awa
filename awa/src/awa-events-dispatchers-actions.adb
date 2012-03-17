74a7373cc5d99c31b90fba55b3f6a41289ada107
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-03-17 22:52:31 +0000

    - Fix resolver used when dispatching an event to an event action - If an event action raises an exception, catch it and report an error in the logs

--- awa/src/awa-events-dispatchers-actions.adb
+++ awa/src/awa-events-dispatchers-actions.adb
@@ -17,32 +17,36 @@
 -----------------------------------------------------------------------
 
 with Ada.Strings.Unbounded;
+with Ada.Exceptions;
 
 with Util.Beans.Objects;
 with Util.Beans.Basic;
+with Util.Log.Loggers;
 
 with EL.Contexts;
 with EL.Contexts.Default;
 with EL.Variables;
 with EL.Variables.Default;
 
-with AWA.Applications;
-
 with ASF.Beans;
 with ASF.Requests;
 with ASF.Sessions;
 
+with AWA.Events.Action_Method;
 package body AWA.Events.Dispatchers.Actions is
 
    use Ada.Strings.Unbounded;
 
+   use Util.Log;
+
+   Log : constant Loggers.Logger := Loggers.Create ("AWA.Events.Dispatchers.Actions");
+
    --  ------------------------------
    --  Default Resolver
    --  ------------------------------
-   type Event_ELResolver is new EL.Contexts.ELResolver with record
+   type Event_ELResolver is limited new EL.Contexts.ELResolver with record
       Request     : ASF.Requests.Request_Access;
       Application : AWA.Applications.Application_Access;
-      --        Beans       : Bean_Vector_Access;
    end record;
 
    overriding
@@ -66,6 +70,7 @@ package body AWA.Events.Dispatchers.Actions is
       use Util.Beans.Objects;
       use Util.Beans.Basic;
       use EL.Variables;
+      use type ASF.Requests.Request_Access;
 
       Result : Object;
       Bean   : Util.Beans.Basic.Readonly_Bean_Access;
@@ -76,29 +81,29 @@ package body AWA.Events.Dispatchers.Actions is
          return Base.Get_Value (Key);
       end if;
 
-      Result := Resolver.Request.Get_Attribute (Key);
-      if not Util.Beans.Objects.Is_Null (Result) then
-         return Result;
+      if Resolver.Request /= null then
+         Result := Resolver.Request.Get_Attribute (Key);
+         if not Util.Beans.Objects.Is_Null (Result) then
+            return Result;
+         end if;
+
+         --  If there is a session, look if the attribute is defined there.
+         declare
+            Session : constant ASF.Sessions.Session := Resolver.Request.Get_Session;
+         begin
+            if Session.Is_Valid then
+               Result := Session.Get_Attribute (Key);
+               if not Util.Beans.Objects.Is_Null (Result) then
+                  return Result;
+               end if;
+            end if;
+         end;
       end if;
 
-      --  If there is a session, look if the attribute is defined there.
-      declare
-         Session : constant ASF.Sessions.Session := Resolver.Request.Get_Session;
-      begin
-         if Session.Is_Valid then
-            Result := Session.Get_Attribute (Key);
-            if not Util.Beans.Objects.Is_Null (Result) then
-               return Result;
-            end if;
-         end if;
-      end;
       Resolver.Application.Create (Name, Context, Bean, Scope);
       if Bean = null then
          return Resolver.Application.Get_Global (Name, Context);
-         --           raise No_Variable
-         --             with "Bean not found: '" & To_String (Name) & "'";
       end if;
-      --        Resolver.Beans.Append (Bean_Object '(Key'Length, Bean, Key));
       Result := To_Object (Bean);
       Resolver.Request.Set_Attribute (Key, Result);
       return Result;
@@ -133,11 +138,14 @@ package body AWA.Events.Dispatchers.Actions is
 
       type Event_Bean is new Util.Beans.Basic.Readonly_Bean with null record;
 
+      --  ------------------------------
       --  Get the value identified by the name.
       --  If the name cannot be found, the method should return the Null object.
+      --  ------------------------------
       overriding
       function Get_Value (From : in Event_Bean;
                           Name : in String) return Util.Beans.Objects.Object is
+         pragma Unreferenced (From);
       begin
          return Event.Get_Value (Name);
       end Get_Value;
@@ -147,8 +155,12 @@ package body AWA.Events.Dispatchers.Actions is
       ELContext      : aliased EL.Contexts.Default.Default_Context;
       Variables      : aliased EL.Variables.Default.Default_Variable_Mapper;
 
+      --  ------------------------------
       --  Dispatch the event to the event action identified by <b>Action</b>.
+      --  ------------------------------
       procedure Dispatch_One (Action : in Event_Action) is
+         use Ada.Exceptions;
+
          Method : EL.Expressions.Method_Info;
       begin
          Method := Action.Action.Get_Method_Info (Context => ELContext);
@@ -162,21 +174,30 @@ package body AWA.Events.Dispatchers.Actions is
          end if;
 
          --  Execute the specified method on the bean and give it the event object.
-         --           AWA.Events.Action_Method.Execute (Method => Method,
-         --                                             Param  => Event);
+         AWA.Events.Action_Method.Execute (Method => Method,
+                                           Param  => Event);
+
+         --  If an exception is raised by the action, do not propagate it:
+         --   o We have to dispatch the event to other actions.
+         --   o The event may be dispatched asynchronously and there is no handler
+         --     that could handle such exception
+      exception
+         when E : others =>
+            Log.Error ("Error when executing event action {0}: {1}: {2}",
+                       Action.Action.Get_Expression, Exception_Name (E), Exception_Message (E));
       end Dispatch_One;
 
       Pos  : Event_Action_Lists.Cursor := Manager.Actions.First;
    begin
-      --           Root_Resolver.Application := App'Unchecked_Access;
-      --           Root_Resolver.Request := Request'Unchecked_Access;
-      --           Root_Resolver.Beans := Beans'Unchecked_Access;
+      Resolver.Application := Manager.Application;
+
       ELContext.Set_Resolver (Resolver'Unchecked_Access);
       ELContext.Set_Variable_Mapper (Variables'Unchecked_Access);
 
       Variables.Bind (Name  => "event",
                       Value => To_Object (Local_Event'Unchecked_Access, STATIC));
-      while not Event_Action_Lists.Has_Element (Pos) loop
+
+      while Event_Action_Lists.Has_Element (Pos) loop
          Event_Action_Lists.Query_Element (Pos, Dispatch_One'Access);
          Event_Action_Lists.Next (Pos);
       end loop;
@@ -198,5 +219,17 @@ package body AWA.Events.Dispatchers.Actions is
       Item.Properties := Params;
       Manager.Actions.Append (Item);
    end Add_Action;
+
+   --  ------------------------------
+   --  Create a new dispatcher associated with the application.
+   --  ------------------------------
+   function Create_Dispatcher (Application : in AWA.Applications.Application_Access)
+                               return Dispatcher_Access is
+      Result : constant Dispatcher_Access := new Action_Dispatcher '(Dispatcher with
+                                                                     Application => Application,
+                                                                     others => <>);
+   begin
+      return Result.all'Access;
+   end Create_Dispatcher;
 
 end AWA.Events.Dispatchers.Actions;
--- awa/src/awa-events-dispatchers-actions.ads
+++ awa/src/awa-events-dispatchers-actions.ads
@@ -22,6 +22,7 @@ with EL.Expressions;
 with EL.Beans;
 
 with AWA.Events.Queues;
+with AWA.Applications;
 
 --  The <b>AWA.Events.Dispatchers.Actions</b> package implements an event dispatcher
 --  which calls a set of Ada bean action methods that have been registered on the dispatcher
@@ -32,7 +33,8 @@ package AWA.Events.Dispatchers.Actions is
    --  Event action dispatcher
    --  ------------------------------
 
-   type Action_Dispatcher is new Dispatcher with private;
+   type Action_Dispatcher (Application : AWA.Applications.Application_Access) is
+     new Dispatcher  with private;
 
    --  Dispatch the event identified by <b>Event</b>.
    --  The event actions which are associated with the event are executed synchronously.
@@ -46,6 +48,10 @@ package AWA.Events.Dispatchers.Actions is
    procedure Add_Action (Manager : in out Action_Dispatcher;
                          Action  : in EL.Expressions.Method_Expression;
                          Params  : in EL.Beans.Param_Vectors.Vector);
+
+   --  Create a new dispatcher associated with the application.
+   function Create_Dispatcher (Application : in AWA.Applications.Application_Access)
+                               return Dispatcher_Access;
 
 private
 
@@ -62,9 +68,10 @@ private
      new Ada.Containers.Doubly_Linked_Lists (Element_Type => Event_Action);
 
    --  The dispatcher maintains a list of event actions to which the events are dispatched.
-   type Action_Dispatcher is new Dispatcher with record
-      Actions : Event_Action_Lists.List;
-      Queue   : AWA.Events.Queues.Queue_Ref;
+   type Action_Dispatcher (Application : AWA.Applications.Application_Access) is
+     new Dispatcher with record
+      Actions     : Event_Action_Lists.List;
+      Queue       : AWA.Events.Queues.Queue_Ref;
    end record;
 
 end AWA.Events.Dispatchers.Actions;
