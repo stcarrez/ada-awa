0ba04271f486244676abae1a2fc0576d51ebd25d
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-03-31 22:20:51 +0000

    Mail client implementation which saves generated emails in a file

---
+++ awa/src/awa-mail-clients-files.adb
@@ -0,0 +1,140 @@
+-----------------------------------------------------------------------
+--  awa-mail-clients-files -- Mail client dump/file implementation
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+
+with Util.Files;
+with Util.Strings;
+package body AWA.Mail.Clients.Files is
+
+   use Ada.Strings.Unbounded;
+
+   --  ------------------------------
+   --  Set the <tt>From</tt> part of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_From (Message : in out File_Mail_Message;
+                       Name    : in String;
+                       Address : in String) is
+   begin
+      Message.From := To_Unbounded_String ("From: " & Name & "<" & Address & ">");
+   end Set_From;
+
+   --  ------------------------------
+   --  Add a recipient for the message.
+   --  ------------------------------
+   overriding
+   procedure Add_Recipient (Message : in out File_Mail_Message;
+                            Kind    : in Recipient_Type;
+                            Name    : in String;
+                            Address : in String) is
+      Email : constant String := Name & "<" & Address & ">";
+   begin
+      case Kind is
+         when TO =>
+            if Length (Message.To) = 0 then
+               Append (Message.To, "To: ");
+            else
+               Append (Message.To, ", ");
+            end if;
+            Append (Message.To, Email);
+
+         when CC =>
+            if Length (Message.Cc) = 0 then
+               Append (Message.Cc, "Cc: ");
+            else
+               Append (Message.Cc, ", ");
+            end if;
+            Append (Message.Cc, Email);
+
+         when BCC =>
+            if Length (Message.Bcc) = 0 then
+               Append (Message.Bcc, "Bcc: ");
+            else
+               Append (Message.Bcc, ", ");
+            end if;
+            Append (Message.Bcc, Email);
+
+      end case;
+   end Add_Recipient;
+
+   --  ------------------------------
+   --  Set the subject of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_Subject (Message : in out File_Mail_Message;
+                          Subject : in String) is
+   begin
+      Message.Subject := To_Unbounded_String (Subject);
+   end Set_Subject;
+
+   --  ------------------------------
+   --  Set the body of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_Body (Message : in out File_Mail_Message;
+                       Content : in String) is
+   begin
+      Message.Message := To_Unbounded_String (Content);
+   end Set_Body;
+
+   --  ------------------------------
+   --  Send the email message.
+   --  ------------------------------
+   overriding
+   procedure Send (Message : in out File_Mail_Message) is
+      N : Natural;
+      Output : Ada.Text_IO.File_Type;
+   begin
+      Util.Concurrent.Counters.Increment (Message.Manager.Index, N);
+      declare
+         Path : constant String := Util.Files.Compose (To_String (Message.Manager.Path),
+                                                       "msg-" & Util.Strings.Image (N));
+      begin
+         Ada.Text_IO.Create (File => Output,
+                             Mode => Ada.Text_IO.Out_File,
+                             Name => Path);
+
+         Ada.Text_IO.Put_Line (To_String (Message.From));
+         if Length (Message.To) > 0 then
+            Ada.Text_IO.Put_Line (To_String (Message.To));
+         end if;
+         if Length (Message.Cc) > 0 then
+            Ada.Text_IO.Put_Line (To_String (Message.Cc));
+         end if;
+         if Length (Message.Bcc) > 0 then
+            Ada.Text_IO.Put_Line (To_String (Message.Bcc));
+         end if;
+         Ada.Text_IO.Put ("Subject: ");
+         Ada.Text_IO.Put_Line (To_String (Message.Subject));
+         Ada.Text_IO.Put_Line (To_String (Message.Message));
+         Ada.Text_IO.Close (Output);
+      end;
+   end Send;
+
+   --  ------------------------------
+   --  Create a new mail message.
+   --  ------------------------------
+   overriding
+   function Create_Message (Manager : in File_Mail_Manager) return Mail_Message_Access is
+      Result : constant File_Mail_Message_Access := new File_Mail_Message;
+   begin
+      Result.Manager := Manager.Self;
+      return Result.all'Access;
+   end Create_Message;
+
+end AWA.Mail.Clients.Files;
---
+++ awa/src/awa-mail-clients-files.ads
@@ -0,0 +1,92 @@
+-----------------------------------------------------------------------
+--  awa-mail-clients-files -- Mail client dump/file implementation
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+with Util.Concurrent.Counters;
+
+--  The <b>AWA.Mail.Clients.Files</b> package provides a dump implementation of the
+--  mail client interfaces on top of raw system files.
+package AWA.Mail.Clients.Files is
+
+   --  ------------------------------
+   --  Mail Message
+   --  ------------------------------
+   --  The <b>File_Mail_Message</b> represents a mail message that is written on a file in
+   --  in specific directory.
+   type File_Mail_Message is new Mail_Message with private;
+   type File_Mail_Message_Access is access all File_Mail_Message'Class;
+
+   --  Set the <tt>From</tt> part of the message.
+   overriding
+   procedure Set_From (Message : in out File_Mail_Message;
+                       Name    : in String;
+                       Address : in String);
+
+   --  Add a recipient for the message.
+   overriding
+   procedure Add_Recipient (Message : in out File_Mail_Message;
+                            Kind    : in Recipient_Type;
+                            Name    : in String;
+                            Address : in String);
+
+   --  Set the subject of the message.
+   overriding
+   procedure Set_Subject (Message : in out File_Mail_Message;
+                          Subject : in String);
+
+   --  Set the body of the message.
+   overriding
+   procedure Set_Body (Message : in out File_Mail_Message;
+                       Content : in String);
+
+   --  Send the email message.
+   overriding
+   procedure Send (Message : in out File_Mail_Message);
+
+   --  ------------------------------
+   --  Mail Manager
+   --  ------------------------------
+   --  The <b>Mail_Manager</b> is the entry point to create a new mail message
+   --  and be able to send it.
+   type File_Mail_Manager is new Mail_Manager with private;
+   type File_Mail_Manager_Access is access all File_Mail_Manager'Class;
+
+   --  Create a new mail message.
+   overriding
+   function Create_Message (Manager : in File_Mail_Manager) return Mail_Message_Access;
+
+private
+
+   type File_Mail_Message is new Mail_Message with record
+      Manager : File_Mail_Manager_Access;
+      From    : Ada.Strings.Unbounded.Unbounded_String;
+      To      : Ada.Strings.Unbounded.Unbounded_String;
+      Cc      : Ada.Strings.Unbounded.Unbounded_String;
+      Bcc     : Ada.Strings.Unbounded.Unbounded_String;
+      Subject : Ada.Strings.Unbounded.Unbounded_String;
+      Message : Ada.Strings.Unbounded.Unbounded_String;
+   end record;
+
+   type File_Mail_Manager is new Mail_Manager with record
+      Self    : File_Mail_Manager_Access;
+      Path    : Ada.Strings.Unbounded.Unbounded_String;
+      Index   : Util.Concurrent.Counters.Counter;
+   end record;
+
+end AWA.Mail.Clients.Files;
