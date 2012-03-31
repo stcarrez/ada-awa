66b841250889674cb08826b818dab2859e3ac446
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-03-31 20:39:01 +0000

    Define a set of interface to send an email and have several possible implementations

---
+++ awa/src/awa-mail-clients.ads
@@ -0,0 +1,68 @@
+-----------------------------------------------------------------------
+--  awa-mail-client -- Mail client interface
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+--  The <b>AWA.Mail.Clients</b> package defines a mail client API used by the mail module.
+--  It defines two interfaces that must be implemented.  This allows to have an implementation
+--  based on an external application such as <tt>sendmail</tt> and other implementation that
+--  use an SMTP connection.
+package AWA.Mail.Clients is
+
+   type Recipient_Type is (TO, CC, BCC);
+
+   --  ------------------------------
+   --  Mail Message
+   --  ------------------------------
+   --  The <b>Mail_Message</b> represents an abstract mail message that can be initialized
+   --  before being sent.
+   type Mail_Message is limited interface;
+   type Mail_Message_Access is access all Mail_Message'Class;
+
+   --  Set the <tt>From</tt> part of the message.
+   procedure Set_From (Message : in out Mail_Message;
+                       Name    : in String;
+                       Address : in String) is abstract;
+
+   --  Add a recipient for the message.
+   procedure Add_Recipient (Message : in out Mail_Message;
+                            Kind    : in Recipient_Type;
+                            Name    : in String;
+                            Address : in String) is abstract;
+
+   --  Set the subject of the message.
+   procedure Set_Subject (Message : in out Mail_Message;
+                          Subject : in String) is abstract;
+
+   --  Set the body of the message.
+   procedure Set_Body (Message : in out Mail_Message;
+                       Content : in String) is abstract;
+
+   --  Send the email message.
+   procedure Send (Message : in out Mail_Message) is abstract;
+
+   --  ------------------------------
+   --  Mail Manager
+   --  ------------------------------
+   --  The <b>Mail_Manager</b> is the entry point to create a new mail message
+   --  and be able to send it.
+   type Mail_Manager is limited interface;
+   type Mail_Manager_Access is access all Mail_Manager'Class;
+
+   --  Create a new mail message.
+   function Create_Message (Manager : in Mail_Manager) return Mail_Message_Access is abstract;
+
+end AWA.Mail.Clients;
