c33bcf46669ff5c7bf4a8c0e46b88ba2d656647f
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-04-01 09:25:58 +0000

    Implementation of SMTP mail client on top of AWS

---
+++ awa/src/aws/awa-mail-clients-aws_smtp.adb
@@ -0,0 +1,128 @@
+-----------------------------------------------------------------------
+--  awa-mail-clients-aws_smtp -- Mail client implementation on top of AWS SMTP client
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Unchecked_Deallocation;
+
+with AWS.SMTP.Client;
+
+--  The <b>AWA.Mail.Clients.AWS_SMTP</b> package provides an implementation of the
+--  mail client interfaces on top of AWS SMTP client API.
+package body AWA.Mail.Clients.AWS_SMTP is
+
+   use Ada.Strings.Unbounded;
+
+   procedure Free is
+      new Ada.Unchecked_Deallocation (Object => AWS.SMTP.Recipients,
+                                      Name   => Recipients_Access);
+
+   --  ------------------------------
+   --  Set the <tt>From</tt> part of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_From (Message : in out AWS_Mail_Message;
+                       Name    : in String;
+                       Address : in String) is
+   begin
+      Message.From := AWS.SMTP.E_Mail (Name    => Name,
+                                       Address => Address);
+   end Set_From;
+
+   --  ------------------------------
+   --  Add a recipient for the message.
+   --  ------------------------------
+   overriding
+   procedure Add_Recipient (Message : in out AWS_Mail_Message;
+                            Kind    : in Recipient_Type;
+                            Name    : in String;
+                            Address : in String) is
+   begin
+      if Message.To = null then
+         Message.To := new AWS.SMTP.Recipients (1 .. 1);
+      else
+         declare
+            To : constant Recipients_Access := new AWS.SMTP.Recipients (1 .. Message.To'Last + 1);
+         begin
+            To (Message.To'Range) := Message.To.all;
+            Free (Message.To);
+            Message.To := To;
+         end;
+      end if;
+      Message.To (Message.To'Last) := AWS.SMTP.E_Mail (Name    => Name,
+                                                       Address => Address);
+   end Add_Recipient;
+
+   --  ------------------------------
+   --  Set the subject of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_Subject (Message : in out AWS_Mail_Message;
+                          Subject : in String) is
+   begin
+      Message.Subject := To_Unbounded_String (Subject);
+   end Set_Subject;
+
+   --  ------------------------------
+   --  Set the body of the message.
+   --  ------------------------------
+   overriding
+   procedure Set_Body (Message : in out AWS_Mail_Message;
+                       Content : in String) is
+   begin
+      Message.Content := To_Unbounded_String (Content);
+   end Set_Body;
+
+   overriding
+   procedure Send (Message : in out AWS_Mail_Message) is
+      Result  : AWS.SMTP.Status;
+   begin
+      if Message.To = null then
+         return;
+      end if;
+
+      AWS.SMTP.Client.Send (Server  => Message.Manager.Server,
+                            From    => Message.From,
+                            To      => Message.To.all,
+                            Subject => To_String (Message.Subject),
+                            Message => To_String (Message.Content),
+                            Status  => Result);
+   end Send;
+
+   --  Create a file based mail manager and configure it according to the properties.
+   function Create_Manager (Props : in Util.Properties.Manager) return Mail_Manager_Access is
+      Server : constant String := Props.Get (Name => "smtp.host", Default => "localhost");
+      Port   : constant String := Props.Get (Name => "smtp.port", Default => "25");
+      Result : constant AWS_Mail_Manager_Access := new AWS_Mail_Manager;
+   begin
+      Result.Self   := Result;
+      Result.Server := AWS.SMTP.Client.Initialize (Server_Name => Server,
+                                                   Port => Positive'Value (Port));
+      return Result.all'Access;
+   end Create_Manager;
+
+   --  ------------------------------
+   --  Create a new mail message.
+   --  ------------------------------
+   overriding
+   function Create_Message (Manager : in AWS_Mail_Manager) return Mail_Message_Access is
+      Result : constant AWS_Mail_Message_Access := new AWS_Mail_Message;
+   begin
+      Result.Manager := Manager.Self;
+      return Result.all'Access;
+   end Create_Message;
+
+end AWA.Mail.Clients.AWS_SMTP;
---
+++ awa/src/aws/awa-mail-clients-aws_smtp.ads
@@ -0,0 +1,98 @@
+-----------------------------------------------------------------------
+--  awa-mail-clients-aws_smtp -- Mail client implementation on top of AWS SMTP client
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Strings.Unbounded;
+
+with Util.Properties;
+
+with AWS.SMTP;
+
+--  The <b>AWA.Mail.Clients.AWS_SMTP</b> package provides an implementation of the
+--  mail client interfaces on top of AWS SMTP client API.
+package AWA.Mail.Clients.AWS_SMTP is
+
+   NAME : constant String := "smtp";
+
+   --  ------------------------------
+   --  Mail Message
+   --  ------------------------------
+   --  The <b>Mail_Message</b> represents an abstract mail message that can be initialized
+   --  before being sent.
+   type AWS_Mail_Message is new Mail_Message with private;
+   type AWS_Mail_Message_Access is access all AWS_Mail_Message'Class;
+
+   --  Set the <tt>From</tt> part of the message.
+   overriding
+   procedure Set_From (Message : in out AWS_Mail_Message;
+                       Name    : in String;
+                       Address : in String);
+
+   --  Add a recipient for the message.
+   overriding
+   procedure Add_Recipient (Message : in out AWS_Mail_Message;
+                            Kind    : in Recipient_Type;
+                            Name    : in String;
+                            Address : in String);
+
+   --  Set the subject of the message.
+   overriding
+   procedure Set_Subject (Message : in out AWS_Mail_Message;
+                          Subject : in String);
+
+   --  Set the body of the message.
+   overriding
+   procedure Set_Body (Message : in out AWS_Mail_Message;
+                       Content : in String);
+
+   --  Send the email message.
+   overriding
+   procedure Send (Message : in out AWS_Mail_Message);
+
+   --  ------------------------------
+   --  Mail Manager
+   --  ------------------------------
+   --  The <b>Mail_Manager</b> is the entry point to create a new mail message
+   --  and be able to send it.
+   type AWS_Mail_Manager is new Mail_Manager with private;
+   type AWS_Mail_Manager_Access is access all AWS_Mail_Manager'Class;
+
+   --  Create a file based mail manager and configure it according to the properties.
+   function Create_Manager (Props : in Util.Properties.Manager) return Mail_Manager_Access;
+
+   --  Create a new mail message.
+   overriding
+   function Create_Message (Manager : in AWS_Mail_Manager) return Mail_Message_Access;
+
+private
+
+   type Recipients_Access is access all AWS.SMTP.Recipients;
+
+   type AWS_Mail_Message is new Mail_Message with record
+      Manager : AWS_Mail_Manager_Access;
+      From    : AWS.SMTP.E_Mail_Data;
+      Subject : Ada.Strings.Unbounded.Unbounded_String;
+      Content : Ada.Strings.Unbounded.Unbounded_String;
+      To      : Recipients_Access := null;
+   end record;
+
+   type AWS_Mail_Manager is new Mail_Manager with record
+      Self    : AWS_Mail_Manager_Access;
+      Server  : AWS.SMTP.Receiver;
+   end record;
+
+end AWA.Mail.Clients.AWS_SMTP;
