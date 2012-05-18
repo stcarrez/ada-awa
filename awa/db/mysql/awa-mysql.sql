64e2d286ca87cde223d673a8cb3e5d6b036f2df0
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-05-18 13:05:41 +0000

    Rebuild the SQL files (the AWA default model no longer integrates the specific Blob, Storage, Images data models)

--- awa/db/mysql/awa-mysql.sql
+++ awa/db/mysql/awa-mysql.sql
@@ -25,54 +25,6 @@ CREATE TABLE acl (
   `writeable` TINYINT ,
   PRIMARY KEY (`id`)
 );
-/* An image that was uploaded by a user in an image folder. */
-CREATE TABLE awa_image (
-  /* the image identifier. */
-  `id` BIGINT NOT NULL,
-  /* the image version. */
-  `version` int ,
-  /* the image width. */
-  `width` INTEGER NOT NULL,
-  /* the image height. */
-  `height` INTEGER NOT NULL,
-  /* the task within the server which is processing this message */
-  `task_id` INTEGER NOT NULL,
-  /* the image name. */
-  `name` VARCHAR(255) NOT NULL,
-  /* the image type. */
-  `mime_type` VARCHAR(255) NOT NULL,
-  /* the image path. */
-  `path` VARCHAR(255) NOT NULL,
-  /* the image creation date. */
-  `create_date` DATETIME NOT NULL,
-  /* the original image if this image was created by the application. */
-  `original_id` INTEGER NOT NULL,
-  /* the thumbnail image to display the image is an image selector. */
-  `thumbnail_id` INTEGER NOT NULL,
-  /* the user who uploaded the image. */
-  `user_id` INTEGER NOT NULL,
-  /* the image folder where this image is stored. */
-  `folder_id` INTEGER NOT NULL,
-  /* the image data if the storage type is DATABASE. */
-  `image_id` INTEGER ,
-  PRIMARY KEY (`id`)
-);
-/* The image folder contains a set of images that have been uploaded by the user. */
-CREATE TABLE awa_image_folder (
-  /* the image folder identifier */
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  /* the image folder version. */
-  `version` int ,
-  /* the image folder name */
-  `name` VARCHAR(256) NOT NULL,
-  /* the image folder creation date */
-  `create_date` DATETIME NOT NULL,
-  /* the user who owns this image folder */
-  `user_id` INTEGER NOT NULL,
-  /* the workspace that this image folder belongs. */
-  `workspace_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
-);
 /* A message in the message queue */
 CREATE TABLE awa_message (
   /* the message identifier */
@@ -123,112 +75,6 @@ CREATE TABLE awa_queue (
   `name` VARCHAR(256) NOT NULL,
   /* the server identifier which is associated with this message queue */
   `server_id` INTEGER ,
-  PRIMARY KEY (`id`)
-);
-/* The database storage data when the storage type is DATABASE. */
-CREATE TABLE awa_storage (
-  /* the storage identifier */
-  `id` INTEGER NOT NULL,
-  /* the storage data version. */
-  `version` int ,
-  /* the local store creation date */
-  `create_date` DATETIME NOT NULL,
-  /* the storage type. */
-  `storage_type` INTEGER NOT NULL,
-  /* the storage specific data */
-  `data` VARCHAR(256) NOT NULL,
-  /* the storage that this local store refers to. */
-  `storage_id` INTEGER NOT NULL,
-  /* the workspace that this storage belongs to. */
-  `workspace_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* The database storage data when the storage type is DATABASE. */
-CREATE TABLE awa_storage_data (
-  /* the storage data identifier */
-  `id` INTEGER NOT NULL,
-  /* the storage data version. */
-  `version` int ,
-  /* the storage data when the storage type is DATABASE. */
-  `data` BLOB NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* The local store record is created when a copy of the data
-is needed on the local file system.  The creation date refers to the date when
-the data was copied on the local file system.  The expiration date indicates a
-date after which the local file can be removed from the local file system. */
-CREATE TABLE awa_store_local (
-  /* the local storage identifier */
-  `id` INTEGER NOT NULL,
-  /* the local storage version. */
-  `version` int ,
-  /* the local store creation date */
-  `create_date` DATETIME NOT NULL,
-  /* the local store expiration date */
-  `expire_date` DATETIME NOT NULL,
-  /* the local store path */
-  `name` VARCHAR(256) NOT NULL,
-  /* the storage that this local store refers to. */
-  `storage_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* Blog  */
-CREATE TABLE blog (
-  /* the blog identifier */
-  `id` INTEGER NOT NULL,
-  /* the blob version. */
-  `version` int ,
-  /* the blog name */
-  `name` VARCHAR(256) NOT NULL,
-  /* the blog uuid */
-  `uid` VARCHAR(256) NOT NULL,
-  /* the blog creation date */
-  `create_date` DATETIME NOT NULL,
-  /* the workspace that this blob belongs to. */
-  `workspace_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* Post in a blog */
-CREATE TABLE blog_post (
-  /* the post identifier */
-  `id` BIGINT NOT NULL,
-  /* the post version. */
-  `version` int ,
-  /* the post title */
-  `title` VARCHAR(256) NOT NULL,
-  /* the uri */
-  `uri` VARCHAR(256) ,
-  /* the blog text content */
-  `text` VARCHAR(60000) ,
-  /* the post creation date */
-  `create_date` DATETIME NOT NULL,
-  /* the post publication date */
-  `publish_date` DATETIME ,
-  /* the post status */
-  `status` INTEGER NOT NULL,
-  /* the post author */
-  `author_id` INTEGER NOT NULL,
-  /* the blog that this post belongs */
-  `blog_id` INTEGER NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* The Comment table records a user comment associated with a database entity.
-                 The comment can be associated with any other database record. */
-CREATE TABLE comments (
-  /*  */
-  `id` INTEGER ,
-  /* the comment version. */
-  `version` int ,
-  /* the comment publication date. */
-  `date` TIMESTAMP NOT NULL,
-  /* the comment message. */
-  `message` VARCHAR(65000) NOT NULL,
-  /* the entity identifier to which this comment is associated. */
-  `entity_id` INTEGER NOT NULL,
-  /* the user who posted this comment */
-  `user_fk` INTEGER NOT NULL,
-  /* the entity type that correspond to the entity associated with this comment. */
-  `entity__type_fk` INTEGER NOT NULL,
   PRIMARY KEY (`id`)
 );
 /* Email address */
@@ -327,17 +173,9 @@ CREATE TABLE workspace_member (
 INSERT INTO entity_type (name) VALUES
 ("access_key")
 ,("acl")
-,("awa_image")
-,("awa_image_folder")
 ,("awa_message")
 ,("awa_message_type")
 ,("awa_queue")
-,("awa_storage")
-,("awa_storage_data")
-,("awa_store_local")
-,("blog")
-,("blog_post")
-,("comments")
 ,("email")
 ,("session")
 ,("user")
