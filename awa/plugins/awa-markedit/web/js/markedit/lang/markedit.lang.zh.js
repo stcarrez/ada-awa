62c9c8175e280d0df52158eda28bc8f5ad7f61c6
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-05-19 14:22:43 +0000

    New plugin AWA Markedit - integrate the jQuery Markedit Javascript library   MIT-LICENSE attached - add support to customize the generated markup - add new buttons underline and strike

---
+++ awa/plugins/awa-markedit/web/js/markedit/lang/markedit.lang.zh.js
@@ -0,0 +1,72 @@
+//
+//  MarkEdit langauge: Chinese
+//  MarkEdit语言：中文
+//
+//  Note:  These translations were created with Google translator.
+//  I'm sure they sound really awkwards.  If a native speaker would
+//  be interested in polishing it up, it would be a great benefit.
+//
+
+MarkEditLanguage = function() {
+
+    return {
+
+        'defaultButtons': {
+            'bold': {
+                'tip': '大胆'
+            },
+            'italic': {
+                'tip': '斜体'
+            },
+            'link': {
+                'tip': '插入超链接'
+            },
+            'image': {
+                'tip': '插入图像'
+            },
+            'code': {
+                'tip': '代码示例'
+            },
+            'quote': {
+                'tip': '大段引用'
+            },
+            'numberlist': {
+                'tip': '编号列表'
+            },
+            'bulletlist': {
+                'tip': '项目符号列表'
+            },
+            'line': {
+                'tip': '水平线'
+            },
+            'heading': {
+                'tip': '标题'
+            },
+            'edit': {
+                'text': '撰写',
+                'tip': '在编辑视图模式'
+            },
+            'preview': {
+                'text': '预览',
+                'tip': '在预览视图模式'
+            }
+        },
+
+        'dialog': {
+            'insertLink': {
+                'title': '插入超链接',
+                'helpText': '输入网址插入。',
+                'insertButton': '插入',
+                'cancelButton': '取消'
+            },
+            'insertImage': {
+                'title':'插入图像',
+                'helpText': '输入图片的网址插入。',
+                'insertButton': '插入',
+                'cancelButton': '取消'
+            }
+        }
+
+    };
+
+}();
