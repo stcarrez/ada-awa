0aeacae7a6f007c891f0b148c56bb008ddbe374e
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-06-15 20:36:52 +0000

    Document the new AWA jobs plugin

--- awa/plugins/awa-jobs/src/awa-jobs.ads
+++ awa/plugins/awa-jobs/src/awa-jobs.ads
@@ -16,16 +16,50 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
-with Util.Strings;
-with Util.Events;
-with Util.Beans.Objects;
-with Util.Beans.Basic;
-
-with ASF.Applications;
-
 --  == Introduction ==
 --  The <b>AWA.Jobs</b> plugin defines a batch job framework for modules to perform and execute
---  long running and deferred actions.
+--  long running and deferred actions.  The `Jobs` plugin is intended to help web application
+--  designers in implementing end to end asynchronous operation.  A client schedules a job
+--  and does not block nor wait for the immediate completion.  Instead, the client asks
+--  periodically or uses other mechanisms to check for the job completion.
+--
+--  === Writing a job ===
+--  A new job type is created by implementing the `Execute` operation of the abstract
+--  `Job_Type` tagged record.
+--
+--    type Resize_Job is new AWA.Jobs.Job_Type with ...;
+--
+--  The `Execute` procedure must be implemented.  It should use the `Get_Parameter` functions
+--  to retrieve the job parameters and perform the work.  While the job is being executed,
+--  it can save result by using the `Set_Result` operations, save messages by using the
+--  `Set_Message` operations and report the progress by using `Set_Progress`.
+--  It may report the job status by using `Set_Status`.
+--
+--    procedure Execute (Job : in out Resize_Job) is
+--    begin
+--        Job.Set_Result ("done", "ok");
+--    end Execute;
+--
+--  === Registering a job ===
+--  The <b>AWA.Jobs</b> plugin must be able to create the job instance when it is going to
+--  be executed.  For this, a registration package must be instantiated:
+--
+--    package Resize_Def is new AWA.Jobs.Definition (Resize_Job);
+--
+--  === Scheduling a job ===
+--  To schedule a job, declare an instance of the job to execute and set the job specific
+--  parameters.  The job parameters will be saved in the database.  As soon as parameters
+--  are defined, call the `Schedule` procedure to schedule the job in the job queue and
+--  obtain a job identifier.
+--
+--    Resize : Resize_Job;
+--    ...
+--    Resize.Set_Parameter ("file", "image.png");
+--    Resize.Set_Parameter ("width", "32");
+--    Resize.Set_Parameter ("height, "32");
+--    Resize.Schedule;
+--
+--  === Checking for job completion ===
 --
 --
 --  == Data Model ==
