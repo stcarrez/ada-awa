f1ca0ce13ef5923ae62e6f6f183d9e7315738559
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-06-24 08:31:34 +0000

    Define and implement the Process_Bean Ada bean that receives the job event and executes the job action

---
+++ awa/plugins/awa-jobs/src/awa-jobs-beans.adb
@@ -0,0 +1,72 @@
+-----------------------------------------------------------------------
+--  awa-jobs-beans -- AWA Jobs Ada Beans
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with AWA.Events.Action_Method;
+
+package body AWA.Jobs.Beans is
+
+   package Execute_Binding is
+     new AWA.Events.Action_Method.Bind (Bean   => Process_Bean,
+                                        Method => Execute,
+                                        Name   => "execute");
+
+   Process_Binding : aliased constant Util.Beans.Methods.Method_Binding_Array
+     := (1 => Execute_Binding.Proxy'Access);
+
+   --  ------------------------------
+   --  Get the value identified by the name.
+   --  ------------------------------
+   overriding
+   function Get_Value (From : in Process_Bean;
+                       Name : in String) return Util.Beans.Objects.Object is
+      pragma Unreferenced (From, Name);
+   begin
+      return Util.Beans.Objects.Null_Object;
+   end Get_Value;
+
+   --  ------------------------------
+   --  Set the value identified by the name.
+   --  ------------------------------
+   overriding
+   procedure Set_Value (From  : in out Process_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object) is
+   begin
+      null;
+   end Set_Value;
+
+   --  ------------------------------
+   --  This bean provides some methods that can be used in a Method_Expression
+   --  ------------------------------
+   overriding
+   function Get_Method_Bindings (From : in Process_Bean)
+                                 return Util.Beans.Methods.Method_Binding_Array_Access is
+   begin
+      return Process_Binding'Access;
+   end Get_Method_Bindings;
+
+   --  ------------------------------
+   --  Execute the job described by the event.
+   --  ------------------------------
+   procedure Execute (Bean    : in out Process_Bean;
+                      Event   : in AWA.Events.Module_Event'Class) is
+   begin
+      null;
+   end Execute;
+
+end AWA.Jobs.Beans;
---
+++ awa/plugins/awa-jobs/src/awa-jobs-beans.ads
@@ -0,0 +1,55 @@
+-----------------------------------------------------------------------
+--  awa-jobs-beans -- AWA Jobs Ada Beans
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Beans.Objects;
+with Util.Beans.Basic;
+with Util.Beans.Methods;
+
+with AWA.Events;
+package AWA.Jobs.Beans is
+
+   --  The <tt>Process_Bean</tt> is the Ada bean that receives the job event and
+   --  performs the job action associated with it.
+   type Process_Bean is new Util.Beans.Basic.Bean
+     and Util.Beans.Methods.Method_Bean with private;
+
+   --  Get the value identified by the name.
+   overriding
+   function Get_Value (From : in Process_Bean;
+                       Name : in String) return Util.Beans.Objects.Object;
+
+   --  Set the value identified by the name.
+   overriding
+   procedure Set_Value (From  : in out Process_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object);
+
+   --  This bean provides some methods that can be used in a Method_Expression
+   overriding
+   function Get_Method_Bindings (From : in Process_Bean)
+                                 return Util.Beans.Methods.Method_Binding_Array_Access;
+
+   --  Execute the job described by the event.
+   procedure Execute (Bean    : in out Process_Bean;
+                      Event   : in AWA.Events.Module_Event'Class);
+
+private
+
+   type Process_Bean is new Util.Beans.Basic.Bean
+     and Util.Beans.Methods.Method_Bean with null record;
+
+end AWA.Jobs.Beans;
