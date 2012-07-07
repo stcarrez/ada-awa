7512864f63963e1afc2aa53fe42094f3f30da841
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-07-07 16:51:09 +0000

    Define the store interface to support several storage services (file, database, Amazon)

---
+++ awa/plugins/awa-storages/src/awa-storages-stores.ads
@@ -0,0 +1,37 @@
+-----------------------------------------------------------------------
+--  awa-storages-stores -- The storage interface
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with ADO.Sessions;
+
+with AWA.Storages.Models;
+package AWA.Storages.Stores is
+
+   --  ------------------------------
+   --  Store Service
+   --  ------------------------------
+   type Store is limited interface;
+   type Store_Access is access all Store'Class;
+
+   procedure Save (Storage : in Store;
+                   Into    : in out AWA.Storages.Models.Storage_Ref'Class;
+                   Path    : in String) is abstract;
+
+   procedure Load (Storage : in Store;
+                   From    : in AWA.Storages.Models.Storage_Ref'Class;
+                   Into    : in String) is abstract;
+
+end AWA.Storages.Stores;
