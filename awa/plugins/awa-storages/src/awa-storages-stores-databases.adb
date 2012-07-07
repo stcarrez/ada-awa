b1a609a7549aa4206d8facded97836fa23db154c
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-07-07 16:51:31 +0000

    The database storage implementation

---
+++ awa/plugins/awa-storages/src/awa-storages-stores-databases.adb
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------
+--  awa-storages-stores-databases -- Database store
+--  Copyright (C) 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with ADO.Sessions;
+
+with AWA.Storages.Models;
+package body AWA.Storages.Stores.Databases is
+
+   --  ------------------------------
+   --  Storage Service
+   --  ------------------------------
+
+   procedure Save (Storage : in Database_Store;
+                   Into    : in out AWA.Storages.Models.Storage_Ref'Class;
+                   Path    : in String) is
+   begin
+      null;
+   end Save;
+
+   procedure Load (Storage : in Database_Store;
+                   From    : in AWA.Storages.Models.Storage_Ref'Class;
+                   Into    : in String) is
+   begin
+      null;
+   end Load;
+
+end AWA.Storages.Stores.Databases;
