67d464e6105d243df1c81810666e596d7a1ad0b4
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-07-25 21:09:26 +0000

    Update the documentation

--- awa/plugins/awa-workspaces/src/awa-workspaces.ads
+++ awa/plugins/awa-workspaces/src/awa-workspaces.ads
@@ -18,6 +18,11 @@
 
 --  == Introduction ==
 --  The *workspaces* plugin defines a workspace area for other plugins.
+--  The workspace is intended to link together all the data objects that an application
+--  manages for a given user or group of users.  A workspace is a possible mechanism
+--  to provide and implement multi-tenancy in a web application.  By using the workspace plugin,
+--  application data from different customers can be kept separate from each other in the
+--  same database.
 --
 --  == Ada Beans ==
 --  @include workspaces.xml
