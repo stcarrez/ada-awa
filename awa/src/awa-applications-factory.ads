0792fa33cf89864cf7e449c6919d4db2c5388aa9
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-24 21:14:24 +0000

    Rename Create_Permission_Manager into Create_Security_Manager

--- awa/src/awa-applications-factory.ads
+++ awa/src/awa-applications-factory.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-applications-factory -- Factory for AWA Applications
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2012 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,7 +17,7 @@
 -----------------------------------------------------------------------
 
 with ASF.Applications.Main;
-with Security.Permissions;
+with Security.Policies;
 package AWA.Applications.Factory is
 
    --  ------------------------------
@@ -25,12 +25,12 @@ package AWA.Applications.Factory is
    --  ------------------------------
    type Application_Factory is new ASF.Applications.Main.Application_Factory with private;
 
-   --  Create the permission manager.  The permission manager is created during
+   --  Create the security manager.  The security manager is created during
    --  the initialization phase of the application.  This implementation
    --  creates a <b>AWA.Permissions.Services.Permission_Manager</b> object.
    overriding
-   function Create_Permission_Manager (App : in Application_Factory)
-                                       return Security.Permissions.Permission_Manager_Access;
+   function Create_Security_Manager (App : in Application_Factory)
+                                     return Security.Policies.Policy_Manager_Access;
 
    --  Set the application instance that will be used when creating the permission manager.
    procedure Set_Application (Factory : in out ASF.Applications.Main.Application_Factory'Class;
