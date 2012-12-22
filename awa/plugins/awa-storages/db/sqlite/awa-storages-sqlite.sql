89ba198b391023c13c462ff839721a3002d6bad8
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-22 23:00:41 +0000

    Rebuild the model files

--- awa/plugins/awa-storages/db/sqlite/awa-storages-sqlite.sql
+++ awa/plugins/awa-storages/db/sqlite/awa-storages-sqlite.sql
@@ -1,76 +1,82 @@
 /* File generated automatically by dynamo */
-/* The database storage data when the storage type is DATABASE. */
+/* The uri member holds the URI if the storage type is URL.
+
+When storage is FILE, the local file path is built by using
+the workspace identifier and the storage identifier. */
 CREATE TABLE awa_storage (
   /* the storage identifier */
-  `id` INTEGER PRIMARY KEY,
-  /* the storage data version. */
-  `version` int ,
-  /* the local store creation date */
+  `id` BIGINT PRIMARY KEY,
+  /*  */
+  `version` INTEGER ,
+  /* the storage URI */
+  `uri` VARCHAR(255) NOT NULL,
+  /* the storage type which defines where the content is stored */
+  `storage` TINYINT NOT NULL,
+  /* the storage creation date */
   `create_date` DATETIME NOT NULL,
-  /* the storage type. */
-  `storage_type` INTEGER NOT NULL,
-  /* the storage specific URI */
-  `uri` VARCHAR(256) NOT NULL,
-  /* the storage name or filename */
-  `name` VARCHAR(256) NOT NULL,
-  /* the content mime type */
-  `mime_type` VARCHAR(256) NOT NULL,
-  /* the content size */
-  `file_size` BIGINT NOT NULL,
-  /* the storage that this local store refers to. */
-  `storage_id` INTEGER ,
-  /* the workspace that this storage belongs to. */
-  `workspace_id` INTEGER NOT NULL,
-  /* the storage folder that this storage belongs to. */
-  `folder_id` INTEGER ,
-  /* the storage file owner (or user who uploaded the document). */
-  `owner_id` INTEGER NOT NULL,
-  /* the original storage file that this storage instance refers to. */
-  `original_id` INTEGER 
+  /* the file name */
+  `name` VARCHAR(255) NOT NULL,
+  /* the file size */
+  `file_size` INTEGER NOT NULL,
+  /* the mime type */
+  `mime_type` VARCHAR(255) NOT NULL,
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  /*  */
+  `store_data_id` BIGINT ,
+  /*  */
+  `folder_id` BIGINT ,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  /*  */
+  `original_id` BIGINT 
 );
-/* The database storage data when the storage type is DATABASE. */
+/* The storage data is created only if the storage type
+is set to DATABASE.  It holds the file content in the blob. */
 CREATE TABLE awa_storage_data (
   /* the storage data identifier */
-  `id` INTEGER PRIMARY KEY,
-  /* the storage data version. */
-  `version` int ,
-  /* the storage data when the storage type is DATABASE. */
-  `data` BLOB NOT NULL
+  `id` BIGINT PRIMARY KEY,
+  /* the storage content */
+  `data`  NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL
 );
-/* Storage files are organized in folders. */
+/*  */
 CREATE TABLE awa_storage_folder (
   /* the storage folder identifier */
-  `id` INTEGER PRIMARY KEY,
-  /* the storage folder version. */
-  `version` int ,
-  /* the storage folder name */
-  `name` VARCHAR(256) NOT NULL,
+  `id` BIGINT PRIMARY KEY,
+  /*  */
+  `name` VARCHAR(255) NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
   /* the folder creation date */
   `create_date` DATETIME NOT NULL,
-  /* the workspace that this storage folder belongs to. */
-  `workspace_id` INTEGER NOT NULL
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL
 );
-/* The local store record is created when a copy of the data
-is needed on the local file system.  The creation date refers to the date when
-the data was copied on the local file system.  The expiration date indicates a
-date after which the local file can be removed from the local file system. */
+/* The local store record is created when a copy of the data is needed on the local file system.
+The creation date refers to the date when the data was copied to the local file system.
+The expiration date indicates a date after which the local file can be removed
+from the local file system. */
 CREATE TABLE awa_store_local (
-  /* the local storage identifier */
-  `id` INTEGER PRIMARY KEY,
-  /* the local storage version. */
-  `version` int ,
-  /* the local store creation date */
+  /* the local store identifier */
+  `id` BIGINT PRIMARY KEY,
+  /* the local store path */
+  `path` VARCHAR(255) NOT NULL,
+  /* the creation date */
   `create_date` DATETIME NOT NULL,
   /* the local store expiration date */
   `expire_date` DATETIME NOT NULL,
-  /* the local store path */
-  `path` VARCHAR(256) NOT NULL,
-  /* the storage version that this local store represents */
+  /*  */
+  `version` INTEGER NOT NULL,
+  /*  */
   `store_version` INTEGER NOT NULL,
-  /* whether the local store file can be shared or not */
+  /* the shared flag which indicates whether this local store can be shared by several clients. */
   `shared` TINYINT NOT NULL,
-  /* the storage that this local store refers to. */
-  `storage_id` INTEGER NOT NULL
+  /*  */
+  `storage_id` BIGINT 
 );
 INSERT INTO entity_type (name) VALUES ("awa_storage");
 INSERT INTO entity_type (name) VALUES ("awa_storage_data");
