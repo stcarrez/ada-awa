cbcb2fa2edb49dfc9a0e00c09c2d8b815552e361
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-31 17:44:01 +0000

    Improvement of storage list - merge the folder list, file lists and current folder in a same Ada bean - initialize the folder when the folderId attribute is set - otherwise set the folder by using the first folder in the list - use the current folder to build the list of files

--- awa/plugins/awa-storages/src/awa-storages-beans.adb
+++ awa/plugins/awa-storages/src/awa-storages-beans.adb
@@ -15,13 +15,14 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Containers;
+
 with ADO;
 with ADO.Queries;
 with ADO.Sessions;
 with ADO.Objects;
 with ADO.Sessions.Entities;
 
-with AWA.Helpers.Requests;
 with AWA.Workspaces.Models;
 with AWA.Services.Contexts;
 with AWA.Storages.Services;
@@ -79,7 +80,7 @@ package body AWA.Storages.Beans is
    --  ------------------------------
    procedure Upload (Bean    : in out Upload_Bean;
                      Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
-
+      pragma Unreferenced (Bean);
    begin
       Outcome := Ada.Strings.Unbounded.To_Unbounded_String ("success");
    end Upload;
@@ -134,11 +135,120 @@ package body AWA.Storages.Beans is
       Outcome := Ada.Strings.Unbounded.To_Unbounded_String ("success");
    end Save;
 
+   --  ------------------------------
+   --  Load the folder instance.
+   --  ------------------------------
+   procedure Load_Folder (Storage : in Storage_List_Bean) is
+      use AWA.Storages.Models;
+      use AWA.Services;
+      use type Ada.Containers.Count_Type;
+
+      Manager : constant Services.Storage_Service_Access := Storage.Module.Get_Storage_Manager;
+   begin
+      Load_Folders (Storage);
+      if Storage.Folder_List.List.Length > 0 then
+         Manager.Load_Folder (Storage.Folder_Bean.all,
+                                      Storage.Folder_List.List.Element (0).Id);
+
+      end if;
+      Storage.Flags (INIT_FOLDER) := True;
+   end Load_Folder;
+
+   --  ------------------------------
+   --  Load the list of folders.
+   --  ------------------------------
+   procedure Load_Folders (Storage : in Storage_List_Bean) is
+      use AWA.Storages.Models;
+      use AWA.Services;
+      use type ADO.Identifier;
+
+      Ctx     : constant Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      User    : constant ADO.Identifier := Ctx.Get_User_Identifier;
+      Session : ADO.Sessions.Session := Storage.Module.Get_Session;
+      Query   : ADO.Queries.Context;
+   begin
+      Query.Set_Query (AWA.Storages.Models.Query_Storage_Folder_List);
+      Query.Bind_Param ("user_id", User);
+      ADO.Sessions.Entities.Bind_Param (Query, "table",
+                                        AWA.Workspaces.Models.WORKSPACE_TABLE, Session);
+      AWA.Storages.Models.List (Storage.Folder_List_Bean.all, Session, Query);
+      Storage.Flags (INIT_FOLDER_LIST) := True;
+   end Load_Folders;
+
+   --  ------------------------------
+   --  Load the list of files associated with the current folder.
+   --  ------------------------------
+   procedure Load_Files (Storage : in Storage_List_Bean) is
+      use AWA.Storages.Models;
+      use AWA.Services;
+
+      Ctx       : constant Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      User      : constant ADO.Identifier := Ctx.Get_User_Identifier;
+      Session   : ADO.Sessions.Session := Storage.Module.Get_Session;
+      Query     : ADO.Queries.Context;
+   begin
+      if not Storage.Init_Flags (INIT_FOLDER) then
+         Load_Folder (Storage);
+      end if;
+      Query.Set_Query (AWA.Storages.Models.Query_Storage_List);
+      Query.Bind_Param ("user_id", User);
+      ADO.Sessions.Entities.Bind_Param (Query, "table",
+                                        AWA.Workspaces.Models.WORKSPACE_TABLE, Session);
+      if Storage.Folder_Bean.Is_Null then
+         Query.Bind_Null_Param ("folder_id");
+      else
+         Query.Bind_Param ("folder_id", Storage.Folder_Bean.Get_Id);
+      end if;
+      AWA.Storages.Models.List (Storage.Files_List_Bean.all, Session, Query);
+      Storage.Flags (INIT_FILE_LIST) := True;
+   end Load_Files;
+
+   --  ------------------------------
+   --  Set the value identified by the name.
+   --  ------------------------------
+   overriding
+   procedure Set_Value (From  : in out Storage_List_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object) is
+      Manager : constant Services.Storage_Service_Access := From.Module.Get_Storage_Manager;
+   begin
+      if Name = "folderId" then
+         Manager.Load_Folder (From.Folder, ADO.Identifier (Util.Beans.Objects.To_Integer (Value)));
+         From.Flags (INIT_FOLDER) := True;
+      end if;
+   end Set_Value;
+
    overriding
    function Get_Value (List : in Storage_List_Bean;
                        Name : in String) return Util.Beans.Objects.Object is
    begin
-      return Models.Storage_Info_List_Bean (List).Get_Value (Name);
+      if Name = "files" then
+         if not List.Init_Flags (INIT_FILE_LIST) then
+            Load_Files (List);
+         end if;
+         return Util.Beans.Objects.To_Object (Value   => List.Files_List_Bean,
+                                              Storage => Util.Beans.Objects.STATIC);
+
+      elsif Name = "folders" then
+         if not List.Init_Flags (INIT_FOLDER_LIST) then
+            Load_Folders (List);
+         end if;
+         return Util.Beans.Objects.To_Object (Value   => List.Folder_List_Bean,
+                                              Storage => Util.Beans.Objects.STATIC);
+
+      elsif Name = "folder" then
+         if not List.Init_Flags (INIT_FOLDER) then
+            Load_Folder (List);
+         end if;
+         if List.Folder_Bean.Is_Null then
+            return Util.Beans.Objects.Null_Object;
+         end if;
+         return Util.Beans.Objects.To_Object (Value   => List.Folder_Bean,
+                                              Storage => Util.Beans.Objects.STATIC);
+
+      else
+         return Util.Beans.Objects.Null_Object;
+      end if;
    end Get_Value;
 
    --  ------------------------------
@@ -170,27 +280,13 @@ package body AWA.Storages.Beans is
    --  ------------------------------
    function Create_Storage_List_Bean (Module : in AWA.Storages.Modules.Storage_Module_Access)
                                       return Util.Beans.Basic.Readonly_Bean_Access is
-      use AWA.Storages.Models;
-      use AWA.Services;
-      use type ADO.Identifier;
-
-      Ctx       : constant Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
-      User      : constant ADO.Identifier := Ctx.Get_User_Identifier;
       Object    : constant Storage_List_Bean_Access := new Storage_List_Bean;
-      Session   : ADO.Sessions.Session := Module.Get_Session;
-      Query     : ADO.Queries.Context;
-      Folder_Id : constant ADO.Identifier := Helpers.Requests.Get_Parameter (FOLDER_ID_PARAMETER);
    begin
-      Query.Set_Query (AWA.Storages.Models.Query_Storage_List);
-      Query.Bind_Param ("user_id", User);
-      ADO.Sessions.Entities.Bind_Param (Query, "table",
-                                        AWA.Workspaces.Models.WORKSPACE_TABLE, Session);
-      if Folder_Id = ADO.NO_IDENTIFIER then
-         Query.Bind_Null_Param ("folder_id");
-      else
-         Query.Bind_Param ("folder_id", Folder_Id);
-      end if;
-      AWA.Storages.Models.List (Object.all, Session, Query);
+      Object.Module           := Module;
+      Object.Folder_Bean      := Object.Folder'Access;
+      Object.Folder_List_Bean := Object.Folder_List'Access;
+      Object.Files_List_Bean  := Object.Files_List'Access;
+      Object.Flags            := Object.Init_Flags'Access;
       return Object.all'Access;
    end Create_Storage_List_Bean;
 
--- awa/plugins/awa-storages/src/awa-storages-beans.ads
+++ awa/plugins/awa-storages/src/awa-storages-beans.ads
@@ -89,18 +89,51 @@ package AWA.Storages.Beans is
    procedure Save (Bean    : in out Folder_Bean;
                    Outcome : in out Ada.Strings.Unbounded.Unbounded_String);
 
+   type Init_Flag is (INIT_FOLDER, INIT_FOLDER_LIST, INIT_FILE_LIST);
+   type Init_Map is array (Init_Flag) of Boolean;
+
    --  ------------------------------
    --  Storage List Bean
    --  ------------------------------
    --  This bean represents a list of storage files for a given folder.
-   type Storage_List_Bean is new AWA.Storages.Models.Storage_Info_List_Bean with record
-      Folder : Folder_Bean;
+   type Storage_List_Bean is new Util.Beans.Basic.Bean with record
+      Module           : AWA.Storages.Modules.Storage_Module_Access := null;
+
+      --  Current folder.
+      Folder           : aliased Folder_Bean;
+      Folder_Bean      : Folder_Bean_Access;
+
+      --  List of folders.
+      Folder_List      : aliased AWA.Storages.Models.Folder_Info_List_Bean;
+      Folder_List_Bean : AWA.Storages.Models.Folder_Info_List_Bean_Access;
+
+      --  List of files.
+      Files_List       : aliased AWA.Storages.Models.Storage_Info_List_Bean;
+      Files_List_Bean  : AWA.Storages.Models.Storage_Info_List_Bean_Access;
+
+      Init_Flags       : aliased Init_Map := (others => False);
+      Flags            : access Init_Map;
    end record;
    type Storage_List_Bean_Access is access all Storage_List_Bean'Class;
+
+   --  Load the folder instance.
+   procedure Load_Folder (Storage : in Storage_List_Bean);
+
+   --  Load the list of folders.
+   procedure Load_Folders (Storage : in Storage_List_Bean);
+
+   --  Load the list of files associated with the current folder.
+   procedure Load_Files (Storage : in Storage_List_Bean);
 
    overriding
    function Get_Value (List : in Storage_List_Bean;
                        Name : in String) return Util.Beans.Objects.Object;
+
+   --  Set the value identified by the name.
+   overriding
+   procedure Set_Value (From  : in out Storage_List_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object);
 
    --  Create the Folder_List_Bean bean instance.
    function Create_Folder_List_Bean (Module : in AWA.Storages.Modules.Storage_Module_Access)
