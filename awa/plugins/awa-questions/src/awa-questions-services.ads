3516c58a2c7204315f86eeb5154a013187c7b3d6
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-06 13:29:35 +0000

    New procedure Load_Question, Delete_Question and Save_Answer

--- awa/plugins/awa-questions/src/awa-questions-services.ads
+++ awa/plugins/awa-questions/src/awa-questions-services.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-questions-services -- Service services
---  Copyright (C) 2012 Stephane Carrez
+--  Copyright (C) 2012, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,9 +15,11 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with ADO;
+
+with Security.Permissions;
 
 with AWA.Modules;
-with Security.Permissions;
 with AWA.Questions.Models;
 package AWA.Questions.Services is
 
@@ -25,6 +27,7 @@ package AWA.Questions.Services is
    package ACL_Create_Questions is new Security.Permissions.Definition ("question-create");
    package ACL_Delete_Questions is new Security.Permissions.Definition ("question-delete");
    package ACL_Update_Questions is new Security.Permissions.Definition ("question-update");
+   package ACL_Answer_Questions is new Security.Permissions.Definition ("answer-create");
 
    --  The maximum length for a short description.
    SHORT_DESCRIPTION_LENGTH : constant Positive := 200;
@@ -36,8 +39,22 @@ package AWA.Questions.Services is
    type Question_Service_Access is access all Question_Service'Class;
 
    --  Create or save the question.
-   procedure Save_Question (Model    : in out Question_Service;
+   procedure Save_Question (Model    : in Question_Service;
                             Question : in out AWA.Questions.Models.Question_Ref'Class);
+
+   --  Delete the question.
+   procedure Delete_Question (Model    : in Question_Service;
+                              Question : in out AWA.Questions.Models.Question_Ref'Class);
+
+   --  Load the question.
+   procedure Load_Question (Model    : in Question_Service;
+                            Question : in out AWA.Questions.Models.Question_Ref'Class;
+                            Id       : in ADO.Identifier);
+
+   --  Create or save the answer.
+   procedure Save_Answer (Model    : in Question_Service;
+                          Question : in AWA.Questions.Models.Question_Ref'Class;
+                          Answer   : in out AWA.Questions.Models.Answer_Ref'Class);
 
 private
 
