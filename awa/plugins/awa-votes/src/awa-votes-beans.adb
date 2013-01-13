62b8c4881ff37fdc13b1a46f86d3e1cc3c304beb
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-13 09:49:35 +0000

    New procedure Vote

--- awa/plugins/awa-votes/src/awa-votes-beans.adb
+++ awa/plugins/awa-votes/src/awa-votes-beans.adb
@@ -28,10 +28,11 @@ package body AWA.Votes.Beans is
       pragma Unreferenced (Outcome);
    begin
       Bean.Entity_Id := AWA.Helpers.Requests.Get_Parameter ("id");
+      Bean.Rating    := 1;
       Bean.Module.Vote_For (Permission  => Ada.Strings.Unbounded.To_String (Bean.Permission),
                             Entity_Type => Ada.Strings.Unbounded.To_String (Bean.Entity_Type),
                             Id          => Bean.Entity_Id,
-                            Value       => 1,
+                            Value       => Bean.Rating,
                             Total       => Bean.Total);
    end Vote_Up;
 
@@ -44,12 +45,33 @@ package body AWA.Votes.Beans is
       pragma Unreferenced (Outcome);
    begin
       Bean.Entity_Id := AWA.Helpers.Requests.Get_Parameter ("id");
+      Bean.Rating    := -1;
       Bean.Module.Vote_For (Permission  => Ada.Strings.Unbounded.To_String (Bean.Permission),
                             Entity_Type => Ada.Strings.Unbounded.To_String (Bean.Entity_Type),
                             Id          => Bean.Entity_Id,
-                            Value       => -1,
+                            Value       => Bean.Rating,
                             Total       => Bean.Total);
    end Vote_Down;
+
+   --  ------------------------------
+   --  Action to vote.
+   --  ------------------------------
+   overriding
+   procedure Vote (Bean    : in out Vote_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+      pragma Unreferenced (Outcome);
+   begin
+      Bean.Entity_Id := AWA.Helpers.Requests.Get_Parameter ("id");
+      Bean.Rating    := AWA.Helpers.Requests.Get_Parameter ("rating", 0);
+      if Bean.Rating /= 0 and Bean.Rating /= -1 and Bean.Rating /= 1 then
+         Bean.Rating := 0;
+      end if;
+      Bean.Module.Vote_For (Permission  => Ada.Strings.Unbounded.To_String (Bean.Permission),
+                            Entity_Type => Ada.Strings.Unbounded.To_String (Bean.Entity_Type),
+                            Id          => Bean.Entity_Id,
+                            Value       => Bean.Rating,
+                            Total       => Bean.Total);
+   end Vote;
 
    --  ------------------------------
    --  Create the Vote_Bean bean instance.
