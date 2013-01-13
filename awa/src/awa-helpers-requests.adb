7f55583feecb9fe6395bf7dc138aadcff950fa6d
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-13 19:40:34 +0000

    New function Get_Parameter

--- awa/src/awa-helpers-requests.adb
+++ awa/src/awa-helpers-requests.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-helpers-requests -- Helpers for AWA applications
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -35,6 +35,25 @@ package body AWA.Helpers.Requests is
    exception
       when others =>
          return ADO.NO_IDENTIFIER;
+   end Get_Parameter;
+
+   --  ------------------------------
+   --  Get the parameter identified by the given name and return it as an integer.
+   --  Returns the default value if the parameter does not exist or is not valid.
+   --  ------------------------------
+   function Get_Parameter (Name    : in String;
+                           Default : in Integer) return Integer is
+      Ctx  : constant ASF.Contexts.Faces.Faces_Context_Access := ASF.Contexts.Faces.Current;
+      P    : constant String := Ctx.Get_Parameter (Name);
+   begin
+      if P = "" then
+         return Default;
+      else
+         return Integer'Value (P);
+      end if;
+   exception
+      when others =>
+         return Default;
    end Get_Parameter;
 
 end AWA.Helpers.Requests;
