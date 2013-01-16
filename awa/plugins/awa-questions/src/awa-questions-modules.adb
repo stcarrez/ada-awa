dedb61a7b372a90d1eb44800c5677fbb8042fc0a
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-16 21:40:48 +0000

    Move the question services in the question module (simplify the implementation)

--- awa/plugins/awa-questions/src/awa-questions-modules.adb
+++ awa/plugins/awa-questions/src/awa-questions-modules.adb
@@ -15,8 +15,21 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Util.Log.Loggers;
+with Ada.Calendar;
+with Ada.Characters.Conversions;
 
+with AWA.Permissions;
+with AWA.Services.Contexts;
+with AWA.Users.Models;
+with AWA.Workspaces.Models;
+with AWA.Workspaces.Modules;
+with AWA.Wikis.Parsers;
+with AWA.Wikis.Writers;
+
+with ADO.Sessions;
+with ADO.Statements;
+
+with Util.Log.Loggers;
 with AWA.Modules.Beans;
 with AWA.Modules.Get;
 with AWA.Questions.Beans;
@@ -62,31 +75,7 @@ package body AWA.Questions.Modules is
                          Handler => AWA.Questions.Beans.Create_Question_Display_Bean'Access);
 
       AWA.Modules.Module (Plugin).Initialize (App, Props);
-
-      --  Add here the creation of manager instances.
-      Plugin.Manager := Plugin.Create_Question_Manager;
    end Initialize;
-
-   --  ------------------------------
-   --  Get the question manager.
-   --  ------------------------------
-   function Get_Question_Manager (Plugin : in Question_Module)
-                                  return Services.Question_Service_Access is
-   begin
-      return Plugin.Manager;
-   end Get_Question_Manager;
-
-   --  ------------------------------
-   --  Create a question manager.  This operation can be overridden to provide another
-   --  question service implementation.
-   --  ------------------------------
-   function Create_Question_Manager (Plugin : in Question_Module)
-                                     return Services.Question_Service_Access is
-      Result : constant Services.Question_Service_Access := new Services.Question_Service;
-   begin
-      Result.Initialize (Plugin);
-      return Result;
-   end Create_Question_Manager;
 
    --  ------------------------------
    --  Get the questions module.
@@ -98,17 +87,174 @@ package body AWA.Questions.Modules is
    end Get_Question_Module;
 
    --  ------------------------------
-   --  Get the question manager instance associated with the current application.
+   --  Create or save the question.
    --  ------------------------------
-   function Get_Question_Manager return Services.Question_Service_Access is
-      Module : constant Question_Module_Access := Get_Question_Module;
+   procedure Save_Question (Model    : in Question_Module;
+                            Question : in out AWA.Questions.Models.Question_Ref'Class) is
+      pragma Unreferenced (Model);
+
+      function To_Wide (Item : in String) return Wide_Wide_String
+                        renames Ada.Characters.Conversions.To_Wide_Wide_String;
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      User  : constant AWA.Users.Models.User_Ref := Ctx.Get_User;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+      WS    : AWA.Workspaces.Models.Workspace_Ref;
    begin
-      if Module = null then
-         Log.Error ("There is no active Question_Module");
-         return null;
+      Ctx.Start;
+      if Question.Is_Inserted then
+         Log.Info ("Updating question {0}", ADO.Identifier'Image (Question.Get_Id));
+
+         WS := AWA.Workspaces.Models.Workspace_Ref (Question.Get_Workspace);
       else
-         return Module.Get_Question_Manager;
+         Log.Info ("Creating new question {0}", String '(Question.Get_Title));
+
+         AWA.Workspaces.Modules.Get_Workspace (DB, Ctx, WS);
+
+         --  Check that the user has the create permission on the given workspace.
+         AWA.Permissions.Check (Permission => ACL_Create_Questions.Permission,
+                                Entity     => WS);
+         Question.Set_Workspace (WS);
+         Question.Set_Author (User);
       end if;
-   end Get_Question_Manager;
+
+      declare
+         Text : constant String := AWA.Wikis.Writers.To_Text (To_Wide (Question.Get_Description),
+                                                              AWA.Wikis.Parsers.SYNTAX_MIX);
+         Last : Natural;
+      begin
+         if Text'Length < SHORT_DESCRIPTION_LENGTH then
+            Last := Text'Last;
+         else
+            Last := SHORT_DESCRIPTION_LENGTH;
+         end if;
+         Question.Set_Short_Description (Text (Text'First .. Last) & "...");
+      end;
+      if not Question.Is_Inserted then
+         Question.Set_Create_Date (Ada.Calendar.Clock);
+      else
+         Question.Set_Edit_Date (Ada.Calendar.Clock);
+      end if;
+      Question.Save (DB);
+      Ctx.Commit;
+   end Save_Question;
+
+   --  ------------------------------
+   --  Delete the question.
+   --  ------------------------------
+   procedure Delete_Question (Model    : in Question_Module;
+                              Question : in out AWA.Questions.Models.Question_Ref'Class) is
+      pragma Unreferenced (Model);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+   begin
+      Ctx.Start;
+
+      --  Check that the user has the delete permission on the given question.
+      AWA.Permissions.Check (Permission => ACL_Delete_Questions.Permission,
+                             Entity     => Question);
+
+      --  Before deleting the question, delete the associated answers.
+      declare
+         Stmt : ADO.Statements.Delete_Statement
+           := DB.Create_Statement (AWA.Questions.Models.ANSWER_TABLE);
+      begin
+         Stmt.Set_Filter (Filter => "question_id = ?");
+         Stmt.Add_Param (Value => Question);
+         Stmt.Execute;
+      end;
+      Question.Delete (DB);
+      Ctx.Commit;
+   end Delete_Question;
+
+   --  ------------------------------
+   --  Load the question.
+   --  ------------------------------
+   procedure Load_Question (Model    : in Question_Module;
+                            Question : in out AWA.Questions.Models.Question_Ref'Class;
+                            Id       : in ADO.Identifier) is
+      pragma Unreferenced (Model);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Session := AWA.Services.Contexts.Get_Session (Ctx);
+      Found : Boolean;
+   begin
+      Question.Load (DB, Id, Found);
+   end Load_Question;
+
+   --  ------------------------------
+   --  Create or save the answer.
+   --  ------------------------------
+   procedure Save_Answer (Model    : in Question_Module;
+                          Question : in AWA.Questions.Models.Question_Ref'Class;
+                          Answer   : in out AWA.Questions.Models.Answer_Ref'Class) is
+      pragma Unreferenced (Model);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      User  : constant AWA.Users.Models.User_Ref := Ctx.Get_User;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+   begin
+      Ctx.Start;
+      if Answer.Is_Inserted then
+         Log.Info ("Updating question {0}", ADO.Identifier'Image (Answer.Get_Id));
+
+      else
+         Log.Info ("Creating new answer for {0}", ADO.Identifier'Image (Question.Get_Id));
+
+         --  Check that the user has the create permission on the given workspace.
+         AWA.Permissions.Check (Permission => ACL_Answer_Questions.Permission,
+                                Entity     => Question);
+         Answer.Set_Author (User);
+      end if;
+
+      if not Answer.Is_Inserted then
+         Answer.Set_Create_Date (Ada.Calendar.Clock);
+         Answer.Set_Question (Question);
+      else
+         Answer.Set_Edit_Date (ADO.Nullable_Time '(Value   => Ada.Calendar.Clock,
+                                                   Is_Null => False));
+      end if;
+      Answer.Save (DB);
+      Ctx.Commit;
+   end Save_Answer;
+
+   --  ------------------------------
+   --  Delete the answer.
+   --  ------------------------------
+   procedure Delete_Answer (Model  : in Question_Module;
+                            Answer : in out AWA.Questions.Models.Answer_Ref'Class) is
+      pragma Unreferenced (Model);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+   begin
+      Ctx.Start;
+
+      --  Check that the user has the delete permission on the given answer.
+      AWA.Permissions.Check (Permission => ACL_Delete_Answer.Permission,
+                             Entity     => Answer);
+
+
+      Answer.Delete (DB);
+      Ctx.Commit;
+   end Delete_Answer;
+
+   --  ------------------------------
+   --  Load the answer.
+   --  ------------------------------
+   procedure Load_Answer (Model    : in Question_Module;
+                          Answer   : in out AWA.Questions.Models.Answer_Ref'Class;
+                          Question : in out AWA.Questions.Models.Question_Ref'Class;
+                          Id       : in ADO.Identifier) is
+      pragma Unreferenced (Model);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Session := AWA.Services.Contexts.Get_Session (Ctx);
+      Found : Boolean;
+   begin
+      Answer.Load (DB, Id, Found);
+      Question := Answer.Get_Question;
+   end Load_Answer;
 
 end AWA.Questions.Modules;
