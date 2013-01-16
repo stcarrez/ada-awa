e888fa9c8afebf91c89fde1ed9bac40ebd71d268
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-01-16 20:39:48 +0000

    Add some documentation

--- awa/plugins/awa-votes/src/awa-votes-beans.ads
+++ awa/plugins/awa-votes/src/awa-votes-beans.ads
@@ -21,6 +21,38 @@ with Ada.Strings.Unbounded;
 with Util.Beans.Basic;
 with AWA.Votes.Modules;
 with AWA.Votes.Models;
+
+--  === Vote Beans ===
+--  The <b>Vote_Bean</b> is a bean intended to be used in presentation files (facelet files)
+--  to vote for an item.  The managed bean can be easily configured in the application XML
+--  configuration file.  The <b>permission</b> and <b>entity_type</b> are the two properties
+--  that should be defined in the configuration.  The <b>permission</b> is the name of the
+--  permission that must be used to verify that the user is allowed to vote for the item.
+--  The <b>entity_type</b> is the name of the entity (table name) used by the item.
+--  The example below defines the bean <tt>questionVote</tt> defined by the question module.
+--
+--    <managed-bean>
+--      <description>The vote bean that allows to vote for a question.</description>
+--      <managed-bean-name>questionVote</managed-bean-name>
+--      <managed-bean-class>AWA.Votes.Beans.Votes_Bean</managed-bean-class>
+--      <managed-bean-scope>request</managed-bean-scope>
+--      <managed-property>
+--        <property-name>permission</property-name>
+--        <property-class>String</property-class>
+--        <value>answer-create</value>
+--      </managed-property>
+--      <managed-property>
+--        <property-name>entity_type</property-name>
+--        <property-class>String</property-class>
+--        <value>awa_question</value>
+--      </managed-property>
+--    </managed-bean>
+--
+--  The vote concerns entities for the <tt>awa_question</tt> entity table.
+--  The permission <tt>answer-create</tt> is used to verify for the vote.
+--
+--  [http://ada-awa.googlecode.com/svn/wiki/awa_votes_bean.png]
+--
 package AWA.Votes.Beans is
 
    type Vote_Bean is new AWA.Votes.Models.Vote_Bean with private;
--- awa/plugins/awa-votes/src/awa-votes.ads
+++ awa/plugins/awa-votes/src/awa-votes.ads
@@ -15,5 +15,24 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+
+--  == Introduction ==
+--  The <b>Votes</b> module allows users to vote for objects defined in the application.
+--  Users can vote by setting a rating value on an item (+1, -1 or any other integer value).
+--  The Votes module makes sure that users can vote only once for an item.  A global rating
+--  is associated with the item to give the vote summary.  The vote can be associated with
+--  any database entity and it is not necessary to change other entities data model.
+--
+--  == Model ==
+--  [http://ada-awa.googlecode.com/svn/wiki/awa_votes_model.png]
+--
+--  == Integration ==
+--  To add the <b>Votes</b> module in an application, the <tt>Vote_Module</tt> instance must
+--  be declared and registered in the AWA application.
+--
+--  @include awa-votes-beans.ads
 package AWA.Votes is
+
+   pragma Preelaborate;
+
 end AWA.Votes;
