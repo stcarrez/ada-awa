fa99c446851c3ff8abccd9c43c1ea11fb057b1b9
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-07-21 09:15:09 +0000

    Document the AWA.Settings module

--- awa/plugins/awa-settings/src/awa-settings.ads
+++ awa/plugins/awa-settings/src/awa-settings.ads
@@ -15,6 +15,30 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+
+--  == Introduction ==
+--  The <b>Settings</b> module provides management of application and user settings.
+--  A setting is identified by a unique name in the application.  It is saved in
+--  the database and associated to a user.
+--
+--  === Getting a user setting ===
+--  Getting a user setting is as simple as calling a function with the setting name
+--  and the default value.  If the setting was modified by the user and saved in the
+--  database, the saved value will be returned.  Otherwise, the default value is returned.
+--  For example, if an application defines a <tt>row-per-page</tt> setting to define how
+--  many rows are defined in a list, the user setting can be retrieved with:
+--
+--    Row_Per_Page : constant Integer := AWA.Settings.Get_User_Setting ("row-per-page", 10);
+--
+--  === Saving a user setting ===
+--  When a user changes the setting value, we just have to save it in the database.
+--  The setting value will either be updated if it exists or created.
+--
+--    AWA.Settings.Set_User_Setting ("row-per-page", 20);
+--
+--  == Model ==
+--  [http://ada-awa.googlecode.com/svn/wiki/awa_settings_model.png]
+--
 package AWA.Settings is
 
    --  Get the user setting identified by the given name.
