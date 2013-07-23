e2dc248ffa97da37c055b668ee3b8a4e19aa7088
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-07-23 20:19:01 +0000

    Rebuild the SQL files

--- awa/plugins/awa-tags/db/mysql/drop-awa-tags-mysql.sql
+++ awa/plugins/awa-tags/db/mysql/drop-awa-tags-mysql.sql
@@ -2,44 +2,6 @@
 /* File generated automatically by dynamo */
 DROP TABLE IF EXISTS `awa_tagged_entity`;
 DROP TABLE IF EXISTS `awa_tag`;
-/* Copied from awa-blogs-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_post`;
-DROP TABLE IF EXISTS `awa_blog`;
-/* Copied from awa-questions-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_question`;
-DROP TABLE IF EXISTS `awa_answer`;
-/* Copied from awa-countries-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_region`;
-DROP TABLE IF EXISTS `awa_country_neighbor`;
-DROP TABLE IF EXISTS `awa_country`;
-DROP TABLE IF EXISTS `awa_city`;
-/* Copied from awa-jobs-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_job`;
-/* Copied from awa-votes-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_vote`;
-DROP TABLE IF EXISTS `awa_rating`;
-/* Copied from awa-images-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_image`;
-/* Copied from awa-storages-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_store_local`;
-DROP TABLE IF EXISTS `awa_storage_folder`;
-DROP TABLE IF EXISTS `awa_storage_data`;
-DROP TABLE IF EXISTS `awa_storage`;
-/* Copied from awa-workspaces-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_workspace_member`;
-DROP TABLE IF EXISTS `awa_workspace_feature`;
-DROP TABLE IF EXISTS `awa_workspace`;
-/* Copied from awa-comments-drop-mysql.sql*/
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `awa_comments`;
 /* Copied from ado-drop-mysql.sql*/
 /* File generated automatically by dynamo */
 DROP TABLE IF EXISTS `sequence`;
