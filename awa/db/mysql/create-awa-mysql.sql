20442d848fc931dc104edc4782572e826db16ac1
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-12-26 21:55:14 +0000

    Rebuild the SQL files

--- awa/db/mysql/create-awa-mysql.sql
+++ awa/db/mysql/create-awa-mysql.sql
@@ -1,3 +1,29 @@
+/* Copied from ado-mysql.sql*/
+/* File generated automatically by dynamo */
+/* Entity types */
+CREATE TABLE entity_type (
+  /* the entity type identifier */
+  `id` INTEGER  AUTO_INCREMENT,
+  /* the entity type name (table name) */
+  `name` VARCHAR(127) UNIQUE NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* Sequence generator */
+CREATE TABLE sequence (
+  /* the sequence name */
+  `name` VARCHAR(127) NOT NULL,
+  /* the sequence record version */
+  `version` int ,
+  /* the sequence value */
+  `value` BIGINT ,
+  /* the sequence block size */
+  `block_size` BIGINT ,
+  PRIMARY KEY (`name`)
+);
+INSERT INTO entity_type (name) VALUES
+("entity_type")
+,("sequence")
+;
 /* Copied from awa-mysql.sql*/
 /* File generated automatically by dynamo */
 /*  */
@@ -672,30 +698,4 @@ CREATE TABLE awa_post (
 INSERT INTO entity_type (name) VALUES
 ("awa_blog")
 ,("awa_post")
-;
-/* Copied from ado-mysql.sql*/
-/* File generated automatically by dynamo */
-/* Entity types */
-CREATE TABLE entity_type (
-  /* the entity type identifier */
-  `id` INTEGER  AUTO_INCREMENT,
-  /* the entity type name (table name) */
-  `name` VARCHAR(127) UNIQUE NOT NULL,
-  PRIMARY KEY (`id`)
-);
-/* Sequence generator */
-CREATE TABLE sequence (
-  /* the sequence name */
-  `name` VARCHAR(127) NOT NULL,
-  /* the sequence record version */
-  `version` int ,
-  /* the sequence value */
-  `value` BIGINT ,
-  /* the sequence block size */
-  `block_size` BIGINT ,
-  PRIMARY KEY (`name`)
-);
-INSERT INTO entity_type (name) VALUES
-("entity_type")
-,("sequence")
 ;
--- awa/db/sqlite/create-awa-sqlite.sql
+++ awa/db/sqlite/create-awa-sqlite.sql
@@ -1,3 +1,25 @@
+/* Copied from ado-sqlite.sql*/
+/* File generated automatically by dynamo */
+/* Entity types */
+CREATE TABLE entity_type (
+  /* the entity type identifier */
+  `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
+  /* the entity type name (table name) */
+  `name` VARCHAR(127) UNIQUE NOT NULL
+);
+/* Sequence generator */
+CREATE TABLE sequence (
+  /* the sequence name */
+  `name` VARCHAR(127) PRIMARY KEY,
+  /* the sequence record version */
+  `version` int ,
+  /* the sequence value */
+  `value` BIGINT ,
+  /* the sequence block size */
+  `block_size` BIGINT 
+);
+INSERT INTO entity_type (name) VALUES ("entity_type");
+INSERT INTO entity_type (name) VALUES ("sequence");
 /* Copied from awa-sqlite.sql*/
 /* File generated automatically by dynamo */
 /*  */
@@ -616,25 +638,3 @@ CREATE TABLE awa_post (
 );
 INSERT INTO entity_type (name) VALUES ("awa_blog");
 INSERT INTO entity_type (name) VALUES ("awa_post");
-/* Copied from ado-sqlite.sql*/
-/* File generated automatically by dynamo */
-/* Entity types */
-CREATE TABLE entity_type (
-  /* the entity type identifier */
-  `ID` INTEGER PRIMARY KEY AUTOINCREMENT,
-  /* the entity type name (table name) */
-  `name` VARCHAR(127) UNIQUE NOT NULL
-);
-/* Sequence generator */
-CREATE TABLE sequence (
-  /* the sequence name */
-  `name` VARCHAR(127) PRIMARY KEY,
-  /* the sequence record version */
-  `version` int ,
-  /* the sequence value */
-  `value` BIGINT ,
-  /* the sequence block size */
-  `block_size` BIGINT 
-);
-INSERT INTO entity_type (name) VALUES ("entity_type");
-INSERT INTO entity_type (name) VALUES ("sequence");
