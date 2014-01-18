683232ccc1022fba2134cbea8b91ff9fb1382779
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-01-18 14:25:36 +0000

    New function Get_Parameter to retrieve a request parameter as an Object

--- awa/src/awa-helpers-requests.ads
+++ awa/src/awa-helpers-requests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-helpers-requests -- Helpers for AWA applications
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2014 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,7 @@
 
 with Ada.Calendar;
 with ADO;
+with Util.Beans.Objects;
 package AWA.Helpers.Requests is
 
    --  Get the parameter identified by the given name and return it as an identifier.
@@ -39,5 +40,9 @@ package AWA.Helpers.Requests is
    --  The date is assumed to be in ISO8601 format.
    function Get_Parameter (Name    : in String;
                            Default : in String) return Ada.Calendar.Time;
+
+   --  Get the parameter identified by the given name and return it as an Object.
+   --  Returns the NULL object value if the parameter does not exist.
+   function Get_Parameter (Name    : in String) return Util.Beans.Objects.Object;
 
 end AWA.Helpers.Requests;
