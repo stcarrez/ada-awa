824083e9f3c9f41e305df860eaad3ef6e4fb0609
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-03-10 22:20:42 +0000

    SQL script for database migration

---
+++ awa/db/migrate/awa-1.0-migrate.sql
@@ -0,0 +1,13 @@
+ALTER TABLE awa_blog ADD COLUMN `url` VARCHAR(255) BINARY NOT NULL;
+
+ALTER TABLE awa_post CHANGE `text` `text` TEXT NOT NULL;
+ALTER TABLE awa_question CHANGE `description` `description` TEXT NOT NULL;
+ALTER TABLE awa_answer CHANGE `answer` `answer` TEXT NOT NULL;
+
+ALTER TABLE awa_comments CHANGE `date` `create_date` DATETIME NOT NULL;
+ALTER TABLE awa_comments CHANGE `message` `message` TEXT NOT NULL;
+ALTER TABLE awa_comments ADD COLUMN `status` INTEGER NOT NULL;
+
+ALTER TABLE awa_post ADD COLUMN `allow_comments` TINYINT NOT NULL;
+
+RENAME TABLE awa_comments TO awa_comment;
