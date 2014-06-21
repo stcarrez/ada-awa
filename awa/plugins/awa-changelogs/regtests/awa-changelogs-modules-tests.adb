d99604011ccd48b9acc4cf33198fad176a2f1e34
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-06-21 20:09:35 +0000

    Add a unit test for the Add_Log operation

---
+++ awa/plugins/awa-changelogs/regtests/awa-changelogs-modules-tests.adb
@@ -0,0 +1,52 @@
+-----------------------------------------------------------------------
+--  awa-changelogs-tests -- Tests for changelogs
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+with Util.Test_Caller;
+
+with Security.Contexts;
+with AWA.Services.Contexts;
+with AWA.Users.Models;
+with AWA.Tests.Helpers.Users;
+package body AWA.Changelogs.Modules.Tests is
+
+   package Caller is new Util.Test_Caller (Test, "AWA.Changelogs");
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
+   begin
+      Caller.Add_Test (Suite, "Test AWA.Changelogs.Add_Log",
+                       Test_Add_Log'Access);
+   end Add_Tests;
+
+   procedure Test_Add_Log (T : in out Test) is
+      Sec_Ctx      : Security.Contexts.Security_Context;
+      Context      : AWA.Services.Contexts.Service_Context;
+   begin
+      AWA.Tests.Helpers.Users.Login (Context, Sec_Ctx, "test-changelog@test.com");
+      declare
+         Change_Manager : constant Changelog_Module_Access := Get_Changelog_Module;
+         User           : constant AWA.Users.Models.User_Ref := Context.Get_User;
+         Total        : Integer;
+      begin
+         T.Assert (Change_Manager /= null, "There is no changelog module");
+
+         Change_Manager.Add_Log (User.Get_Id, "awa_user", "A first changelog for the user");
+      end;
+   end Test_Add_Log;
+
+end AWA.Changelogs.Modules.Tests;
---
+++ awa/plugins/awa-changelogs/regtests/awa-changelogs-modules-tests.ads
@@ -0,0 +1,28 @@
+-----------------------------------------------------------------------
+--  awa-changelogs-tests -- Tests for changelogs
+--  Copyright (C) 2014 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+package AWA.Changelogs.Modules.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   procedure Test_Add_Log (T : in out Test);
+
+end AWA.Changelogs.Modules.Tests;
