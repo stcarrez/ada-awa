c8ca5f68a3089de8ef89ac991337d34fe892b1d1
Stephane Carrez <Stephane.Carrez@gmail.com>
2014-06-21 20:14:59 +0000

    Update the SQL migration script

--- awa/db/migrate/awa-1.0-migrate.sql
+++ awa/db/migrate/awa-1.0-migrate.sql
@@ -13,3 +13,22 @@ ALTER TABLE awa_post ADD COLUMN `allow_comments` TINYINT NOT NULL;
 RENAME TABLE awa_comments TO awa_comment;
 
 ALTER TABLE awa_comment ADD COLUMN `format` INTEGER NOT NULL;
+
+CREATE TABLE awa_changelog (
+  /* the changelog identifier. */
+  `id` BIGINT NOT NULL,
+  /* the changelog date. */
+  `date` DATETIME NOT NULL,
+  /* the changelog text. */
+  `text` VARCHAR(255) BINARY NOT NULL,
+  /* the optional entity to which the changelog is associated. */
+  `for_entity_id` BIGINT NOT NULL,
+  /*  */
+  `user_id` BIGINT NOT NULL,
+  /*  */
+  `entity_type` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_changelog")
+;
