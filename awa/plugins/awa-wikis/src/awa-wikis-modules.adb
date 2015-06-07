19f83b0a5709ee2d9e53762e12faed92257a53c6
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-06-07 21:26:00 +0200

    Implement the Create_Wiki_Space procedure

--- awa/plugins/awa-wikis/src/awa-wikis-modules.adb
+++ awa/plugins/awa-wikis/src/awa-wikis-modules.adb
@@ -15,10 +15,17 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-
+with AWA.Services.Contexts;
+with AWA.Workspaces.Models;
+with AWA.Workspaces.Modules;
 with AWA.Modules.Get;
+with AWA.Permissions;
+with AWA.Permissions.Services;
+
+with ADO.Sessions;
+
 with Util.Log.Loggers;
-package body Awa.Wikis.Modules is
+package body AWA.Wikis.Modules is
 
    Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("Awa.Wikis.Module");
 
@@ -46,4 +53,71 @@ package body Awa.Wikis.Modules is
       return Get;
    end Get_Wiki_Module;
 
-end Awa.Wikis.Modules;
+   --  ------------------------------
+   --  Create the wiki space.
+   --  ------------------------------
+   procedure Create_Wiki_Space (Module : in Wiki_Module;
+                                Wiki   : in out AWA.Wikis.Models.Wiki_Space_Ref'Class) is
+      pragma Unreferenced (Module);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      User  : constant ADO.Identifier := Ctx.Get_User_Identifier;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+      WS    : AWA.Workspaces.Models.Workspace_Ref;
+   begin
+      Log.Info ("Creating new wiki space {0}", String '(Wiki.Get_Name));
+
+      Ctx.Start;
+      AWA.Workspaces.Modules.Get_Workspace (DB, Ctx, WS);
+
+      --  Check that the user has the create permission on the given workspace.
+      AWA.Permissions.Check (Permission => ACL_Create_Wiki_Space.Permission,
+                             Entity     => WS);
+      Wiki.Set_Workspace (WS);
+      Wiki.Save (DB);
+
+      --  Add the permission for the user to use the new wiki space.
+      AWA.Permissions.Services.Add_Permission (Session => DB,
+                                               User    => User,
+                                               Entity  => Wiki);
+      Ctx.Commit;
+
+      Log.Info ("Wiki {0} created for user {1}",
+                ADO.Identifier'Image (Wiki.Get_Id), ADO.Identifier'Image (User));
+   end Create_Wiki_Space;
+
+   --  ------------------------------
+   --  Save the wiki space.
+   --  ------------------------------
+   procedure Save_Wiki_Space (Module : in Wiki_Module;
+                              Wiki   : in out AWA.Wikis.Models.Wiki_Space_Ref'Class) is
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Master_Session := AWA.Services.Contexts.Get_Master_Session (Ctx);
+   begin
+      Log.Info ("Updating wiki space {0}", String '(Wiki.Get_Name));
+
+      Ctx.Start;
+
+      --  Check that the user has the update permission on the given wiki space.
+      AWA.Permissions.Check (Permission => ACL_Update_Wiki_Space.Permission,
+                             Entity     => Wiki);
+      Wiki.Save (DB);
+      Ctx.Commit;
+   end Save_Wiki_Space;
+
+   --  ------------------------------
+   --  Load the wiki space.
+   --  ------------------------------
+   procedure Load_Wiki_Space (Module : in Wiki_Module;
+                              Wiki   : in out AWA.Wikis.Models.Wiki_Space_Ref'Class;
+                              Id     : in ADO.Identifier) is
+      pragma Unreferenced (Module);
+
+      Ctx   : constant Services.Contexts.Service_Context_Access := AWA.Services.Contexts.Current;
+      DB    : ADO.Sessions.Session := AWA.Services.Contexts.Get_Session (Ctx);
+      Found : Boolean;
+   begin
+      Wiki.Load (DB, Id, Found);
+   end Load_Wiki_Space;
+
+end AWA.Wikis.Modules;
