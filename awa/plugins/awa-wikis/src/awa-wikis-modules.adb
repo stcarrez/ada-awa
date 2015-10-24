73f48661b9734dd7b62a88356d00f245a7de3c0a
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-10-24 16:24:26 +0200

    Register the Wiki_View_Bean handler

--- awa/plugins/awa-wikis/src/awa-wikis-modules.adb
+++ awa/plugins/awa-wikis/src/awa-wikis-modules.adb
@@ -68,6 +68,10 @@ package body AWA.Wikis.Modules is
                          Name   => "AWA.Wikis.Beans.Wiki_List_Bean",
                          Handler => AWA.Wikis.Beans.Create_Wiki_List_Bean'Access);
 
+      Register.Register (Plugin => Plugin,
+                         Name   => "AWA.Wikis.Beans.Wiki_View_Bean",
+                         Handler => AWA.Wikis.Beans.Create_Wiki_View_Bean'Access);
+
       AWA.Modules.Module (Plugin).Initialize (App, Props);
 
       --  Add here the creation of manager instances.
