3f9a53f96aea6441c8b3d2cd44135a17cec01d03
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-13 21:23:58 +0100

    - Add Element_Type generic type parameter - Define the Element_Type_Access type - Change the definition to use the Element_Type generic parameter instead of a String

--- awa/src/awa-index_arrays.ads
+++ awa/src/awa-index_arrays.ads
@@ -20,16 +20,23 @@ with Util.Strings;
 
 generic
    type Index_Type is range <>;
+   type Element_Type (<>) is private;
+   with function "=" (Left, Right : in Element_Type) return Boolean is <>;
+   with function "<" (Left, Right : in Element_Type) return Boolean is <>;
+   with function "&" (Left  : in String;
+                      Right : in Element_Type) return String is <>;
 package AWA.Index_Arrays is
 
    --  This package must be instantiated for each definition.
    --  It allocates a unique <tt>Index_Type</tt> value for each definition.
    generic
-      Name : String;
+      Name : Element_Type;
    package Definition is
       function Kind return Index_Type;
       pragma Inline_Always (Kind);
    end Definition;
+
+   type Element_Type_Access is access constant Element_Type;
 
    --  Exception raised if a name is not found.
    Not_Found     : exception;
@@ -39,10 +46,10 @@ package AWA.Index_Arrays is
 
    --  Find the runtime index given the name.
    --  Raises Not_Found exception if the name is not recognized.
-   function Find (Name : in String) return Index_Type;
+   function Find (Name : in Element_Type) return Index_Type;
 
-   --  Get the name associated with the index.
-   function Get_Name (Index : in Index_Type) return Util.Strings.Name_Access;
+   --  Get the element associated with the index.
+   function Get_Element (Index : in Index_Type) return Element_Type_Access;
 
    --  Check if the index is a valid index.
    function Is_Valid (Index : in Index_Type) return Boolean;
