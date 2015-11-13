d603fd0bb2519543a4f128dde437863325ef1c66
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-13 21:45:58 +0100

    Move the Counter_Arrays.Definition package instantiation in the private part

--- awa/plugins/awa-counters/src/awa-counters-definition.ads
+++ awa/plugins/awa-counters/src/awa-counters-definition.ads
@@ -24,6 +24,11 @@ package AWA.Counters.Definition is
 
    Def_Name : aliased constant String := Field;
 
+   --  Get the counter definition index.
+   function Index return Counter_Index_Type;
+
+private
+
    package Def is new Counter_Arrays.Definition (Counter_Def '(Table, Def_Name'Access));
 
    --  Get the counter definition index.
