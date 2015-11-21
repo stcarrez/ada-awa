4cb8bf1cae8997eeee3dde138a2579c1e1bd7d80
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-21 18:49:20 +0100

    Change the UICounter to use the UIHtmlComponent base type

---
+++ awa/plugins/awa-counters/src/awa-counters-components.ads
@@ -0,0 +1,42 @@
+-----------------------------------------------------------------------
+--  awa-counters-components -- Counter UI component
+--  Copyright (C) 2015 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with ASF.Factory;
+with ASF.Contexts.Faces;
+with ASF.Components.Html;
+
+--  == Counter Component ==
+--
+package AWA.Counters.Components is
+
+   type UICounter is new ASF.Components.Html.UIHtmlComponent with private;
+
+   --  Render the counter component.  Starts the DL/DD list and write the input
+   --  component with the possible associated error message.
+   overriding
+   procedure Encode_Begin (UI      : in UICounter;
+                           Context : in out ASF.Contexts.Faces.Faces_Context'Class);
+
+   --  Get the AWA Counter component factory.
+   function Definition return ASF.Factory.Factory_Bindings_Access;
+
+private
+
+   type UICounter is new ASF.Components.Html.UIHtmlComponent with null record;
+
+end AWA.Counters.Components;
