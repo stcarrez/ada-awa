e3e5c90ba726d8d051ea00d80e7c71cfa58f1bad
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-21 18:37:35 +0100

    Implement the Get_Value procedure for the Counter_Bean

---
+++ awa/plugins/awa-counters/src/awa-counters-beans.adb
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------
+--  awa-counters-beans -- Counter bean definition
+--  Copyright (C) 2015 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+package body AWA.Counters.Beans is
+
+   --  ------------------------------
+   --  Get the value identified by the name.
+   --  ------------------------------
+   overriding
+   function Get_Value (From : in Counter_Bean;
+                       Name : in String) return Util.Beans.Objects.Object is
+   begin
+      return Util.Beans.Objects.To_Object (From.Value);
+   end Get_Value;
+
+end AWA.Counters.Beans;
