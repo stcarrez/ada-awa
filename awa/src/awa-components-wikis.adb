42926dc2f924ae7a2287a922bfed2522578f6030
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-11-28 22:40:25 +0100

    Use the Ada Wiki library - implement the Html_Writer_Type to render the result in the ASF context writer - configure the HTML renderer

--- awa/src/awa-components-wikis.adb
+++ awa/src/awa-components-wikis.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-components-wikis -- Wiki rendering component
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2015 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,13 +19,146 @@
 with Util.Strings;
 with Util.Beans.Objects;
 
+with Ada.Strings.Wide_Wide_Unbounded;
 with ASF.Contexts.Writer;
 with ASF.Utils;
 
-with AWA.Wikis.Writers.Html;
+with Wiki.Render.Html;
+with Wiki.Writers;
 package body AWA.Components.Wikis is
 
+   use Ada.Strings.Wide_Wide_Unbounded;
+
    WIKI_ATTRIBUTE_NAMES  : Util.Strings.String_Set.Set;
+
+   type Html_Writer_Type is limited new Wiki.Writers.Html_Writer_Type with record
+      Writer : ASF.Contexts.Writer.Response_Writer_Access;
+   end record;
+   type Html_Writer_Type_Access is access all Html_Writer_Type'Class;
+
+   overriding
+   procedure Write (Writer  : in out Html_Writer_Type;
+                    Content : in Wide_Wide_String);
+
+   --  Write a single character to the string builder.
+   overriding
+   procedure Write (Writer : in out Html_Writer_Type;
+                    Char   : in Wide_Wide_Character);
+
+   overriding
+   procedure Write (Writer  : in out Html_Writer_Type;
+                    Content : in Unbounded_Wide_Wide_String);
+
+
+   --  Write an XML element using the given name and with the content.
+   --  This is similar to calling <b>Start_Element</b>, <b>Write_Text</b>
+   --  and <b>End_Element</b>.
+   overriding
+   procedure Write_Wide_Element (Writer  : in out Html_Writer_Type;
+                                 Name    : in String;
+                                 Content : in Unbounded_Wide_Wide_String);
+
+   --  Write an XML attribute within an XML element.
+   --  The attribute value is escaped according to the XML escape rules.
+   overriding
+   procedure Write_Wide_Attribute (Writer  : in out Html_Writer_Type;
+                                   Name    : in String;
+                                   Content : in Unbounded_Wide_Wide_String);
+
+   --  Start an XML element with the given name.
+   overriding
+   procedure Start_Element (Writer : in out Html_Writer_Type;
+                            Name   : in String);
+
+   --  Closes an XML element of the given name.
+   overriding
+   procedure End_Element (Writer : in out Html_Writer_Type;
+                          Name   : in String);
+
+   --  Write a text escaping any character as necessary.
+   overriding
+   procedure Write_Wide_Text (Writer  : in out Html_Writer_Type;
+                              Content : in Unbounded_Wide_Wide_String);
+
+   overriding
+   procedure Write (Writer  : in out Html_Writer_Type;
+                    Content : in Wide_Wide_String) is
+   begin
+      Writer.Writer.Write_Wide_Text (Content);
+   end Write;
+
+   --  ------------------------------
+   --  Write a single character to the string builder.
+   --  ------------------------------
+   overriding
+   procedure Write (Writer : in out Html_Writer_Type;
+                    Char   : in Wide_Wide_Character) is
+   begin
+      Writer.Writer.Write_Wide_Char (Char);
+   end Write;
+
+   overriding
+   procedure Write (Writer  : in out Html_Writer_Type;
+                    Content : in Unbounded_Wide_Wide_String) is
+   begin
+      Writer.Writer.Write (Content);
+   end Write;
+
+
+   --  ------------------------------
+   --  Write an XML element using the given name and with the content.
+   --  This is similar to calling <b>Start_Element</b>, <b>Write_Text</b>
+   --  and <b>End_Element</b>.
+   --  ------------------------------
+   overriding
+   procedure Write_Wide_Element (Writer  : in out Html_Writer_Type;
+                                 Name    : in String;
+                                 Content : in Unbounded_Wide_Wide_String) is
+   begin
+      Writer.Writer.Write_Wide_Element (Name, Content);
+   end Write_Wide_Element;
+
+   --  ------------------------------
+   --  Write an XML attribute within an XML element.
+   --  The attribute value is escaped according to the XML escape rules.
+   --  ------------------------------
+   overriding
+   procedure Write_Wide_Attribute (Writer  : in out Html_Writer_Type;
+                                   Name    : in String;
+                                   Content : in Unbounded_Wide_Wide_String) is
+   begin
+      Writer.Writer.Write_Wide_Attribute (Name, Content);
+   end Write_Wide_Attribute;
+
+   --  ------------------------------
+   --  Start an XML element with the given name.
+   --  ------------------------------
+   overriding
+   procedure Start_Element (Writer : in out Html_Writer_Type;
+                            Name   : in String) is
+   begin
+      Writer.Writer.Start_Element (Name);
+   end Start_Element;
+
+   --  ------------------------------
+   --  Closes an XML element of the given name.
+   --  ------------------------------
+   overriding
+   procedure End_Element (Writer : in out Html_Writer_Type;
+                          Name   : in String) is
+   begin
+      Writer.Writer.End_Element (Name);
+   end End_Element;
+
+   --  ------------------------------
+   --  Write a text escaping any character as necessary.
+   --  ------------------------------
+   overriding
+   procedure Write_Wide_Text (Writer  : in out Html_Writer_Type;
+                              Content : in Unbounded_Wide_Wide_String) is
+   begin
+      Writer.Writer.Write_Wide_Text (Content);
+   end Write_Wide_Text;
 
    --  ------------------------------
    --  Get the wiki format style.  The format style is obtained from the <b>format</b>
@@ -33,23 +166,23 @@ package body AWA.Components.Wikis is
    --  ------------------------------
    function Get_Wiki_Style (UI      : in UIWiki;
                             Context : in Faces_Context'Class)
-                            return AWA.Wikis.Parsers.Wiki_Syntax_Type is
+                            return Wiki.Parsers.Wiki_Syntax_Type is
       Format : constant String := UI.Get_Attribute (Name    => FORMAT_NAME,
                                                     Context => Context,
                                                     Default => "dotclear");
    begin
       if Format = "dotclear" then
-         return AWA.Wikis.Parsers.SYNTAX_DOTCLEAR;
+         return Wiki.Parsers.SYNTAX_DOTCLEAR;
       elsif Format = "google" then
-         return AWA.Wikis.Parsers.SYNTAX_GOOGLE;
+         return Wiki.Parsers.SYNTAX_GOOGLE;
       elsif Format = "phpbb" then
-         return AWA.Wikis.Parsers.SYNTAX_PHPBB;
+         return Wiki.Parsers.SYNTAX_PHPBB;
       elsif Format = "creole" then
-         return AWA.Wikis.Parsers.SYNTAX_CREOLE;
+         return Wiki.Parsers.SYNTAX_CREOLE;
       elsif Format = "mediawiki" then
-         return AWA.Wikis.Parsers.SYNTAX_MEDIA_WIKI;
+         return Wiki.Parsers.SYNTAX_MEDIA_WIKI;
       else
-         return AWA.Wikis.Parsers.SYNTAX_MIX;
+         return Wiki.Parsers.SYNTAX_MIX;
       end if;
    end Get_Wiki_Style;
 
@@ -66,18 +199,20 @@ package body AWA.Components.Wikis is
       end if;
       declare
          Writer   : constant Response_Writer_Access := Context.Get_Response_Writer;
-         Html     : aliased AWA.Wikis.Writers.Html.Html_Writer;
-         Format   : constant AWA.Wikis.Parsers.Wiki_Syntax_Type := UI.Get_Wiki_Style (Context);
+         Html     : aliased Html_Writer_Type;
+         Renderer : aliased Wiki.Render.Html.Html_Renderer;
+         Format   : constant Wiki.Parsers.Wiki_Syntax_Type := UI.Get_Wiki_Style (Context);
          Value    : constant Util.Beans.Objects.Object := UI.Get_Attribute (Context, VALUE_NAME);
       begin
+         Html.Writer := Writer;
          Writer.Start_Element ("div");
          UI.Render_Attributes (Context, WIKI_ATTRIBUTE_NAMES, Writer);
 
          if not Util.Beans.Objects.Is_Empty (Value) then
-            Html.Set_Writer (Writer);
-            AWA.Wikis.Parsers.Parse (Html'Unchecked_Access,
-                                     Util.Beans.Objects.To_Wide_Wide_String (Value),
-                                     Format);
+            Renderer.Set_Writer (Html'Unchecked_Access);
+            Wiki.Parsers.Parse (Renderer'Unchecked_Access,
+                                Util.Beans.Objects.To_Wide_Wide_String (Value),
+                                Format);
          end if;
          Writer.End_Element ("div");
       end;
