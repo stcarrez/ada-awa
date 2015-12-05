5f00cb7edbd25b48c632503d26619908377e4c22
Stephane Carrez <Stephane.Carrez@gmail.com>
2015-12-05 17:07:26 +0100

    Rebuild the SQL model files

--- awa/samples/db/mysql/create-atlas-mysql.sql
+++ awa/samples/db/mysql/create-atlas-mysql.sql
@@ -185,6 +185,104 @@ INSERT INTO entity_type (name) VALUES
 ,("awa_session")
 ,("awa_user")
 ;
+/* Copied from awa-workspaces-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The workspace controls the features available in the application
+for a set of users: the workspace members.  A user could create
+several workspaces and be part of several workspaces that other
+users have created. */
+CREATE TABLE awa_workspace (
+  /* the workspace identifier */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /*  */
+  `create_date` DATETIME NOT NULL,
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/*  */
+CREATE TABLE awa_workspace_feature (
+  /*  */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `limit` INTEGER NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* The workspace member indicates the users who
+are part of the workspace. */
+CREATE TABLE awa_workspace_member (
+  /*  */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `member_id` BIGINT NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_workspace")
+,("awa_workspace_feature")
+,("awa_workspace_member")
+;
+/* Copied from awa-tags-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The tag definition. */
+CREATE TABLE awa_tag (
+  /* the tag identifier */
+  `id` BIGINT NOT NULL,
+  /* the tag name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/*  */
+CREATE TABLE awa_tagged_entity (
+  /* the tag entity identifier */
+  `id` BIGINT NOT NULL,
+  /* Title: Tag model
+Date: 2013-02-23the database entity to which the tag is associated */
+  `for_entity_id` BIGINT NOT NULL,
+  /* the entity type */
+  `entity_type` INTEGER NOT NULL,
+  /*  */
+  `tag_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_tag")
+,("awa_tagged_entity")
+;
+/* Copied from awa-comments-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The Comment table records a user comment associated with a database entity.
+The comment can be associated with any other database record. */
+CREATE TABLE awa_comment (
+  /* the comment publication date */
+  `create_date` DATETIME NOT NULL,
+  /* the comment message. */
+  `message` TEXT NOT NULL,
+  /* the entity identifier to which this comment is associated */
+  `entity_id` BIGINT NOT NULL,
+  /* the comment identifier */
+  `id` BIGINT NOT NULL,
+  /* the optimistic lock version. */
+  `version` INTEGER NOT NULL,
+  /* the entity type that identifies the table to which the comment is associated. */
+  `entity_type` INTEGER NOT NULL,
+  /* the comment status to decide whether the comment is visible (published) or not. */
+  `status` integer NOT NULL,
+  /* the comment format type. */
+  `format` integer NOT NULL,
+  /*  */
+  `author_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_comment")
+;
 /* Copied from awa_counters-mysql.sql*/
 /* File generated automatically by dynamo */
 /*  */
@@ -214,6 +312,398 @@ CREATE TABLE awa_counter_definition (
 INSERT INTO entity_type (name) VALUES
 ("awa_counter")
 ,("awa_counter_definition")
+;
+/* Copied from awa-blogs-mysql.sql*/
+/* File generated automatically by dynamo */
+/*  */
+CREATE TABLE awa_blog (
+  /* the blog identifier */
+  `id` BIGINT NOT NULL,
+  /* the blog name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /* the version */
+  `version` INTEGER NOT NULL,
+  /* the blog uuid */
+  `uid` VARCHAR(255) BINARY NOT NULL,
+  /* the blog creation date */
+  `create_date` DATETIME NOT NULL,
+  /* the date when the blog was updated */
+  `update_date` DATETIME NOT NULL,
+  /* The blog base URL. */
+  `url` VARCHAR(255) BINARY NOT NULL,
+  /* the workspace that this blog belongs to */
+  `workspace_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/*  */
+CREATE TABLE awa_post (
+  /* the post identifier */
+  `id` BIGINT NOT NULL,
+  /* the post title */
+  `title` VARCHAR(255) BINARY NOT NULL,
+  /* the post text content */
+  `text` TEXT NOT NULL,
+  /* the post creation date */
+  `create_date` DATETIME NOT NULL,
+  /* the post URI */
+  `uri` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the post publication date */
+  `publish_date` DATETIME ,
+  /* the post status */
+  `status` TINYINT NOT NULL,
+  /*  */
+  `allow_comments` TINYINT NOT NULL,
+  /* the number of times the post was read. */
+  `read_count` INTEGER NOT NULL,
+  /*  */
+  `author_id` BIGINT NOT NULL,
+  /*  */
+  `blog_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_blog")
+,("awa_post")
+;
+/* Copied from awa-storages-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The uri member holds the URI if the storage type is URL.
+
+When storage is FILE, the local file path is built by using
+the workspace identifier and the storage identifier. */
+CREATE TABLE awa_storage (
+  /* the storage type which defines where the content is stored */
+  `storage` TINYINT NOT NULL,
+  /* the storage creation date */
+  `create_date` DATETIME NOT NULL,
+  /* the file name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /* the file size */
+  `file_size` INTEGER NOT NULL,
+  /* the mime type */
+  `mime_type` VARCHAR(255) BINARY NOT NULL,
+  /* the storage URI */
+  `uri` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the storage identifier */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `original_id` BIGINT ,
+  /*  */
+  `store_data_id` BIGINT ,
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  /*  */
+  `folder_id` BIGINT ,
+  PRIMARY KEY (`id`)
+);
+/* The storage data is created only if the storage type
+is set to DATABASE.  It holds the file content in the blob. */
+CREATE TABLE awa_storage_data (
+  /* the storage data identifier */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the storage content */
+  `data` LONGBLOB NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/*  */
+CREATE TABLE awa_storage_folder (
+  /* the storage folder identifier */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the folder creation date */
+  `create_date` DATETIME NOT NULL,
+  /*  */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* The local store record is created when a copy of the data is needed on the local file system.
+The creation date refers to the date when the data was copied to the local file system.
+The expiration date indicates a date after which the local file can be removed
+from the local file system. */
+CREATE TABLE awa_store_local (
+  /* the local store identifier */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /*  */
+  `store_version` INTEGER NOT NULL,
+  /* the shared flag which indicates whether this local store can be shared by several clients. */
+  `shared` TINYINT NOT NULL,
+  /* the local store path */
+  `path` VARCHAR(255) BINARY NOT NULL,
+  /* the local store expiration date */
+  `expire_date` DATE NOT NULL,
+  /* the creation date */
+  `create_date` DATETIME NOT NULL,
+  /*  */
+  `storage_id` BIGINT ,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_storage")
+,("awa_storage_data")
+,("awa_storage_folder")
+,("awa_store_local")
+;
+/* Copied from awa-images-mysql.sql*/
+/* File generated automatically by dynamo */
+/* - The workspace contains one or several folders.
+- Each image folder contains a set of images that have been uploaded by the user.
+- An image can be visible if a user has an ACL permission to read the associated folder.
+- An image marked as 'public=True' can be visible by anybody
+ */
+CREATE TABLE awa_image (
+  /* the image identifier */
+  `id` BIGINT NOT NULL,
+  /* the image width */
+  `width` INTEGER NOT NULL,
+  /* the image height */
+  `height` INTEGER NOT NULL,
+  /* the thumbnail width */
+  `thumb_width` INTEGER NOT NULL,
+  /* the thumbnail height */
+  `thumb_height` INTEGER NOT NULL,
+  /*  */
+  `path` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `public` TINYINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /*  */
+  `thumbnail_id` BIGINT ,
+  /*  */
+  `folder_id` BIGINT NOT NULL,
+  /*  */
+  `owner_id` BIGINT NOT NULL,
+  /*  */
+  `storage_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_image")
+;
+/* Copied from awa-questions-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The answer table gives a list of anwsers to the question.
+Ranking is updating according to users voting for the anwser.
+ */
+CREATE TABLE awa_answer (
+  /* the answer creation date. */
+  `create_date` DATETIME NOT NULL,
+  /* the date when the answer was edited. */
+  `edit_date` DATETIME ,
+  /* the answer text. */
+  `answer` TEXT NOT NULL,
+  /* the anwser rank number. */
+  `rank` INTEGER NOT NULL,
+  /* the answer identifier. */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the user who wrote the answer. */
+  `author_id` BIGINT NOT NULL,
+  /*  */
+  `question_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* The question table holds a single question asked by a user to the community.
+The short description is used to give an overview of the question in long lists
+while the description contains the full question text.  The rating is updating
+according to users voting for the question. */
+CREATE TABLE awa_question (
+  /* the date when the question was created. */
+  `create_date` DATETIME NOT NULL,
+  /* the question title. */
+  `title` VARCHAR(255) BINARY NOT NULL,
+  /* the full description. */
+  `description` TEXT NOT NULL,
+  /* the date when the question was edited. */
+  `edit_date` DATETIME ,
+  /* Title: Questions and Answers model
+Date: 2015-11-15
+the question short description. */
+  `short_description` VARCHAR(255) BINARY NOT NULL,
+  /* the question rating. */
+  `rating` INTEGER NOT NULL,
+  /* the question identifier. */
+  `id` BIGINT NOT NULL,
+  /* the optimistic locking version. */
+  `version` INTEGER NOT NULL,
+  /* the user who asked the question. */
+  `author_id` BIGINT NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  /*  */
+  `accepted_answer_id` BIGINT ,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_answer")
+,("awa_question")
+;
+/* Copied from awa-votes-mysql.sql*/
+/* File generated automatically by dynamo */
+/*  */
+CREATE TABLE awa_rating (
+  /* the rating identifier */
+  `id` BIGINT NOT NULL,
+  /* the rating taking into account all votes */
+  `rating` INTEGER NOT NULL,
+  /* the number of votes */
+  `vote_count` INTEGER NOT NULL,
+  /*  */
+  `for_entity_id` BIGINT NOT NULL,
+  /* the entity type */
+  `for_entity_type` INTEGER NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* The vote table tracks a vote action by a user on a given database entity.
+The primary key is made of the user, the entity id and entity type.
+ */
+CREATE TABLE awa_vote (
+  /*  */
+  `rating` INTEGER NOT NULL,
+  /*  */
+  `id` BIGINT NOT NULL,
+  /*  */
+  `entity_id` BIGINT NOT NULL,
+  /*  */
+  `user_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_rating")
+,("awa_vote")
+;
+/* Copied from awa-jobs-mysql.sql*/
+/* File generated automatically by dynamo */
+/* The job is associated with a dispatching queue. */
+CREATE TABLE awa_job (
+  /* the job identifier */
+  `id` BIGINT NOT NULL,
+  /* the job status */
+  `status` TINYINT NOT NULL,
+  /* the job name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /* the job start date */
+  `start_date` DATETIME ,
+  /* the job creation date */
+  `create_date` DATETIME NOT NULL,
+  /* the job finish date */
+  `finish_date` DATETIME ,
+  /* the job progress indicator */
+  `progress` INTEGER NOT NULL,
+  /* the job parameters */
+  `parameters` TEXT NOT NULL,
+  /* the job result */
+  `results` TEXT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the job priority */
+  `priority` INTEGER NOT NULL,
+  /*  */
+  `user_id` BIGINT ,
+  /*  */
+  `event_id` BIGINT ,
+  /*  */
+  `session_id` BIGINT ,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_job")
+;
+/* Copied from awa-wikis-mysql.sql*/
+/* File generated automatically by dynamo */
+/*  */
+CREATE TABLE awa_wiki_content (
+  /* the wiki page content identifier */
+  `id` BIGINT NOT NULL,
+  /* the wiki content creation date */
+  `create_date` DATETIME NOT NULL,
+  /* the wiki text content */
+  `content` TEXT NOT NULL,
+  /* the format type used used by the wiki content */
+  `format` TINYINT NOT NULL,
+  /* the content comment string */
+  `save_comment` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the wiki page version */
+  `page_version` INTEGER NOT NULL,
+  /* the wiki page that this Wiki_Content belongs to */
+  `page_id` BIGINT NOT NULL,
+  /* the page version author */
+  `author_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+/* The wiki page represents a page with its versions.
+It refers to the last version which is currently visible.
+It has an optional preview image which defines
+the thumbnail preview of the last/current wiki content. */
+CREATE TABLE awa_wiki_page (
+  /* the wiki page identifier */
+  `id` BIGINT NOT NULL,
+  /* the wiki page name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /* the last page version number */
+  `last_version` INTEGER NOT NULL,
+  /* whether the wiki page is public */
+  `is_public` TINYINT NOT NULL,
+  /* the page title */
+  `title` VARCHAR(255) BINARY NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* a read counter which indicates how many times the page was read. */
+  `read_count` INTEGER NOT NULL,
+  /* the wiki page preview. */
+  `preview_id` BIGINT ,
+  /* the wiki space that this page belongs to */
+  `wiki_id` BIGINT NOT NULL,
+  /* the current content (or last version) */
+  `content_id` BIGINT ,
+  PRIMARY KEY (`id`)
+);
+/* Permission is granted to display a wiki page if there is
+an ACL entry between the wiki space and the user. */
+CREATE TABLE awa_wiki_space (
+  /* the wiki space identifier */
+  `id` BIGINT NOT NULL,
+  /* the wiki name */
+  `name` VARCHAR(255) BINARY NOT NULL,
+  /* whether the wiki is public */
+  `is_public` TINYINT NOT NULL,
+  /*  */
+  `version` INTEGER NOT NULL,
+  /* the wiki creation date. */
+  `create_date` DATETIME NOT NULL,
+  /* the left panel side wiki text for every page. */
+  `left_side` TEXT NOT NULL,
+  /* the right panel wiki text for every page.
+ */
+  `right_side` TEXT NOT NULL,
+  /* the default wiki page format. */
+  `format` TINYINT NOT NULL,
+  /*  */
+  `workspace_id` BIGINT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+INSERT INTO entity_type (name) VALUES
+("awa_wiki_content")
+,("awa_wiki_page")
+,("awa_wiki_space")
 ;
 /* Copied from atlas-mysql.sql*/
 /* File generated automatically by dynamo */
