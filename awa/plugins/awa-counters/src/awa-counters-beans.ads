bfb23be38fd9582b3a9c4c8d0ddf6e0ca8471300
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-05-05 11:11:34 +0200

    Declare the Counter_Stat_Bean type with Get_Value, Set_Value, Load operations

--- awa/plugins/awa-counters/src/awa-counters-beans.ads
+++ awa/plugins/awa-counters/src/awa-counters-beans.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-counters-beans -- Counter bean definition
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2015, 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,10 +15,15 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+
 with ADO.Objects;
 with ADO.Schemas;
 with Util.Beans.Objects;
 with Util.Beans.Basic;
+
+with AWA.Counters.Modules;
+with AWA.Counters.Models;
 
 --  == Counter Bean ==
 --  The <b>Counter_Bean</b> allows to represent a counter associated with some database
@@ -39,5 +44,32 @@ package AWA.Counters.Beans is
    overriding
    function Get_Value (From : in Counter_Bean;
                        Name : in String) return Util.Beans.Objects.Object;
+
+   type Counter_Stat_Bean is new AWA.Counters.Models.Stat_List_Bean with record
+      Module           : AWA.Counters.Modules.Counter_Module_Access;
+
+      Stats            : aliased AWA.Counters.Models.Stat_Info_List_Bean;
+      Stats_Bean       : AWA.Counters.Models.Stat_Info_List_Bean_Access;
+   end record;
+   type Counter_Stat_Bean_Access is access all Counter_Stat_Bean'Class;
+
+   overriding
+   function Get_Value (List : in Counter_Stat_Bean;
+                       Name : in String) return Util.Beans.Objects.Object;
+
+   --  Set the value identified by the name.
+   overriding
+   procedure Set_Value (From  : in out Counter_Stat_Bean;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object);
+
+   --  Load the statistics information.
+   overriding
+   procedure Load (List    : in out Counter_Stat_Bean;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String);
+
+   --  Create the Blog_Stat_Bean bean instance.
+   function Create_Counter_Stat_Bean (Module : in AWA.Counters.Modules.Counter_Module_Access)
+                                      return Util.Beans.Basic.Readonly_Bean_Access;
 
 end AWA.Counters.Beans;
