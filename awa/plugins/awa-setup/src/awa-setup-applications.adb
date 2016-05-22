fdff92e24cd3e6f08713ba7a690dc27159cf6929
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-05-22 14:36:44 +0200

    Setup application to help in the installation and configuration process

---
+++ awa/plugins/awa-setup/src/awa-setup-applications.adb
@@ -0,0 +1,180 @@
+-----------------------------------------------------------------------
+--  awa-setup -- Setup and installation
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Text_IO;
+with Ada.IO_Exceptions;
+with Util.Files;
+with Util.Log.Loggers;
+with Util.Strings;
+with ASF.Events.Faces.Actions;
+with ASF.Applications.Main.Configs;
+with AWA.Applications;
+package body AWA.Setup.Applications is
+
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("AWA.Setup.Applications");
+
+   package Save_Binding is
+     new ASF.Events.Faces.Actions.Action_Method.Bind (Bean   => Application,
+                                                      Method => Save,
+                                                      Name   => "save");
+
+   package Finish_Binding is
+     new ASF.Events.Faces.Actions.Action_Method.Bind (Bean   => Application,
+                                                      Method => Finish,
+                                                      Name   => "finish");
+   package Configure_Binding is
+     new ASF.Events.Faces.Actions.Action_Method.Bind (Bean   => Application,
+                                                      Method => Configure_Database,
+                                                      Name   => "configure_database");
+
+
+   Binding_Array : aliased constant Util.Beans.Methods.Method_Binding_Array
+     := (Save_Binding.Proxy'Access,
+         Finish_Binding.Proxy'Access,
+         Configure_Binding.Proxy'Access);
+
+   --  Get the value identified by the name.
+   function Get_Value (From : in Application;
+                       Name : in String) return Util.Beans.Objects.Object is
+   begin
+      if Name = "database" then
+         return Util.Beans.Objects.To_Object (From.Database.Get_Database);
+      elsif Name = "database_server" then
+         return Util.Beans.Objects.To_Object (From.Database.Get_Server);
+      elsif Name = "database_port" then
+         return Util.Beans.Objects.To_Object (From.Database.Get_Port);
+      end if;
+      if From.Changed.Exists (Name) then
+         return Util.Beans.Objects.To_Object (String '(From.Changed.Get (Name)));
+      end if;
+      declare
+         Param : constant String := From.Config.Get (Name);
+      begin
+         return Util.Beans.Objects.To_Object (Param);
+      end;
+
+   exception
+      when others =>
+         return Util.Beans.Objects.Null_Object;
+   end Get_Value;
+
+   --  Set the value identified by the name.
+   procedure Set_Value (From  : in out Application;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object) is
+   begin
+      From.Changed.Set (Name, Util.Beans.Objects.To_String (Value));
+   end Set_Value;
+
+   --  Configure the database.
+   procedure Configure_Database (From    : in out Application;
+                                 Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      Log.Info ("Configure database");
+   end Configure_Database;
+
+   --  Save the configuration.
+   procedure Save (From    : in out Application;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+      Path     : constant String := Ada.Strings.Unbounded.To_String (From.Path);
+      New_File : constant String := Path & ".tmp";
+      Output   : Ada.Text_IO.File_Type;
+
+      procedure Read_Property (Line : in String) is
+         Pos : constant Natural := Util.Strings.Index (Line, '=');
+      begin
+         if Pos = 0 or else not From.Changed.Exists (Line (Line'First .. Pos - 1)) then
+            Ada.Text_IO.Put_Line (Line);
+            return;
+         end if;
+         Ada.Text_IO.Put (Line (Line'First .. Pos));
+         Ada.Text_IO.Put_Line (From.Changed.Get (Line (Line'First .. Pos - 1)));
+      end Read_Property;
+
+   begin
+      Log.Info ("Saving configuration file {0}", Path);
+      Ada.Text_IO.Create (File => Output, Name => Path);
+      Util.Files.Read_File (Path, Read_Property'Access);
+      Ada.Text_IO.Close (Output);
+   end Save;
+
+   --  Finish the setup and exit the setup.
+   procedure Finish (From    : in out Application;
+                     Outcome : in out Ada.Strings.Unbounded.Unbounded_String) is
+   begin
+      Log.Info ("Finish configuration");
+      From.Done := True;
+   end Finish;
+
+   --  This bean provides some methods that can be used in a Method_Expression
+   overriding
+   function Get_Method_Bindings (From : in Application)
+                                 return Util.Beans.Methods.Method_Binding_Array_Access is
+   begin
+      return Binding_Array'Access;
+   end Get_Method_Bindings;
+
+   --  Enter in the application setup
+   procedure Setup (App    : in out Application;
+                    Config : in String;
+                    Server : in out ASF.Server.Container'Class) is
+   begin
+      Log.Info ("Entering configuration for {0}", Config);
+      App.Path := Ada.Strings.Unbounded.To_Unbounded_String (Config);
+      begin
+         App.Config.Load_Properties (Config);
+         Util.Log.Loggers.Initialize (Config);
+
+      exception
+         when Ada.IO_Exceptions.Name_Error =>
+            Log.Error ("Cannot read application configuration file: {0}", Config);
+      end;
+      App.Initialize (App.Config, App.Factory);
+      App.Set_Global ("contextPath", App.Config.Get ("contextPath"));
+      App.Set_Global ("setup",
+                      Util.Beans.Objects.To_Object (App'Unchecked_Access,
+                        Util.Beans.Objects.STATIC));
+      App.Add_Servlet (Name   => "faces",
+                       Server => App.Faces'Unchecked_Access);
+      App.Add_Servlet (Name   => "files",
+                       Server => App.Files'Unchecked_Access);
+      App.Add_Mapping (Pattern => "*.html",
+                       Name    => "faces");
+      App.Add_Mapping (Pattern => "*.css",
+                       Name    => "files");
+      App.Add_Mapping (Pattern => "*.js",
+                       Name    => "files");
+
+      declare
+         Paths : constant String := App.Get_Config (AWA.Applications.P_Module_Dir.P);
+         Path  : constant String := Util.Files.Find_File_Path ("setup.xml", Paths);
+      begin
+         ASF.Applications.Main.Configs.Read_Configuration (App, Path);
+
+      exception
+         when Ada.IO_Exceptions.Name_Error =>
+            Log.Error ("Setup configuration file '{0}' does not exist", Path);
+      end;
+      App.Database.Set_Connection (App.Config.Get ("database", "mysql://localhost:3306/db"));
+      Server.Register_Application (App.Config.Get ("contextPath"), App'Unchecked_Access);
+      Server.Start;
+      while not App.Done loop
+         delay 5.0;
+      end loop;
+   end Setup;
+
+end AWA.Setup.Applications;
---
+++ awa/plugins/awa-setup/src/awa-setup-applications.ads
@@ -0,0 +1,74 @@
+-----------------------------------------------------------------------
+--  awa-setup -- Setup and installation
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Ada.Strings.Unbounded;
+with ASF.Applications.Main;
+with ASF.Servlets.Faces;
+with ASF.Servlets.Files;
+with ASF.Server;
+with Util.Beans.Objects;
+with Util.Beans.Basic;
+with Util.Beans.Methods;
+with ADO.Drivers.Connections;
+package AWA.Setup.Applications is
+
+   type Application is new ASF.Applications.Main.Application and Util.Beans.Basic.Bean
+     and Util.Beans.Methods.Method_Bean with record
+      Faces    : aliased ASF.Servlets.Faces.Faces_Servlet;
+      Files    : aliased ASF.Servlets.Files.File_Servlet;
+      Config   : ASF.Applications.Config;
+      Changed  : ASF.Applications.Config;
+      Factory  : ASF.Applications.Main.Application_Factory;
+      Path     : Ada.Strings.Unbounded.Unbounded_String;
+      Database : ADO.Drivers.Connections.Configuration;
+      Done     : Boolean := False;
+      pragma Atomic (Done);
+      pragma Volatile (Done);
+   end record;
+
+   --  Get the value identified by the name.
+   function Get_Value (From : in Application;
+                       Name : in String) return Util.Beans.Objects.Object;
+
+   --  Set the value identified by the name.
+   procedure Set_Value (From  : in out Application;
+                        Name  : in String;
+                        Value : in Util.Beans.Objects.Object);
+
+   --  Configure the database.
+   procedure Configure_Database (From    : in out Application;
+                                 Outcome : in out Ada.Strings.Unbounded.Unbounded_String);
+
+   --  Save the configuration.
+   procedure Save (From    : in out Application;
+                   Outcome : in out Ada.Strings.Unbounded.Unbounded_String);
+
+   --  Finish the setup and exit the setup.
+   procedure Finish (From    : in out Application;
+                     Outcome : in out Ada.Strings.Unbounded.Unbounded_String);
+
+   --  This bean provides some methods that can be used in a Method_Expression
+   overriding
+   function Get_Method_Bindings (From : in Application)
+                                 return Util.Beans.Methods.Method_Binding_Array_Access;
+
+   --  Enter in the application setup
+   procedure Setup (App    : in out Application;
+                    Config : in String;
+                    Server : in out ASF.Server.Container'Class);
+
+end AWA.Setup.Applications;
