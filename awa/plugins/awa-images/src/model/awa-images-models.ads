b1ac701c7f1c6c3cb8cf0fe6a4975d84c8c999f2
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-06-11 18:07:33 +0200

    Rebuild the model files

--- awa/plugins/awa-images/src/model/awa-images-models.ads
+++ awa/plugins/awa-images/src/model/awa-images-models.ads
@@ -5,7 +5,7 @@
 --  Template used: templates/model/package-spec.xhtml
 --  Ada Generator: https://ada-gen.googlecode.com/svn/trunk Revision 1095
 -----------------------------------------------------------------------
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -324,7 +324,7 @@ private
       := IMAGE_DEF'Access;
 
    Null_Image : constant Image_Ref
-      := Image_Ref'(ADO.Objects.Object_Ref with others => <>);
+      := Image_Ref'(ADO.Objects.Object_Ref with null record);
 
    type Image_Impl is
       new ADO.Objects.Object_Record (Key_Type => ADO.Objects.KEY_INTEGER,
