9a53a78f25f8d81b51504304ab9c0241cc33d0ee
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-06-11 18:20:28 +0200

    Rebuild (fix compilation warning)

--- awa/plugins/awa-comments/src/model/awa-comments-models.ads
+++ awa/plugins/awa-comments/src/model/awa-comments-models.ads
@@ -5,7 +5,7 @@
 --  Template used: templates/model/package-spec.xhtml
 --  Ada Generator: https://ada-gen.googlecode.com/svn/trunk Revision 1095
 -----------------------------------------------------------------------
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -313,7 +313,7 @@ private
       := COMMENT_DEF'Access;
 
    Null_Comment : constant Comment_Ref
-      := Comment_Ref'(ADO.Objects.Object_Ref with others => <>);
+      := Comment_Ref'(ADO.Objects.Object_Ref with null record);
 
    type Comment_Impl is
       new ADO.Objects.Object_Record (Key_Type => ADO.Objects.KEY_INTEGER,
