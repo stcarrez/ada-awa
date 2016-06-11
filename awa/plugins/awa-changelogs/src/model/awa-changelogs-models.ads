85c3ff8909b6fab89baff98b19795a84b7c23dae
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-06-11 18:20:43 +0200

    Rebuild (fix compilation warning)

--- awa/plugins/awa-changelogs/src/model/awa-changelogs-models.ads
+++ awa/plugins/awa-changelogs/src/model/awa-changelogs-models.ads
@@ -5,7 +5,7 @@
 --  Template used: templates/model/package-spec.xhtml
 --  Ada Generator: https://ada-gen.googlecode.com/svn/trunk Revision 1095
 -----------------------------------------------------------------------
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2016 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -175,7 +175,7 @@ private
       := CHANGELOG_DEF'Access;
 
    Null_Changelog : constant Changelog_Ref
-      := Changelog_Ref'(ADO.Objects.Object_Ref with others => <>);
+      := Changelog_Ref'(ADO.Objects.Object_Ref with null record);
 
    type Changelog_Impl is
       new ADO.Objects.Object_Record (Key_Type => ADO.Objects.KEY_INTEGER,
