6aa026b92055224482ea1d1cb4c3197992dc40ce
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2016-08-05 17:23:40 +0200

    Rebuild the model files

--- awa/plugins/awa-counters/src/model/awa-counters-models.adb
+++ awa/plugins/awa-counters/src/model/awa-counters-models.adb
@@ -32,7 +32,7 @@ package body AWA.Counters.Models is
    pragma Warnings (Off, "formal parameter * is not referenced");
 
    function Counter_Key (Id : in ADO.Identifier) return ADO.Objects.Object_Key is
-      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_STRING,
+      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_INTEGER,
                                        Of_Class => COUNTER_DEF'Access);
    begin
       ADO.Objects.Set_Value (Result, Id);
@@ -40,7 +40,7 @@ package body AWA.Counters.Models is
    end Counter_Key;
 
    function Counter_Key (Id : in String) return ADO.Objects.Object_Key is
-      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_STRING,
+      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_INTEGER,
                                        Of_Class => COUNTER_DEF'Access);
    begin
       ADO.Objects.Set_Value (Result, Id);
@@ -127,19 +127,19 @@ package body AWA.Counters.Models is
 
 
    procedure Set_Definition_Id (Object : in out Counter_Ref;
-                                Value  : in Integer) is
+                                Value  : in ADO.Identifier) is
       Impl : Counter_Access;
    begin
       Set_Field (Object, Impl);
-      ADO.Objects.Set_Field_Key_Value (Impl.all, 4, ADO.Identifier (Value));
+      ADO.Objects.Set_Field_Key_Value (Impl.all, 4, Value);
    end Set_Definition_Id;
 
    function Get_Definition_Id (Object : in Counter_Ref)
-                  return Integer is
+                  return ADO.Identifier is
       Impl : constant Counter_Access
          := Counter_Impl (Object.Get_Object.all)'Access;
    begin
-      return Integer (ADO.Identifier '(Impl.Get_Key_Value));
+      return Impl.Get_Key_Value;
    end Get_Definition_Id;
 
    --  Copy of the object.
@@ -181,7 +181,7 @@ package body AWA.Counters.Models is
 
    procedure Load (Object  : in out Counter_Ref;
                    Session : in out ADO.Sessions.Session'Class;
-                   Id      : in Integer) is
+                   Id      : in ADO.Identifier) is
       Impl  : constant Counter_Access := new Counter_Impl;
       Found : Boolean;
       Query : ADO.SQL.Query;
@@ -198,7 +198,7 @@ package body AWA.Counters.Models is
 
    procedure Load (Object  : in out Counter_Ref;
                    Session : in out ADO.Sessions.Session'Class;
-                   Id      : in Integer;
+                   Id      : in ADO.Identifier;
                    Found   : out Boolean) is
       Impl  : constant Counter_Access := new Counter_Impl;
       Query : ADO.SQL.Query;
@@ -273,7 +273,7 @@ package body AWA.Counters.Models is
                    Session : in out ADO.Sessions.Session'Class) is
       Found : Boolean;
       Query : ADO.SQL.Query;
-      Id    : constant Integer := Integer (ADO.Identifier '(Object.Get_Key_Value));
+      Id    : constant ADO.Identifier := Object.Get_Key_Value;
    begin
       Query.Bind_Param (Position => 1, Value => Id);
       Query.Set_Filter ("definition_id = ?");
@@ -389,12 +389,11 @@ package body AWA.Counters.Models is
    procedure Load (Object  : in out Counter_Impl;
                    Stmt    : in out ADO.Statements.Query_Statement'Class;
                    Session : in out ADO.Sessions.Session'Class) is
-      pragma Unreferenced (Session);
    begin
       Object.Object_Id := Stmt.Get_Identifier (0);
       Object.Date := Stmt.Get_Time (1);
       Object.Counter := Stmt.Get_Integer (2);
-      Object.Set_Key_Value (Stmt.Get_Unbounded_String (3));
+      Object.Set_Key_Value (Stmt.Get_Identifier (3));
       ADO.Objects.Set_Created (Object);
    end Load;
    function Counter_Definition_Key (Id : in ADO.Identifier) return ADO.Objects.Object_Key is
@@ -747,6 +746,420 @@ package body AWA.Counters.Models is
       Object.Name := Stmt.Get_Unbounded_String (0);
       Object.Set_Key_Value (Stmt.Get_Identifier (1));
       Object.Entity_Type := Stmt.Get_Nullable_Entity_Type (2);
+      ADO.Objects.Set_Created (Object);
+   end Load;
+   function Visit_Key (Id : in ADO.Identifier) return ADO.Objects.Object_Key is
+      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_INTEGER,
+                                       Of_Class => VISIT_DEF'Access);
+   begin
+      ADO.Objects.Set_Value (Result, Id);
+      return Result;
+   end Visit_Key;
+
+   function Visit_Key (Id : in String) return ADO.Objects.Object_Key is
+      Result : ADO.Objects.Object_Key (Of_Type  => ADO.Objects.KEY_INTEGER,
+                                       Of_Class => VISIT_DEF'Access);
+   begin
+      ADO.Objects.Set_Value (Result, Id);
+      return Result;
+   end Visit_Key;
+
+   function "=" (Left, Right : Visit_Ref'Class) return Boolean is
+   begin
+      return ADO.Objects.Object_Ref'Class (Left) = ADO.Objects.Object_Ref'Class (Right);
+   end "=";
+
+   procedure Set_Field (Object : in out Visit_Ref'Class;
+                        Impl   : out Visit_Access) is
+      Result : ADO.Objects.Object_Record_Access;
+   begin
+      Object.Prepare_Modify (Result);
+      Impl := Visit_Impl (Result.all)'Access;
+   end Set_Field;
+
+   --  Internal method to allocate the Object_Record instance
+   procedure Allocate (Object : in out Visit_Ref) is
+      Impl : Visit_Access;
+   begin
+      Impl := new Visit_Impl;
+      Impl.Object_Id := ADO.NO_IDENTIFIER;
+      Impl.Counter := 0;
+      Impl.Date := ADO.DEFAULT_TIME;
+      Impl.User := ADO.NO_IDENTIFIER;
+      ADO.Objects.Set_Object (Object, Impl.all'Access);
+   end Allocate;
+
+   -- ----------------------------------------
+   --  Data object: Visit
+   -- ----------------------------------------
+
+   procedure Set_Object_Id (Object : in out Visit_Ref;
+                            Value  : in ADO.Identifier) is
+      Impl : Visit_Access;
+   begin
+      Set_Field (Object, Impl);
+      ADO.Objects.Set_Field_Identifier (Impl.all, 1, Impl.Object_Id, Value);
+   end Set_Object_Id;
+
+   function Get_Object_Id (Object : in Visit_Ref)
+                  return ADO.Identifier is
+      Impl : constant Visit_Access
+         := Visit_Impl (Object.Get_Load_Object.all)'Access;
+   begin
+      return Impl.Object_Id;
+   end Get_Object_Id;
+
+
+   procedure Set_Counter (Object : in out Visit_Ref;
+                          Value  : in Integer) is
+      Impl : Visit_Access;
+   begin
+      Set_Field (Object, Impl);
+      ADO.Objects.Set_Field_Integer (Impl.all, 2, Impl.Counter, Value);
+   end Set_Counter;
+
+   function Get_Counter (Object : in Visit_Ref)
+                  return Integer is
+      Impl : constant Visit_Access
+         := Visit_Impl (Object.Get_Load_Object.all)'Access;
+   begin
+      return Impl.Counter;
+   end Get_Counter;
+
+
+   procedure Set_Date (Object : in out Visit_Ref;
+                       Value  : in Ada.Calendar.Time) is
+      Impl : Visit_Access;
+   begin
+      Set_Field (Object, Impl);
+      ADO.Objects.Set_Field_Time (Impl.all, 3, Impl.Date, Value);
+   end Set_Date;
+
+   function Get_Date (Object : in Visit_Ref)
+                  return Ada.Calendar.Time is
+      Impl : constant Visit_Access
+         := Visit_Impl (Object.Get_Load_Object.all)'Access;
+   begin
+      return Impl.Date;
+   end Get_Date;
+
+
+   procedure Set_User (Object : in out Visit_Ref;
+                       Value  : in ADO.Identifier) is
+      Impl : Visit_Access;
+   begin
+      Set_Field (Object, Impl);
+      ADO.Objects.Set_Field_Identifier (Impl.all, 4, Impl.User, Value);
+   end Set_User;
+
+   function Get_User (Object : in Visit_Ref)
+                  return ADO.Identifier is
+      Impl : constant Visit_Access
+         := Visit_Impl (Object.Get_Load_Object.all)'Access;
+   begin
+      return Impl.User;
+   end Get_User;
+
+
+   procedure Set_Definition_Id (Object : in out Visit_Ref;
+                                Value  : in ADO.Identifier) is
+      Impl : Visit_Access;
+   begin
+      Set_Field (Object, Impl);
+      ADO.Objects.Set_Field_Key_Value (Impl.all, 5, Value);
+   end Set_Definition_Id;
+
+   function Get_Definition_Id (Object : in Visit_Ref)
+                  return ADO.Identifier is
+      Impl : constant Visit_Access
+         := Visit_Impl (Object.Get_Object.all)'Access;
+   begin
+      return Impl.Get_Key_Value;
+   end Get_Definition_Id;
+
+   --  Copy of the object.
+   procedure Copy (Object : in Visit_Ref;
+                   Into   : in out Visit_Ref) is
+      Result : Visit_Ref;
+   begin
+      if not Object.Is_Null then
+         declare
+            Impl : constant Visit_Access
+              := Visit_Impl (Object.Get_Load_Object.all)'Access;
+            Copy : constant Visit_Access
+              := new Visit_Impl;
+         begin
+            ADO.Objects.Set_Object (Result, Copy.all'Access);
+            Copy.Copy (Impl.all);
+            Copy.Object_Id := Impl.Object_Id;
+            Copy.Counter := Impl.Counter;
+            Copy.Date := Impl.Date;
+            Copy.User := Impl.User;
+         end;
+      end if;
+      Into := Result;
+   end Copy;
+
+   procedure Find (Object  : in out Visit_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class;
+                   Found   : out Boolean) is
+      Impl  : constant Visit_Access := new Visit_Impl;
+   begin
+      Impl.Find (Session, Query, Found);
+      if Found then
+         ADO.Objects.Set_Object (Object, Impl.all'Access);
+      else
+         ADO.Objects.Set_Object (Object, null);
+         Destroy (Impl);
+      end if;
+   end Find;
+
+   procedure Load (Object  : in out Visit_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Id      : in ADO.Identifier) is
+      Impl  : constant Visit_Access := new Visit_Impl;
+      Found : Boolean;
+      Query : ADO.SQL.Query;
+   begin
+      Query.Bind_Param (Position => 1, Value => Id);
+      Query.Set_Filter ("definition_id = ?");
+      Impl.Find (Session, Query, Found);
+      if not Found then
+         Destroy (Impl);
+         raise ADO.Objects.NOT_FOUND;
+      end if;
+      ADO.Objects.Set_Object (Object, Impl.all'Access);
+   end Load;
+
+   procedure Load (Object  : in out Visit_Ref;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Id      : in ADO.Identifier;
+                   Found   : out Boolean) is
+      Impl  : constant Visit_Access := new Visit_Impl;
+      Query : ADO.SQL.Query;
+   begin
+      Query.Bind_Param (Position => 1, Value => Id);
+      Query.Set_Filter ("definition_id = ?");
+      Impl.Find (Session, Query, Found);
+      if not Found then
+         Destroy (Impl);
+      else
+         ADO.Objects.Set_Object (Object, Impl.all'Access);
+      end if;
+   end Load;
+
+   procedure Save (Object  : in out Visit_Ref;
+                   Session : in out ADO.Sessions.Master_Session'Class) is
+      Impl : ADO.Objects.Object_Record_Access := Object.Get_Object;
+   begin
+      if Impl = null then
+         Impl := new Visit_Impl;
+         ADO.Objects.Set_Object (Object, Impl);
+      end if;
+      if not ADO.Objects.Is_Created (Impl.all) then
+         Impl.Create (Session);
+      else
+         Impl.Save (Session);
+      end if;
+   end Save;
+
+   procedure Delete (Object  : in out Visit_Ref;
+                     Session : in out ADO.Sessions.Master_Session'Class) is
+      Impl : constant ADO.Objects.Object_Record_Access := Object.Get_Object;
+   begin
+      if Impl /= null then
+         Impl.Delete (Session);
+      end if;
+   end Delete;
+
+   --  --------------------
+   --  Free the object
+   --  --------------------
+   procedure Destroy (Object : access Visit_Impl) is
+      type Visit_Impl_Ptr is access all Visit_Impl;
+      procedure Unchecked_Free is new Ada.Unchecked_Deallocation
+              (Visit_Impl, Visit_Impl_Ptr);
+      pragma Warnings (Off, "*redundant conversion*");
+      Ptr : Visit_Impl_Ptr := Visit_Impl (Object.all)'Access;
+      pragma Warnings (On, "*redundant conversion*");
+   begin
+      Unchecked_Free (Ptr);
+   end Destroy;
+
+   procedure Find (Object  : in out Visit_Impl;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class;
+                   Found   : out Boolean) is
+      Stmt : ADO.Statements.Query_Statement
+          := Session.Create_Statement (Query, VISIT_DEF'Access);
+   begin
+      Stmt.Execute;
+      if Stmt.Has_Elements then
+         Object.Load (Stmt, Session);
+         Stmt.Next;
+         Found := not Stmt.Has_Elements;
+      else
+         Found := False;
+      end if;
+   end Find;
+
+   overriding
+   procedure Load (Object  : in out Visit_Impl;
+                   Session : in out ADO.Sessions.Session'Class) is
+      Found : Boolean;
+      Query : ADO.SQL.Query;
+      Id    : constant ADO.Identifier := Object.Get_Key_Value;
+   begin
+      Query.Bind_Param (Position => 1, Value => Id);
+      Query.Set_Filter ("definition_id = ?");
+      Object.Find (Session, Query, Found);
+      if not Found then
+         raise ADO.Objects.NOT_FOUND;
+      end if;
+   end Load;
+
+   procedure Save (Object  : in out Visit_Impl;
+                   Session : in out ADO.Sessions.Master_Session'Class) is
+      Stmt : ADO.Statements.Update_Statement
+         := Session.Create_Statement (VISIT_DEF'Access);
+   begin
+      if Object.Is_Modified (1) then
+         Stmt.Save_Field (Name  => COL_0_3_NAME, --  object_id
+                          Value => Object.Object_Id);
+         Object.Clear_Modified (1);
+      end if;
+      if Object.Is_Modified (2) then
+         Stmt.Save_Field (Name  => COL_1_3_NAME, --  counter
+                          Value => Object.Counter);
+         Object.Clear_Modified (2);
+      end if;
+      if Object.Is_Modified (3) then
+         Stmt.Save_Field (Name  => COL_2_3_NAME, --  date
+                          Value => Object.Date);
+         Object.Clear_Modified (3);
+      end if;
+      if Object.Is_Modified (4) then
+         Stmt.Save_Field (Name  => COL_3_3_NAME, --  user
+                          Value => Object.User);
+         Object.Clear_Modified (4);
+      end if;
+      if Object.Is_Modified (5) then
+         Stmt.Save_Field (Name  => COL_4_3_NAME, --  definition_id
+                          Value => Object.Get_Key);
+         Object.Clear_Modified (5);
+      end if;
+      if Stmt.Has_Save_Fields then
+         Stmt.Set_Filter (Filter => "definition_id = ?");
+         Stmt.Add_Param (Value => Object.Get_Key);
+         declare
+            Result : Integer;
+         begin
+            Stmt.Execute (Result);
+            if Result /= 1 then
+               if Result /= 0 then
+                  raise ADO.Objects.UPDATE_ERROR;
+               end if;
+            end if;
+         end;
+      end if;
+   end Save;
+
+   procedure Create (Object  : in out Visit_Impl;
+                     Session : in out ADO.Sessions.Master_Session'Class) is
+      Query : ADO.Statements.Insert_Statement
+                  := Session.Create_Statement (VISIT_DEF'Access);
+      Result : Integer;
+   begin
+      Query.Save_Field (Name  => COL_0_3_NAME, --  object_id
+                        Value => Object.Object_Id);
+      Query.Save_Field (Name  => COL_1_3_NAME, --  counter
+                        Value => Object.Counter);
+      Query.Save_Field (Name  => COL_2_3_NAME, --  date
+                        Value => Object.Date);
+      Query.Save_Field (Name  => COL_3_3_NAME, --  user
+                        Value => Object.User);
+      Session.Allocate (Id => Object);
+      Query.Save_Field (Name  => COL_4_3_NAME, --  definition_id
+                        Value => Object.Get_Key);
+      Query.Execute (Result);
+      if Result /= 1 then
+         raise ADO.Objects.INSERT_ERROR;
+      end if;
+      ADO.Objects.Set_Created (Object);
+   end Create;
+
+   procedure Delete (Object  : in out Visit_Impl;
+                     Session : in out ADO.Sessions.Master_Session'Class) is
+      Stmt : ADO.Statements.Delete_Statement
+         := Session.Create_Statement (VISIT_DEF'Access);
+   begin
+      Stmt.Set_Filter (Filter => "definition_id = ?");
+      Stmt.Add_Param (Value => Object.Get_Key);
+      Stmt.Execute;
+   end Delete;
+
+   --  ------------------------------
+   --  Get the bean attribute identified by the name.
+   --  ------------------------------
+   overriding
+   function Get_Value (From : in Visit_Ref;
+                       Name : in String) return Util.Beans.Objects.Object is
+      Obj  : constant ADO.Objects.Object_Record_Access := From.Get_Load_Object;
+      Impl : access Visit_Impl;
+   begin
+      if Obj = null then
+         return Util.Beans.Objects.Null_Object;
+      end if;
+      Impl := Visit_Impl (Obj.all)'Access;
+      if Name = "object_id" then
+         return Util.Beans.Objects.To_Object (Long_Long_Integer (Impl.Object_Id));
+      elsif Name = "counter" then
+         return Util.Beans.Objects.To_Object (Long_Long_Integer (Impl.Counter));
+      elsif Name = "date" then
+         return Util.Beans.Objects.Time.To_Object (Impl.Date);
+      elsif Name = "user" then
+         return Util.Beans.Objects.To_Object (Long_Long_Integer (Impl.User));
+      elsif Name = "definition_id" then
+         return ADO.Objects.To_Object (Impl.Get_Key);
+      end if;
+      return Util.Beans.Objects.Null_Object;
+   end Get_Value;
+
+
+   procedure List (Object  : in out Visit_Vector;
+                   Session : in out ADO.Sessions.Session'Class;
+                   Query   : in ADO.SQL.Query'Class) is
+      Stmt : ADO.Statements.Query_Statement
+        := Session.Create_Statement (Query, VISIT_DEF'Access);
+   begin
+      Stmt.Execute;
+      Visit_Vectors.Clear (Object);
+      while Stmt.Has_Elements loop
+         declare
+            Item : Visit_Ref;
+            Impl : constant Visit_Access := new Visit_Impl;
+         begin
+            Impl.Load (Stmt, Session);
+            ADO.Objects.Set_Object (Item, Impl.all'Access);
+            Object.Append (Item);
+         end;
+         Stmt.Next;
+      end loop;
+   end List;
+
+   --  ------------------------------
+   --  Load the object from current iterator position
+   --  ------------------------------
+   procedure Load (Object  : in out Visit_Impl;
+                   Stmt    : in out ADO.Statements.Query_Statement'Class;
+                   Session : in out ADO.Sessions.Session'Class) is
+   begin
+      Object.Object_Id := Stmt.Get_Identifier (0);
+      Object.Counter := Stmt.Get_Integer (1);
+      Object.Date := Stmt.Get_Time (2);
+      Object.User := Stmt.Get_Identifier (3);
+      Object.Set_Key_Value (Stmt.Get_Identifier (4));
       ADO.Objects.Set_Created (Object);
    end Load;
 
