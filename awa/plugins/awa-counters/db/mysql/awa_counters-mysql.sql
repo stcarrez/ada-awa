c1ff5824232e903d3c483128919fb944e735eaf1
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2016-08-05 17:25:43 +0200

    Rebuild the SQL files

--- awa/plugins/awa-counters/db/mysql/awa_counters-mysql.sql
+++ awa/plugins/awa-counters/db/mysql/awa_counters-mysql.sql
@@ -7,8 +7,8 @@ CREATE TABLE awa_counter (
   `date` DATE NOT NULL,
   /* the counter value. */
   `counter` INTEGER NOT NULL,
-  /* the definition id. */
-  `definition_id` INTEGER NOT NULL,
+  /* the counter definition identifier. */
+  `definition_id` BIGINT NOT NULL,
   PRIMARY KEY (`object_id`, `date`, `definition_id`)
 );
 /* A counter definition defines what the counter represents. It uniquely identifies
@@ -18,12 +18,27 @@ CREATE TABLE awa_counter_definition (
   /* the counter name. */
   `name` VARCHAR(255) BINARY NOT NULL,
   /* the counter unique id. */
-  `id` BIGINT NOT NULL,
-  /*  */
+  `id` INTEGER NOT NULL,
+  /* the optional entity type that identifies the database table. */
   `entity_type` INTEGER ,
   PRIMARY KEY (`id`)
+);
+/*  */
+CREATE TABLE awa_visit (
+  /* the entity identifier. */
+  `object_id` BIGINT NOT NULL,
+  /* the number of times the entity was visited by the user. */
+  `counter` INTEGER NOT NULL,
+  /* the date and time when the entity was last visited. */
+  `date` DATETIME NOT NULL,
+  /* the user who visited the entity. */
+  `user` BIGINT NOT NULL,
+  /* the counter definition identifier. */
+  `definition_id` BIGINT NOT NULL,
+  PRIMARY KEY (`object_id`, `user`, `definition_id`)
 );
 INSERT INTO entity_type (name) VALUES
 ("awa_counter")
 ,("awa_counter_definition")
+,("awa_visit")
 ;
