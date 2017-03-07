daf8d479eb11b4bdd60d1442bfd2775fabc9c972
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-07 20:45:36 +0100

    Add some documentation

--- awa/plugins/awa-setup/src/awa-setup.ads
+++ awa/plugins/awa-setup/src/awa-setup.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-setup -- Setup and installation
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +15,20 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+
+--  == Setup Application ==
+--  The <tt>AWA.Setup</tt> package implements a simple setup application
+--  that allows to configure the database, the Google and Facebook application
+--  identifiers and some other configuration parameters.  It is intended to
+--  help in the installation process of any AWA-based application.
+--
+--  It defines a specific web application that is installed in the web container
+--  for the duration of the setup.  The setup application takes control over all
+--  the web requests during the lifetime of the installation.  As soon as the
+--  installation is finished, the normal application is configured and installed
+--  in the web container and the user is automatically redirected to it.
+--
+--  @include awa-setup-applications.ads
 package AWA.Setup is
 
    pragma Pure;
