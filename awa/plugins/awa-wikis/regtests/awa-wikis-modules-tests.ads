796b573cdf77b1ecdbfb9a2017b0c6612cae45b0
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-09 23:13:19 +0100

    Declare the Test_Wiki_Page procedure to simulate HTTP requests on wiki page

--- awa/plugins/awa-wikis/regtests/awa-wikis-modules-tests.ads
+++ awa/plugins/awa-wikis/regtests/awa-wikis-modules-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-wikis-modules-tests -- Unit tests for wikis service
---  Copyright (C) 2015 Stephane Carrez
+--  Copyright (C) 2015, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,13 +18,17 @@
 
 with Util.Tests;
 with AWA.Tests;
+with ADO;
 
 package AWA.Wikis.Modules.Tests is
 
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
 
    type Test is new AWA.Tests.Test with record
-      Manager : AWA.Wikis.Modules.Wiki_Module_Access;
+      Manager    : AWA.Wikis.Modules.Wiki_Module_Access;
+      Wiki_Id    : ADO.Identifier := ADO.NO_IDENTIFIER;
+      Public_Id  : ADO.Identifier := ADO.NO_IDENTIFIER;
+      Private_Id : ADO.Identifier := ADO.NO_IDENTIFIER;
    end record;
 
    --  Test creation of a wiki space.
@@ -35,5 +39,7 @@ package AWA.Wikis.Modules.Tests is
 
    --  Test creation of a wiki page content.
    procedure Test_Create_Wiki_Content (T : in out Test);
+
+   procedure Test_Wiki_Page (T : in out Test);
 
 end AWA.Wikis.Modules.Tests;
