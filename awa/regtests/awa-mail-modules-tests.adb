4aa53491672a65bc4864387009f0b2f638acceb7
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-09 23:00:53 +0100

    Add new tests for mail Cc and Bcc

--- awa/regtests/awa-mail-modules-tests.adb
+++ awa/regtests/awa-mail-modules-tests.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-mail-module-tests -- Unit tests for Mail module
---  Copyright (C) 2012 Stephane Carrez
+--  Copyright (C) 2012, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,10 +28,17 @@ package body AWA.Mail.Modules.Tests is
    begin
       Caller.Add_Test (Suite, "Test AWA.Mail.Module.Create_Message",
                        Test_Create_Message'Access);
+      Caller.Add_Test (Suite, "Test AWA.Mail.Module.Create_Message (CC:)",
+                       Test_Cc_Message'Access);
+      Caller.Add_Test (Suite, "Test AWA.Mail.Module.Create_Message (BCC:)",
+                       Test_Bcc_Message'Access);
    end Add_Tests;
 
-   --  Create an email message and verify its content.
-   procedure Test_Create_Message (T : in out Test) is
+   --  ------------------------------
+   --  Create an email message with the given template and verify its content.
+   --  ------------------------------
+   procedure Test_Mail_Message (T    : in out Test;
+                                Name : in String) is
       use Util.Beans.Objects;
 
       Mail  : constant AWA.Mail.Modules.Mail_Module_Access := AWA.Mail.Modules.Get_Mail_Module;
@@ -42,10 +49,34 @@ package body AWA.Mail.Modules.Tests is
 
       Props.Insert ("name", To_Object (String '("joe")));
       Props.Insert ("email", To_Object (String '("joe.rogers@somewhere.org")));
-      Mail.Send_Mail (Template => "mail-info.html",
+      Mail.Send_Mail (Template => Name,
                       Props    => Props,
                       Content  => Event);
 
+   end Test_Mail_Message;
+
+   --  ------------------------------
+   --  Create an email message and verify its content.
+   --  ------------------------------
+   procedure Test_Create_Message (T : in out Test) is
+   begin
+      T.Test_Mail_Message ("mail-info.html");
    end Test_Create_Message;
+
+   --  ------------------------------
+   --  Create an email message with Cc: and verify its content.
+   --  ------------------------------
+   procedure Test_Cc_Message (T : in out Test) is
+   begin
+      T.Test_Mail_Message ("mail-cc.html");
+   end Test_Cc_Message;
+
+   --  ------------------------------
+   --  Create an email message with Bcc: and verify its content.
+   --  ------------------------------
+   procedure Test_Bcc_Message (T : in out Test) is
+   begin
+      T.Test_Mail_Message ("mail-bcc.html");
+   end Test_Bcc_Message;
 
 end AWA.Mail.Modules.Tests;
