28f2eee8587ceebfc2b868ea6e5af21e3cdd5f53
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-03-12 10:49:23 +0100

    Remove the samples because they are now moved in the Atlas Git repository

--- awa/samples/db/migrate/atlas-0.3-migrate.sql
+++
@@ -1,7 +0,0 @@
-/*
- * Migration for Atlas 0.2 to 0.3
- *
- * AWA migration from 0.2 to 0.3 must be executed separately.
- */
-ALTER TABLE mblog CHANGE COLUMN `create_date` `creation_date` DATETIME NOT NULL;
-
--- awa/samples/db/mysql/atlas-drop-mysql.sql
+++
@@ -1,3 +0,0 @@
-/* File generated automatically by dynamo */
-DROP TABLE IF EXISTS `atlas_review`;
-DROP TABLE IF EXISTS `mblog`;
