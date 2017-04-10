0645b9dd1bf64c6939928daed4c49b57e9f5b75f
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-10 09:46:37 +0200

    Unit tests for workspaces and invitations

---
+++ awa/plugins/awa-workspaces/regtests/awa-workspaces-tests.ads
@@ -0,0 +1,31 @@
+-----------------------------------------------------------------------
+--  awa-workspaces-tests -- Unit tests for workspaces and invitations
+--  Copyright (C) 2017 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+with AWA.Tests;
+
+package AWA.Workspaces.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new AWA.Tests.Test with null record;
+
+   --  Test sending an invitation.
+   procedure Test_Invite_User (T : in out Test);
+
+end AWA.Workspaces.Tests;
