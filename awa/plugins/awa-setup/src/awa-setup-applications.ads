7a8f25f60ff8bfd8cd33939e49e0891bef424083
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-04-17 11:38:30 +0200

    Add some documentation

--- awa/plugins/awa-setup/src/awa-setup-applications.ads
+++ awa/plugins/awa-setup/src/awa-setup-applications.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  awa-setup -- Setup and installation
---  Copyright (C) 2016 Stephane Carrez
+--  awa-setup-applications -- Setup and installation
+--  Copyright (C) 2016, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,6 +26,37 @@ with Util.Beans.Objects;
 with Util.Beans.Basic;
 with Util.Beans.Methods;
 with ADO.Drivers.Connections;
+
+--  == Setup Procedure Instantiation==
+--  The setup process is managed by the *Configure* generic procedure.  The procedure must
+--  be instantiated with the application class type and the application initialize procedure.
+--
+--     procedure Setup is
+--        new AWA.Setup.Applications.Configure (MyApp.Application'Class,
+--                                              MyApp.Application_Access,
+--                                              MyApp.Initialize);
+--
+--  == Setup Operation ==
+--  The *Setup* instantiated operation must then be called with the web container.
+--  The web container is started first and the *Setup* procedure gets as parameter the
+--  web container, the application instance to configure, the application name and
+--  the application context path.
+--
+--    Setup (WS, App, "atlas", MyApp.CONTEXT_PATH)
+--
+--  The operation will install the setup application to handle the setup actions.  Through
+--  the setup actions, the installer will be able to:
+--
+--  * Configure the database (MySQL or SQLite),
+--  * Configure the Google+ and Facebook OAuth authentication keys,
+--  * Configure the application name,
+--  * Configure the mail parameters to be able to send email.
+--
+--  After the setup and configure is finished, the file <tt>.initialized</tt> is created in
+--  the application directory to indicate the application is configured.  The next time the
+--  *Setup* operation is called, the installation process will be skipped.
+--
+--  To run again the installation, remove manually the <tt>.initialized</tt> file.
 package AWA.Setup.Applications is
 
    --  The <b>Servlet</b> represents the component that will handle
