345d6351f01d0911da182a8d21b6f8e17839fe09
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-14 23:29:14 +0200

    Fix SQL for the migration script

--- awa/db/migrate/awa-1.1-migrate.sql
+++ awa/db/migrate/awa-1.1-migrate.sql
@@ -245,7 +245,7 @@ INSERT INTO awa_workspace_member (id, member_id, workspace_id, join_date, role)
    FROM awa_workspace AS ws
    INNER JOIN entity_type AS ent
    INNER JOIN awa_acl AS acl ON acl.entity_id = ws.id AND acl.entity_type = ent.id
-   WHERE ent.name = 'awa_workspace')
+   WHERE ent.name = 'awa_workspace');
 
 ALTER TABLE awa_acl ADD COLUMN permission BIGINT NOT NULL;
 ALTER TABLE awa_acl ADD COLUMN workspace_id BIGINT NOT NULL;
