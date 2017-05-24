3b08082bec8782ef79f350b341de5e54420e6056
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-24 22:10:38 +0200

    Update the permission creation to create a create/update/delete/view permission

--- awa/plugins/awa-wikis/src/awa-wikis-modules.adb
+++ awa/plugins/awa-wikis/src/awa-wikis-modules.adb
@@ -181,10 +181,16 @@ package body AWA.Wikis.Modules is
       Wiki.Save (DB);
 
       --  Add the permission for the user to use the new wiki space.
-      AWA.Permissions.Services.Add_Permission (Session   => DB,
-                                               User      => User,
-                                               Entity    => Wiki,
-                                               Workspace => WS.Get_Id);
+      AWA.Workspaces.Modules.Add_Permission (Session   => DB,
+                                             User      => User,
+                                             Entity    => Wiki,
+                                             Workspace => WS.Get_Id,
+                                             List      => (ACL_Update_Wiki_Space.Permission,
+                                                           ACL_Delete_Wiki_Space.Permission,
+                                                           ACL_Create_Wiki_Pages.Permission,
+                                                           ACL_Delete_Wiki_Pages.Permission,
+                                                           ACL_Update_Wiki_Pages.Permission,
+                                                           ACL_View_Wiki_Page.Permission));
 
       Util.Strings.Tokenizers.Iterate_Tokens (Content => Module.Get_Config (PARAM_WIKI_COPY_LIST),
                                               Pattern => ",",
