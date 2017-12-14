fb35c708f237766d15b8e278735f0aeebcd00508
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-14 21:58:33 +0100

    Rebuild the model files

--- awa/plugins/awa-images/src/model/awa-images-models.adb
+++ awa/plugins/awa-images/src/model/awa-images-models.adb
@@ -5,7 +5,7 @@
 --  Template used: templates/model/package-body.xhtml
 --  Ada Generator: https://ada-gen.googlecode.com/svn/trunk Revision 1095
 -----------------------------------------------------------------------
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -882,7 +882,7 @@ package body AWA.Images.Models is
 
       Stmt : ADO.Statements.Query_Statement
           := Session.Create_Statement (Context);
-      Pos  : Natural := 0;
+      Pos  : Positive := 1;
       procedure Read (Into : in out Image_Info) is
       begin
          Into.Id := Stmt.Get_Identifier (0);
