947f4ee789a754192248db2f2156130821ef972b
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-12-14 21:54:02 +0100

    Rebuild the model files

--- awa/plugins/awa-wikis/src/model/awa-wikis-models.adb
+++ awa/plugins/awa-wikis/src/model/awa-wikis-models.adb
@@ -1840,7 +1840,7 @@ package body AWA.Wikis.Models is
 
       Stmt : ADO.Statements.Query_Statement
           := Session.Create_Statement (Context);
-      Pos  : Natural := 0;
+      Pos  : Positive := 1;
       procedure Read (Into : in out Wiki_Image_Info) is
       begin
          Into.Folder_Id := Stmt.Get_Identifier (0);
@@ -1930,7 +1930,7 @@ package body AWA.Wikis.Models is
 
       Stmt : ADO.Statements.Query_Statement
           := Session.Create_Statement (Context);
-      Pos  : Natural := 0;
+      Pos  : Positive := 1;
       procedure Read (Into : in out Wiki_Info) is
       begin
          Into.Id := Stmt.Get_Identifier (0);
@@ -2028,7 +2028,7 @@ package body AWA.Wikis.Models is
 
       Stmt : ADO.Statements.Query_Statement
           := Session.Create_Statement (Context);
-      Pos  : Natural := 0;
+      Pos  : Positive := 1;
       procedure Read (Into : in out Wiki_Page_Info) is
       begin
          Into.Id := Stmt.Get_Identifier (0);
@@ -2117,7 +2117,7 @@ package body AWA.Wikis.Models is
 
       Stmt : ADO.Statements.Query_Statement
           := Session.Create_Statement (Context);
-      Pos  : Natural := 0;
+      Pos  : Positive := 1;
       procedure Read (Into : in out Wiki_Version_Info) is
       begin
          Into.Id := Stmt.Get_Identifier (0);
