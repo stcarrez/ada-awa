56ba2ebc340167550cb7b3e5bbd9c78e5ca6d45e
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-06-10 08:40:44 +0200

    Move the Reader_Config generic package to a separate file to avoid circular elaboration check with GNAT 2018

--- awa/src/awa-events-configs.ads
+++ awa/src/awa-events-configs.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-events-configs -- Event configuration
---  Copyright (C) 2012, 2013, 2017 Stephane Carrez
+--  Copyright (C) 2012, 2013, 2017, 2018 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -60,38 +60,9 @@ package AWA.Events.Configs is
                          Field : in Config_Fields;
                          Value : in Util.Beans.Objects.Object);
 
-   --  Setup the XML parser to read the <b>queue</b> and <b>on-event</b> description.
-   --  For example:
-   --
-   --  <dispatcher name="async">
-   --     <queue name="async"/>
-   --     <queue name="persist"/>
-   --     <count>4</count>
-   --     <priority>10</priority>
-   --  </dispatcher>
-   --
-   --  <queue name="async" type="fifo">
-   --     <property name="size">254</property>
-   --  </queue>
-   --
-   --  <queue name="defer" type="persist">
-   --  </queue>
-   --
-   --  <on-event name="create-user" queue="async">
-   --     <action>#{mail.send}</action>
-   --     <property name="user">#{event.name}</property>
-   --     <property name="template">mail/welcome.xhtml</property>
-   --  </on-event>
-   --
-   --  This defines an event action called when the <b>create-user</b> event is posted.
-   --  The Ada bean <b>mail</b> is created and is populated with the <b>user</b> and
-   --  <b>template</b> properties.  The Ada bean action method <b>send</b> is called.
-   generic
-      Mapper  : in out Util.Serialize.Mappers.Processing;
-      Manager : in AWA.Events.Services.Event_Manager_Access;
-      Context : in EL.Contexts.ELContext_Access;
-   package Reader_Config is
-      Config : aliased Controller_Config;
-   end Reader_Config;
+private
+
+   procedure Add_Mapping (Mapper : in out Util.Serialize.Mappers.Processing;
+                          Config : in Controller_Config_Access);
 
 end AWA.Events.Configs;
