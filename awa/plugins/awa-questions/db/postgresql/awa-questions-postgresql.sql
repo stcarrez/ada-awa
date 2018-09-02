8016e56b1cdc6ceecaae872c5f336dbe495d8350
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-09-02 16:58:56 +0200

    Add Postgresql SQL files

---
+++ awa/plugins/awa-questions/db/postgresql/awa-questions-postgresql.sql
@@ -0,0 +1,57 @@
+/* File generated automatically by dynamo */
+/* The answer table gives a list of anwsers to the question.
+Ranking is updating according to users voting for the anwser. */
+CREATE TABLE awa_answer (
+  /* the answer creation date. */
+  "create_date" TIMESTAMP NOT NULL,
+  /* the date when the answer was edited. */
+  "edit_date" TIMESTAMP ,
+  /* the answer text. */
+  "answer" TEXT NOT NULL,
+  /* the anwser rank number. */
+  "rank" INTEGER NOT NULL,
+  /* the answer identifier. */
+  "id" BIGINT NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the user who wrote the answer. */
+  "author_id" BIGINT NOT NULL,
+  /*  */
+  "question_id" BIGINT NOT NULL,
+  PRIMARY KEY ("id")
+);
+/* The question table holds a single question asked by a user to the community.
+The short description is used to give an overview of the question in long lists
+while the description contains the full question text.  The rating is updating
+according to users voting for the question. */
+CREATE TABLE awa_question (
+  /* the date when the question was created. */
+  "create_date" TIMESTAMP NOT NULL,
+  /* the question title. */
+  "title" VARCHAR(255) NOT NULL,
+  /* the full description. */
+  "description" TEXT NOT NULL,
+  /* the date when the question was edited. */
+  "edit_date" TIMESTAMP ,
+  /* Title: Questions and Answers model
+Date: 2015-11-15
+the question short description. */
+  "short_description" VARCHAR(255) NOT NULL,
+  /* the question rating. */
+  "rating" INTEGER NOT NULL,
+  /* the question identifier. */
+  "id" BIGINT NOT NULL,
+  /* the optimistic locking version. */
+  "version" INTEGER NOT NULL,
+  /* the user who asked the question. */
+  "author_id" BIGINT NOT NULL,
+  /*  */
+  "workspace_id" BIGINT NOT NULL,
+  /*  */
+  "accepted_answer_id" BIGINT ,
+  PRIMARY KEY ("id")
+);
+INSERT INTO entity_type (name) VALUES
+('awa_answer')
+,('awa_question')
+;
