d70aa9fd597e6c82494630259043e1a1553505f9
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-09-02 17:02:12 +0200

    Add Postgresql SQL files

---
+++ awa/plugins/awa-wikis/db/postgresql/awa-wikis-drop-postgresql.sql
@@ -0,0 +1,4 @@
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS awa_wiki_space;
+DROP TABLE IF EXISTS awa_wiki_page;
+DROP TABLE IF EXISTS awa_wiki_content;
---
+++ awa/plugins/awa-wikis/db/postgresql/awa-wikis-postgresql.sql
@@ -0,0 +1,78 @@
+/* File generated automatically by dynamo */
+/*  */
+CREATE TABLE awa_wiki_content (
+  /* the wiki page content identifier */
+  "id" BIGINT NOT NULL,
+  /* the wiki content creation date */
+  "create_date" TIMESTAMP NOT NULL,
+  /* the wiki text content */
+  "content" TEXT NOT NULL,
+  /* the format type used used by the wiki content */
+  "format" SMALLINT NOT NULL,
+  /* the content comment string */
+  "save_comment" VARCHAR(255) NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the wiki page version */
+  "page_version" INTEGER NOT NULL,
+  /* the wiki page that this Wiki_Content belongs to */
+  "page_id" BIGINT NOT NULL,
+  /* the page version author */
+  "author_id" BIGINT NOT NULL,
+  PRIMARY KEY ("id")
+);
+/* The wiki page represents a page with its versions.
+It refers to the last version which is currently visible.
+It has an optional preview image which defines
+the thumbnail preview of the last/current wiki content. */
+CREATE TABLE awa_wiki_page (
+  /* the wiki page identifier */
+  "id" BIGINT NOT NULL,
+  /* the wiki page name */
+  "name" VARCHAR(255) NOT NULL,
+  /* the last page version number */
+  "last_version" INTEGER NOT NULL,
+  /* whether the wiki page is public */
+  "is_public" BOOLEAN NOT NULL,
+  /* the page title */
+  "title" VARCHAR(255) NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* a read counter which indicates how many times the page was read. */
+  "read_count" INTEGER NOT NULL,
+  /* the wiki page preview. */
+  "preview_id" BIGINT ,
+  /* the wiki space that this page belongs to */
+  "wiki_id" BIGINT NOT NULL,
+  /* the current content (or last version) */
+  "content_id" BIGINT ,
+  PRIMARY KEY ("id")
+);
+/* Permission is granted to display a wiki page if there is
+an ACL entry between the wiki space and the user. */
+CREATE TABLE awa_wiki_space (
+  /* the wiki space identifier */
+  "id" BIGINT NOT NULL,
+  /* the wiki name */
+  "name" VARCHAR(255) NOT NULL,
+  /* whether the wiki is public */
+  "is_public" BOOLEAN NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the wiki creation date. */
+  "create_date" TIMESTAMP NOT NULL,
+  /* the left panel side wiki text for every page. */
+  "left_side" TEXT NOT NULL,
+  /* the right panel wiki text for every page. */
+  "right_side" TEXT NOT NULL,
+  /* the default wiki page format. */
+  "format" SMALLINT NOT NULL,
+  /*  */
+  "workspace_id" BIGINT NOT NULL,
+  PRIMARY KEY ("id")
+);
+INSERT INTO entity_type (name) VALUES
+('awa_wiki_content')
+,('awa_wiki_page')
+,('awa_wiki_space')
+;
