4af44118d1ac21d58f9bb3718f7b6d874056a67f
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-09-02 17:01:41 +0200

    Add Postgresql SQL files

---
+++ awa/plugins/awa-votes/db/postgresql/awa-votes-postgresql.sql
@@ -0,0 +1,30 @@
+/* File generated automatically by dynamo */
+/*  */
+CREATE TABLE awa_rating (
+  /* the rating identifier */
+  "id" BIGINT NOT NULL,
+  /* the rating taking into account all votes */
+  "rating" INTEGER NOT NULL,
+  /* the number of votes */
+  "vote_count" INTEGER NOT NULL,
+  /*  */
+  "for_entity_id" BIGINT NOT NULL,
+  /* the entity type */
+  "for_entity_type" INTEGER NOT NULL,
+  PRIMARY KEY ("id")
+);
+/* The vote table tracks a vote action by a user on a given database entity.
+The primary key is made of the user, the entity id and entity type. */
+CREATE TABLE awa_vote (
+  /*  */
+  "rating" INTEGER NOT NULL,
+  /*  */
+  "entity_id" BIGINT NOT NULL,
+  /*  */
+  "user_id" BIGINT NOT NULL,
+  PRIMARY KEY ("entity_id", "user_id")
+);
+INSERT INTO entity_type (name) VALUES
+('awa_rating')
+,('awa_vote')
+;
