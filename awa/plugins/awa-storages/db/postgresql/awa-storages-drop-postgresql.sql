5bfdf341aa56602c440b95b7d324c7dae7cb5fc4
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-09-02 17:00:53 +0200

    Add Postgresql SQL files

---
+++ awa/plugins/awa-storages/db/postgresql/awa-storages-drop-postgresql.sql
@@ -0,0 +1,5 @@
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS awa_store_local;
+DROP TABLE IF EXISTS awa_storage_folder;
+DROP TABLE IF EXISTS awa_storage_data;
+DROP TABLE IF EXISTS awa_storage;
---
+++ awa/plugins/awa-storages/db/postgresql/awa-storages-postgresql.sql
@@ -0,0 +1,92 @@
+/* File generated automatically by dynamo */
+/* The uri member holds the URI if the storage type is URL.
+
+When storage is FILE, the local file path is built by using
+the workspace identifier and the storage identifier. */
+CREATE TABLE awa_storage (
+  /* the storage type which defines where the content is stored */
+  "storage" SMALLINT NOT NULL,
+  /* the storage creation date */
+  "create_date" TIMESTAMP NOT NULL,
+  /* the file name */
+  "name" VARCHAR(255) NOT NULL,
+  /* the file size */
+  "file_size" INTEGER NOT NULL,
+  /* the mime type */
+  "mime_type" VARCHAR(255) NOT NULL,
+  /* the storage URI */
+  "uri" VARCHAR(255) NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the storage identifier */
+  "id" BIGINT NOT NULL,
+  /* whether the document is public or not. */
+  "is_public" BOOLEAN NOT NULL,
+  /*  */
+  "original_id" BIGINT ,
+  /*  */
+  "store_data_id" BIGINT ,
+  /*  */
+  "owner_id" BIGINT NOT NULL,
+  /*  */
+  "workspace_id" BIGINT NOT NULL,
+  /*  */
+  "folder_id" BIGINT ,
+  PRIMARY KEY ("id")
+);
+/* The storage data is created only if the storage type
+is set to DATABASE.  It holds the file content in the blob. */
+CREATE TABLE awa_storage_data (
+  /* the storage data identifier */
+  "id" BIGINT NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the storage content */
+  "data" BYTEA NOT NULL,
+  PRIMARY KEY ("id")
+);
+/*  */
+CREATE TABLE awa_storage_folder (
+  /* the storage folder identifier */
+  "id" BIGINT NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /* the folder creation date */
+  "create_date" TIMESTAMP NOT NULL,
+  /*  */
+  "name" VARCHAR(255) NOT NULL,
+  /*  */
+  "workspace_id" BIGINT NOT NULL,
+  /*  */
+  "owner_id" BIGINT NOT NULL,
+  PRIMARY KEY ("id")
+);
+/* The local store record is created when a copy of the data is needed on the local file system.
+The creation date refers to the date when the data was copied to the local file system.
+The expiration date indicates a date after which the local file can be removed
+from the local file system. */
+CREATE TABLE awa_store_local (
+  /* the local store identifier */
+  "id" BIGINT NOT NULL,
+  /*  */
+  "version" INTEGER NOT NULL,
+  /*  */
+  "store_version" INTEGER NOT NULL,
+  /* the shared flag which indicates whether this local store can be shared by several clients. */
+  "shared" BOOLEAN NOT NULL,
+  /* the local store path */
+  "path" VARCHAR(255) NOT NULL,
+  /* the local store expiration date */
+  "expire_date" DATE NOT NULL,
+  /* the creation date */
+  "create_date" TIMESTAMP NOT NULL,
+  /*  */
+  "storage_id" BIGINT ,
+  PRIMARY KEY ("id")
+);
+INSERT INTO entity_type (name) VALUES
+('awa_storage')
+,('awa_storage_data')
+,('awa_storage_folder')
+,('awa_store_local')
+;
