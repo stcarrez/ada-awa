82ff82fea2f51c5e52e1cc744b49c5e75f999259
Stephane Carrez <Stephane.Carrez@gmail.com>
2018-12-24 18:39:12 +0100

    Add audit field support in the model: - Declare Get_Audit_Field function - Declare Field_Key record - Instantiate Audit_Fields_Maps package - Keep a map of audited field to the audit field unique id

--- awa/src/awa-audits-services.ads
+++ awa/src/awa-audits-services.ads
@@ -15,6 +15,7 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
+with Ada.Containers.Indefinite_Hashed_Maps;
 
 with ADO.Audits;
 with ADO.Sessions;
@@ -28,7 +29,7 @@ package AWA.Audits.Services is
    --  or to the event action.  The event manager holds a list of actions that must be
    --  triggered for a particular event/queue pair.  Such list is created and initialized
    --  when the application is configured.  It never changes.
-   type Audit_Manager is limited new ADO.Audits.Audit_Manager with null record;
+   type Audit_Manager is limited new ADO.Audits.Audit_Manager with private;
    type Audit_Manager_Access is access all Audit_Manager'Class;
 
    --  Save the audit changes in the database.
@@ -37,5 +38,30 @@ package AWA.Audits.Services is
                    Session : in out ADO.Sessions.Master_Session'Class;
                    Object  : in ADO.Audits.Auditable_Object_Record'Class;
                    Changes : in ADO.Audits.Audit_Array);
+
+   --  Find the audit field identification number from the entity type and field name.
+   function Get_Audit_Field (Manager : in Audit_Manager;
+                             Name    : in String;
+                             Entity  : in ADO.Entity_Type) return ADO.Identifier;
+
+private
+
+   type Field_Key (Len : Natural) is record
+      Entity : ADO.Entity_Type;
+      Name   : String (1 .. Len);
+   end record;
+
+   function Hash (Item : in Field_Key) return Ada.Containers.Hash_Type;
+
+   package Audit_Field_Maps is
+     new Ada.Containers.Indefinite_Hashed_Maps (Key_Type        => Field_Key,
+                                                Element_Type    => ADO.Identifier,
+                                                Hash            => Hash,
+                                                Equivalent_Keys => "=",
+                                                "="             => ADO."=");
+
+   type Audit_Manager is limited new ADO.Audits.Audit_Manager with record
+      Fields : Audit_Field_Maps.Map;
+   end record;
 
 end AWA.Audits.Services;
