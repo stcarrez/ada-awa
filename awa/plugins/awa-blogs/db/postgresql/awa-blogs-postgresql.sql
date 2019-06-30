a4b13ac74f4e4ef03dd9829b8943fa131cffb77f
Stephane Carrez <Stephane.Carrez@gmail.com>
2019-06-30 18:30:51 +0200

    Rebuild the SQL files

--- awa/plugins/awa-blogs/db/postgresql/awa-blogs-postgresql.sql
+++ awa/plugins/awa-blogs/db/postgresql/awa-blogs-postgresql.sql
@@ -41,10 +41,16 @@ CREATE TABLE awa_post (
   "allow_comments" BOOLEAN NOT NULL,
   /* the number of times the post was read. */
   "read_count" INTEGER NOT NULL,
+  /* the post summary. */
+  "summary" VARCHAR(255) NOT NULL,
+  /* the blog post format. */
+  "format"  NOT NULL,
   /*  */
   "author_id" BIGINT NOT NULL,
   /*  */
   "blog_id" BIGINT NOT NULL,
+  /*  */
+  "image_id" BIGINT ,
   PRIMARY KEY ("id")
 );
 INSERT INTO entity_type (name) VALUES
