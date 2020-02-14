eecdf44fdb079c3fbed6c797dd6eb3dfab889ce8
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-14 23:38:55 +0100

    Update the coverage script

--- awa/coverage.sh
+++ awa/coverage.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 NAME=awa.cov
-lcov --base-directory . --directory . -c --include "*/awa/*" -o $NAME
-lcov --remove $NAME awa/b__awa_harness.adb -o $NAME
-lcov --remove $NAME "/usr*" -o $NAME
-lcov --remove $NAME "*/regtests/*" -o $NAME
-lcov --remove $NAME "*/awaunit/*" -o $NAME
-lcov --remove $NAME "*/ada-util/*" -o $NAME
-lcov --remove $NAME "*/ada-el/*" -o $NAME
-lcov --remove $NAME "*/ada-servlet/*" -o $NAME
-lcov --remove $NAME "*/ada-security/*" -o $NAME
-lcov --remove $NAME "*/ada-asf/*" -o $NAME
-lcov --remove $NAME "*/ada-ado/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c --include "*/awa/*" -o $NAME
+lcov --quiet --remove $NAME awa/b__awa_harness.adb -o $NAME
+lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests/*" -o $NAME
+lcov --quiet --remove $NAME "*/awaunit/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-util/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-el/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-servlet/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-security/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-asf/*" -o $NAME
+lcov --quiet --remove $NAME "*/ada-ado/*" -o $NAME
 rm -rf cover
-genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
+genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
