8f17630ba9dce65bd99e25929048218afe11e019
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-15 14:29:47 +0100

    Revert to avoid using --include and --exclude option since old version of lcov do not support these options

--- awa/coverage.sh
+++ awa/coverage.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 NAME=awa.cov
-lcov --quiet --base-directory . --directory . -c --include "*/awa/*" -o $NAME
-lcov --quiet --remove $NAME awa/b__awa_harness.adb -o $NAME
+lcov --quiet --base-directory . --directory . -c -o $NAME
 lcov --quiet --remove $NAME "/usr*" -o $NAME
+lcov --quiet --remove $NAME "/build*" -o $NAME
+lcov --quiet --remove $NAME "/opt*" -o $NAME
+lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
+lcov --quiet --remove $NAME "*/regtests*" -o $NAME
+lcov --quiet --remove $NAME "*/b__*" -o $NAME
 lcov --quiet --remove $NAME "*/regtests/*" -o $NAME
 lcov --quiet --remove $NAME "*/awaunit/*" -o $NAME
 lcov --quiet --remove $NAME "*/ada-util/*" -o $NAME
@@ -11,6 +15,7 @@ lcov --quiet --remove $NAME "*/ada-servlet/*" -o $NAME
 lcov --quiet --remove $NAME "*/ada-security/*" -o $NAME
 lcov --quiet --remove $NAME "*/ada-asf/*" -o $NAME
 lcov --quiet --remove $NAME "*/ada-ado/*" -o $NAME
+lcov --quiet --remove $NAME "*/openapi-ada/*" -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
