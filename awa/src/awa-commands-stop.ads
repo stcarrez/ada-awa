8294604e0ee7969f4138912268cf57c25e7e7b91
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-22 22:44:21 +0100

    New command to stop the web server

---
+++ awa/src/awa-commands-stop.ads
@@ -0,0 +1,44 @@
+-----------------------------------------------------------------------
+--  awa-commands-stop -- Command to stop the web server
+--  Copyright (C) 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with GNAT.Command_Line;
+with AWA.Commands.Drivers;
+generic
+   with package Command_Drivers is new AWA.Commands.Drivers (<>);
+package AWA.Commands.Stop is
+
+   type Command_Type is new Command_Drivers.Command_Type with record
+      Management_Port   : aliased Integer := 0;
+   end record;
+
+   --  Setup the command before parsing the arguments and executing it.
+   overriding
+   procedure Setup (Command : in out Command_Type;
+                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
+                    Context : in out Context_Type);
+
+   --  Stop the server by sending a 'stop' command on the management socket.
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   Command       : aliased Command_Type;
+
+end AWA.Commands.Stop;
