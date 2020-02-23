a7f0d4fb2b3a23dc79ec0b89ff85ef2263cd54bc
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-23 18:19:57 +0100

    Implement the Test_Start_Stop procedure to test the 'start' and 'stop' commands

---
+++ awa/regtests/awa-commands-tests.adb
@@ -0,0 +1,114 @@
+-----------------------------------------------------------------------
+--  awa-commands-tests -- Test the AWA.Commands
+--  Copyright (C) 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Text_IO;
+with Util.Processes;
+with Util.Streams.Pipes;
+with Util.Streams.Buffered;
+with Util.Test_Caller;
+with Util.Log.Loggers;
+
+package body AWA.Commands.Tests is
+
+   Log : constant Util.Log.Loggers.Logger := Util.Log.Loggers.Create ("AWA.Commands.Tests");
+
+   package Caller is new Util.Test_Caller (Test, "Commands");
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite) is
+   begin
+      Caller.Add_Test (Suite, "Test AWA.Commands.Start_Stop",
+                       Test_Start_Stop'Access);
+   end Add_Tests;
+
+   --  ------------------------------
+   --  Execute the command and get the output in a string.
+   --  ------------------------------
+   procedure Execute (T       : in out Test;
+                      Command : in String;
+                      Input   : in String;
+                      Output  : in String;
+                      Result  : out Ada.Strings.Unbounded.Unbounded_String;
+                      Status  : in Natural := 0) is
+      P        : aliased Util.Streams.Pipes.Pipe_Stream;
+      Buffer   : Util.Streams.Buffered.Input_Buffer_Stream;
+   begin
+      if Input'Length > 0 then
+         Log.Info ("Execute: {0} < {1}", Command, Input);
+      elsif Output'Length > 0 then
+         Log.Info ("Execute: {0} > {1}", Command, Output);
+      else
+         Log.Info ("Execute: {0}", Command);
+      end if;
+      P.Set_Input_Stream (Input);
+      P.Set_Output_Stream (Output);
+      P.Open (Command, Util.Processes.READ_ALL);
+
+      --  Write on the process input stream.
+      Result := Ada.Strings.Unbounded.Null_Unbounded_String;
+      Buffer.Initialize (P'Unchecked_Access, 8192);
+      Buffer.Read (Result);
+      P.Close;
+      Ada.Text_IO.Put_Line (Ada.Strings.Unbounded.To_String (Result));
+      Log.Info ("Command result: {0}", Result);
+      Util.Tests.Assert_Equals (T, Status, P.Get_Exit_Status, "Command '" & Command & "' failed");
+   end Execute;
+
+   --  ------------------------------
+   --  Test start and stop command.
+   --  ------------------------------
+   procedure Test_Start_Stop (T : in out Test) is
+
+      task Start_Server is
+         entry Start;
+         entry Wait (Result : out Ada.Strings.Unbounded.Unbounded_String);
+      end Start_Server;
+
+      task body Start_Server is
+         Output : Ada.Strings.Unbounded.Unbounded_String;
+      begin
+         accept Start do
+            null;
+         end Start;
+         begin
+            T.Execute ("bin/awa_command -c test-sqlite.properties start -m 26123",
+                       "", "", Output, 0);
+         exception
+            when others =>
+               Output := Ada.Strings.Unbounded.To_Unbounded_String ("* exception *");
+         end;
+         accept Wait (Result : out Ada.Strings.Unbounded.Unbounded_String) do
+            Result := Output;
+         end Wait;
+      end Start_Server;
+
+      Result : Ada.Strings.Unbounded.Unbounded_String;
+   begin
+      --  Launch the 'start' command in a separate task because the command will hang.
+      Start_Server.Start;
+      delay 5.0;
+
+      --  Launch the 'stop' command, this should terminate the 'start' command.
+      T.Execute ("bin/awa_command -c test-sqlite.properties stop -m 26123",
+                 "", "", Result, 0);
+
+      --  Wait for the task result.
+      Start_Server.Wait (Result);
+      Util.Tests.Assert_Equals (T, "", Result, "AWA start command output");
+   end Test_Start_Stop;
+
+end AWA.Commands.Tests;
