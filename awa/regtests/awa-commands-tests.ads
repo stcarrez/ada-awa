5b5b7295a481290b2b02282ea6d9016585e2c8d5
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-23 18:08:23 +0100

    New package to test the AWA commands

---
+++ awa/regtests/awa-commands-tests.ads
@@ -0,0 +1,38 @@
+-----------------------------------------------------------------------
+--  awa-commands-tests -- Test the AWA.Commands
+--  Copyright (C) 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Tests;
+with Ada.Strings.Unbounded;
+
+package AWA.Commands.Tests is
+
+   procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
+
+   type Test is new Util.Tests.Test with null record;
+
+   --  Test start and stop command.
+   procedure Test_Start_Stop (T : in out Test);
+
+   procedure Execute (T       : in out Test;
+                      Command : in String;
+                      Input   : in String;
+                      Output  : in String;
+                      Result  : out Ada.Strings.Unbounded.Unbounded_String;
+                      Status  : in Natural := 0);
+
+end AWA.Commands.Tests;
