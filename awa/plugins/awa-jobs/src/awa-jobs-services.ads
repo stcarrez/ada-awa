fd698b855b8326c39d3daf2de3e7e26822e72c6c
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-29 20:55:25 +0100

    Minor update of documentation

--- awa/plugins/awa-jobs/src/awa-jobs-services.ads
+++ awa/plugins/awa-jobs/src/awa-jobs-services.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
---  awa-jobs -- AWA Jobs
---  Copyright (C) 2012, 2014, 2015 Stephane Carrez
+--  awa-jobs-services -- Job services
+--  Copyright (C) 2012, 2014, 2015, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -28,9 +28,9 @@ with AWA.Events;
 with AWA.Jobs.Models;
 
 --  == Job Service ==
---  The <b>AWA.Jobs.Services</b> package defines the type abstractions and the core operation
---  to define a job operation procedure, create and schedule a job and perform the job work
---  when it is scheduled.
+--  The `AWA.Jobs.Services` package defines the type abstractions and the
+--  core operation to define a job operation procedure, create and schedule
+--  a job and perform the job work when it is scheduled.
 --
 --  @type Abstract_Job_Type
 --
@@ -53,45 +53,53 @@ package AWA.Jobs.Services is
    package Job_Create_Event is new AWA.Events.Definition (Name => "job-create");
 
    --  Get the job status.
-   function Get_Job_Status (Id : in ADO.Identifier) return AWA.Jobs.Models.Job_Status_Type;
+   function Get_Job_Status (Id : in ADO.Identifier) return Models.Job_Status_Type;
 
    --  ------------------------------
    --  Abstract_Job Type
    --  ------------------------------
-   --  The <b>Abstract_Job_Type</b> is an abstract tagged record which defines a job that can be
-   --  scheduled and executed.  This is the base type of any job implementation.  It defines
-   --  the <tt>Execute</tt> abstract procedure that must be implemented in concrete job types.
-   --  It provides operation to setup and retrieve the job parameter.  When the job
-   --  <tt>Execute</tt> procedure is called, it allows to set the job execution status and result.
+   --  The `Abstract_Job_Type` is an abstract tagged record which defines
+   --  a job that can be scheduled and executed.  This is the base type of
+   --  any job implementation.  It defines the `Execute` abstract procedure
+   --  that must be implemented in concrete job types.
+   --  It provides operation to setup and retrieve the job parameter.
+   --  When the job `Execute` procedure is called, it allows to set the
+   --  job execution status and result.
    type Abstract_Job_Type is abstract new Util.Refs.Ref_Entity
      and Util.Beans.Basic.Readonly_Bean with private;
    type Abstract_Job_Type_Access is access all Abstract_Job_Type'Class;
 
    type Work_Access is access procedure (Job : in out Abstract_Job_Type'Class);
 
-   --  Execute the job.  This operation must be implemented and should perform the work
-   --  represented by the job.  It should use the <tt>Get_Parameter</tt> function to retrieve
-   --  the job parameter and it can use the <tt>Set_Result</tt> operation to save the result.
+   --  Execute the job.  This operation must be implemented and should
+   --  perform the work represented by the job.  It should use the
+   --  `Get_Parameter` function to retrieve the job parameter and it can
+   --  use the `Set_Result` operation to save the result.
    procedure Execute (Job : in out Abstract_Job_Type) is abstract;
 
-   --  Set the job parameter identified by the <b>Name</b> to the value given in <b>Value</b>.
+   --  Set the job parameter identified by the `Name` to the value
+   --  given in `Value`.
    procedure Set_Parameter (Job   : in out Abstract_Job_Type;
                             Name  : in String;
                             Value : in String);
 
-   --  Set the job parameter identified by the <b>Name</b> to the value given in <b>Value</b>.
+   --  Set the job parameter identified by the `Name` to the value
+   --  given in `Value`.
    procedure Set_Parameter (Job   : in out Abstract_Job_Type;
                             Name  : in String;
                             Value : in Integer);
 
-   --  Set the job parameter identified by the <b>Name</b> to the value given in <b>Value</b>.
+   --  Set the job parameter identified by the `Name` to the value
+   --  given in `Value`.
    procedure Set_Parameter (Job   : in out Abstract_Job_Type;
                             Name  : in String;
                             Value : in ADO.Objects.Object_Ref'Class);
 
-   --  Set the job parameter identified by the <b>Name</b> to the value given in <b>Value</b>.
-   --  The value object can hold any kind of basic value type (integer, enum, date, strings).
-   --  If the value represents a bean, the <tt>Invalid_Value</tt> exception is raised.
+   --  Set the job parameter identified by the `Name` to the value
+   --  given in `Value`.
+   --  The value object can hold any kind of basic value type
+   --  (integer, enum, date, strings).  If the value represents
+   --  a bean, the `Invalid_Value` exception is raised.
    procedure Set_Parameter (Job   : in out Abstract_Job_Type;
                             Name  : in String;
                             Value : in Util.Beans.Objects.Object);
@@ -102,61 +110,67 @@ package AWA.Jobs.Services is
    function Get_Value (Job  : in Abstract_Job_Type;
                        Name : in String) return Util.Beans.Objects.Object;
 
-   --  Get the job parameter identified by the <b>Name</b> and convert the value into a string.
+   --  Get the job parameter identified by the `Name` and convert
+   --  the value into a string.
    function Get_Parameter (Job  : in Abstract_Job_Type;
                            Name : in String) return String;
 
-   --  Get the job parameter identified by the <b>Name</b> and convert the value as an integer.
-   --  If the parameter is not defined, return the default value passed in <b>Default</b>.
+   --  Get the job parameter identified by the `Name` and convert
+   --  the value as an integer.  If the parameter is not defined,
+   --  return the default value passed in `Default`.
    function Get_Parameter (Job     : in Abstract_Job_Type;
                            Name    : in String;
                            Default : in Integer) return Integer;
 
-   --  Get the job parameter identified by the <b>Name</b> and convert the value as a database
-   --  identifier.  If the parameter is not defined, return the <tt>ADO.NO_IDENTIFIER</tt>.
+   --  Get the job parameter identified by the `Name` and convert
+   --  the value as a database identifier.  If the parameter is not defined,
+   --  return the `ADO.NO_IDENTIFIER`.
    function Get_Parameter (Job     : in Abstract_Job_Type;
                            Name    : in String) return ADO.Identifier;
 
-   --  Get the job parameter identified by the <b>Name</b> and return it as a typed object.
+   --  Get the job parameter identified by the `Name` and return it as
+   --  a typed object.
    function Get_Parameter (Job  : in Abstract_Job_Type;
                            Name : in String) return Util.Beans.Objects.Object;
 
    --  Get the job status.
-   function Get_Status (Job : in Abstract_Job_Type) return AWA.Jobs.Models.Job_Status_Type;
+   function Get_Status (Job : in Abstract_Job_Type) return Models.Job_Status_Type;
 
-   --  Get the job identifier once the job was scheduled.  The job identifier allows to
-   --  retrieve the job and check its execution and completion status later on.
+   --  Get the job identifier once the job was scheduled.
+   --  The job identifier allows to retrieve the job and check its
+   --  execution and completion status later on.
    function Get_Identifier (Job : in Abstract_Job_Type) return ADO.Identifier;
 
-   --  Set the job status.
-   --  When the job is terminated, it is closed and the job parameters or results cannot be
-   --  changed.
+   --  Set the job status.  When the job is terminated, it is closed
+   --  and the job parameters or results cannot be changed.
    procedure Set_Status (Job    : in out Abstract_Job_Type;
                          Status : in AWA.Jobs.Models.Job_Status_Type);
 
-   --  Set the job result identified by the <b>Name</b> to the value given in <b>Value</b>.
-   --  The value object can hold any kind of basic value type (integer, enum, date, strings).
-   --  If the value represents a bean, the <tt>Invalid_Value</tt> exception is raised.
+   --  Set the job result identified by the `Name` to the value given
+   --  in `Value`.  The value object can hold any kind of basic value
+   --  type (integer, enum, date, strings).  If the value represents a bean,
+   --  the `Invalid_Value` exception is raised.
    procedure Set_Result (Job   : in out Abstract_Job_Type;
                          Name  : in String;
                          Value : in Util.Beans.Objects.Object);
 
-   --  Set the job result identified by the <b>Name</b> to the value given in <b>Value</b>.
+   --  Set the job result identified by the `Name` to the value given in `Value`.
    procedure Set_Result (Job   : in out Abstract_Job_Type;
                          Name  : in String;
                          Value : in String);
 
-   --  Save the job information in the database.  Use the database session defined by <b>DB</b>
-   --  to save the job.
+   --  Save the job information in the database.  Use the database session
+   --  defined by `DB` to save the job.
    procedure Save (Job : in out Abstract_Job_Type;
                    DB  : in out ADO.Sessions.Master_Session'Class);
 
    --  ------------------------------
    --  Job Factory
    --  ------------------------------
-   --  The <b>Job_Factory</b> is the interface that allows to create a job instance in order
-   --  to execute a scheduled job.  The <tt>Create</tt> function is called to create a new
-   --  job instance when the job is scheduled for execution.
+   --  The `Job_Factory` is the interface that allows to create a job
+   --  instance in order to execute a scheduled job.  The `Create` function
+   --  is called to create a new job instance when the job is scheduled
+   --  for execution.
    type Job_Factory is abstract tagged limited null record;
    type Job_Factory_Access is access all Job_Factory'Class;
 
@@ -173,8 +187,8 @@ package AWA.Jobs.Services is
    --  ------------------------------
    --  Job Type
    --  ------------------------------
-   --  The <tt>Job_Type</tt> is a concrete job used by the <tt>Work_Factory</tt> to execute
-   --  a simple <tt>Work_Access</tt> procedure.
+   --  The `Job_Type` is a concrete job used by the `Work_Factory` to execute
+   --  a simple `Work_Access` procedure.
    type Job_Type is new Abstract_Job_Type with private;
 
    overriding
@@ -184,18 +198,18 @@ package AWA.Jobs.Services is
    --  ------------------------------
    --  Work Factory
    --  ------------------------------
-   --  The <tt>Work_Factory</tt> is a simplified <tt>Job_Factory</tt> that allows to register
-   --  simple <tt>Work_Access</tt> procedures to execute the job.
+   --  The `Work_Factory` is a simplified `Job_Factory` that allows to register
+   --  simple `Work_Access` procedures to execute the job.
    type Work_Factory (Work : Work_Access) is new Job_Factory with null record;
 
-   --  Create the job instance to execute the associated <tt>Work_Access</tt> procedure.
+   --  Create the job instance to execute the associated `Work_Access` procedure.
    overriding
    function Create (Factory : in Work_Factory) return Abstract_Job_Type_Access;
 
    --  ------------------------------
    --  Job Declaration
    --  ------------------------------
-   --  The <tt>Definition</tt> package must be instantiated with a given job type to
+   --  The `Definition` package must be instantiated with a given job type to
    --  register the new job definition.
    generic
       type T is new Abstract_Job_Type with private;
@@ -227,8 +241,9 @@ package AWA.Jobs.Services is
 
    type Job_Ref is private;
 
-   --  Get the job parameter identified by the <b>Name</b> and return it as a typed object.
-   --  Return the Null_Object if the job is empty or there is no such parameter.
+   --  Get the job parameter identified by the `Name` and return it as
+   --  a typed object.  Return the `Null_Object` if the job is empty
+   --  or there is no such parameter.
    function Get_Parameter (Job  : in Job_Ref;
                            Name : in String) return Util.Beans.Objects.Object;
 
@@ -254,8 +269,8 @@ private
    --  ------------------------------
    --  Job Type
    --  ------------------------------
-   --  The <tt>Job_Type</tt> is a concrete job used by the <tt>Work_Factory</tt> to execute
-   --  a simple <tt>Work_Access</tt> procedure.
+   --  The `Job_Type` is a concrete job used by the `Work_Factory` to execute
+   --  a simple `Work_Access` procedure.
    type Job_Type is new Abstract_Job_Type with record
       Work : Work_Access;
    end record;
@@ -265,6 +280,5 @@ private
                                           Element_Access => Abstract_Job_Type_Access);
 
    type Job_Ref is new Job_Refs.Ref with null record;
-   --  subtype Job_Ref is Job_Refs.Ref;
 
 end AWA.Jobs.Services;
