91cae67c717b1bd5b986e1f07d839e3dbe80ec53
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-29 18:19:18 +0100

    Add Add_Listener to allow registration of a listener to be notified when the queue is updated

--- awa/src/awa-events-queues.ads
+++ awa/src/awa-events-queues.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-events-queues -- AWA Event Queues
---  Copyright (C) 2012, 2019 Stephane Carrez
+--  Copyright (C) 2012, 2019, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -16,6 +16,7 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
+with Util.Listeners;
 private with Util.Refs;
 
 with EL.Beans;
@@ -23,6 +24,9 @@ with EL.Contexts;
 
 with AWA.Events.Models;
 package AWA.Events.Queues is
+
+   FIFO_QUEUE_TYPE       : constant String := "fifo";
+   PERSISTENT_QUEUE_TYPE : constant String := "persist";
 
    type Queue_Ref is tagged private;
 
@@ -34,6 +38,10 @@ package AWA.Events.Queues is
    procedure Dequeue (From    : in Queue_Ref;
                       Process : access procedure (Event : in Module_Event'Class));
 
+   --  Add a listener that will be called each time an event is queued.
+   procedure Add_Listener (Into     : in Queue_Ref;
+                           Listener : in Util.Listeners.Listener_Access);
+
    --  Returns true if the queue is available.
    function Has_Queue (Queue : in Queue_Ref'Class) return Boolean;
 
@@ -42,10 +50,10 @@ package AWA.Events.Queues is
 
    --  Get the model queue reference object.
    --  Returns a null object if the queue is not persistent.
-   function Get_Queue (Queue : in Queue_Ref'Class) return AWA.Events.Models.Queue_Ref;
+   function Get_Queue (Queue : in Queue_Ref'Class) return Events.Models.Queue_Ref;
 
-   --  Create the event queue identified by the name <b>Name</b>.  The queue factory
-   --  identified by <b>Kind</b> is called to create the event queue instance.
+   --  Create the event queue identified by the name `Name`.  The queue factory
+   --  identified by `Kind` is called to create the event queue instance.
    --  Returns a reference to the queue.
    function Create_Queue (Name    : in String;
                           Kind    : in String;
@@ -65,13 +73,13 @@ private
 
    --  Get the model queue reference object.
    --  Returns a null object if the queue is not persistent.
-   function Get_Queue (From : in Queue) return AWA.Events.Models.Queue_Ref is abstract;
+   function Get_Queue (From : in Queue) return Events.Models.Queue_Ref is abstract;
 
    --  Queue the event.
    procedure Enqueue (Into  : in out Queue;
                       Event : in AWA.Events.Module_Event'Class) is abstract;
 
-   --  Dequeue an event and process it with the <b>Process</b> procedure.
+   --  Dequeue an event and process it with the `Process` procedure.
    procedure Dequeue (From    : in out Queue;
                       Process : access procedure (Event : in Module_Event'Class)) is abstract;
 
@@ -79,8 +87,9 @@ private
    procedure Finalize (From : in out Queue) is null;
 
    type Queue_Info (Length : Natural) is new Util.Refs.Ref_Entity with record
-      Queue : Queue_Access := null;
-      Name  : String (1 .. Length);
+      Queue     : Queue_Access := null;
+      Listeners : Util.Listeners.List;
+      Name      : String (1 .. Length);
    end record;
    type Queue_Info_Access is access all Queue_Info;
 
