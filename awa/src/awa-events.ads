5f20993169c44547f4146a96a4cabbfd0aa08ace
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-29 15:10:10 +0100

    Add Set_Entity and Set_Parameters operations

--- awa/src/awa-events.ads
+++ awa/src/awa-events.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-events -- AWA Events
---  Copyright (C) 2012, 2015, 2018 Stephane Carrez
+--  Copyright (C) 2012, 2015, 2018, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,29 +20,33 @@ with Util.Events;
 with Util.Beans.Objects;
 with Util.Beans.Basic;
 with Util.Beans.Objects.Maps;
+with Util.Properties;
 
-with ADO;
+with ADO.Objects;
+with ADO.Sessions;
 
 with AWA.Index_Arrays;
 
 --  == AWA Events ==
---  The <b>AWA.Events</b> package defines an event framework for modules to post events
---  and have Ada bean methods be invoked when these events are dispatched.  Subscription to
---  events is done through configuration files.  This allows to configure the modules and
---  integrate them together easily at configuration time.
+--  The `AWA.Events` package defines an event framework for modules to post
+--  events and have Ada bean methods be invoked when these events are dispatched.
+--  Subscription to events is done through configuration files.  This allows to
+--  configure the modules and integrate them together easily at configuration time.
 --
 --  === Declaration ===
---  Modules define the events that they can generate by instantiating the <b>Definition</b>
---  package. This is a static definition of the event.  Each event is given a unique name.
+--  Modules define the events that they can generate by instantiating the
+--  `Definition` package. This is a static definition of the event.  Each event
+--  is given a unique name.
 --
 --    with AWA.Events.Definition;
 --    ...
 --    package Event_New_User is new AWA.Events.Definition ("new-user");
 --
 --  === Posting an event ===
---  The module can post an event to inform other modules or the system that a particular
---  action occurred.  The module creates the event instance of type <b>Module_Event</b> and
---  populates that event with useful properties for event receivers.
+--  The module can post an event to inform other modules or the system that
+--  a particular action occurred.  The module creates the event instance of
+--  type `Module_Event` and populates that event with useful properties for
+--  event receivers.
 --
 --    with AWA.Events;
 --    ...
@@ -56,42 +60,48 @@ with AWA.Index_Arrays;
 --    Manager.Send_Event (Event);
 --
 --  === Receiving an event ===
---  Modules or applications interested by a particular event will configure the event manager
---  to dispatch the event to an Ada bean event action.  The Ada bean is an object that must
---  implement a procedure that matches the prototype:
+--  Modules or applications interested by a particular event will configure
+--  the event manager to dispatch the event to an Ada bean event action.
+--  The Ada bean is an object that must implement a procedure that matches
+--  the prototype:
 --
 --    type Action_Bean is new Util.Beans.Basic.Readonly_Bean ...;
---    procedure Action (Bean : in out Action_Bean; Event : in AWA.Events.Module_Event'Class);
+--    procedure Action (Bean : in out Action_Bean;
+--                      Event : in AWA.Events.Module_Event'Class);
 --
 --  The Ada bean method and object are registered as other Ada beans.
 --
---  The configuration file indicates how to bind the Ada bean action and the event together.
---  The action is specified using an EL Method Expression (See Ada EL or JSR 245).
+--  The configuration file indicates how to bind the Ada bean action and
+--  the event together.  The action is specified using an EL Method Expression
+--  (See Ada EL or JSR 245).
 --
 --    <on-event name="new_user">
 --        <action>#{ada_bean.action}</action>
 --    </on-event>
 --
 --  === Event queues and dispatchers ===
---  The *AWA.Events* framework posts events on queues and it uses a dispatcher to process them.
---  There are two kinds of dispatchers:
+--  The `AWA.Events` framework posts events on queues and it uses a dispatcher
+--  to process them.  There are two kinds of dispatchers:
 --
---    * Synchronous dispatcher process the event when it is posted.  The task which posts
---      the event invokes the Ada bean action.  In this dispatching mode, there is no event queue.
+--    * Synchronous dispatcher process the event when it is posted.
+--      The task which posts the event invokes the Ada bean action.
+--      In this dispatching mode, there is no event queue.
 --      If the action method raises an exception, it will however be blocked.
 --
---    * Asynchronous dispatcher are executed by dedicated tasks.  The event is put in an event
---      queue.  A dispatcher task processes the event and invokes the action method at a later
---      time.
+--    * Asynchronous dispatcher are executed by dedicated tasks.  The event
+--      is put in an event queue.  A dispatcher task processes the event and
+--      invokes the action method at a later time.
 --
 --  When the event is queued, there are two types of event queues:
 --
 --    * A Fifo memory queue manages the event and dispatches them in FIFO order.
---      If the application is stopped, the events present in the Fifo queue are lost.
+--      If the application is stopped, the events present in the Fifo queue
+--      are lost.
 --
---    * A persistent event queue manages the event in a similar way as the FIFO queue but
---      saves them in the database.  If the application is stopped, events that have not yet
---      been processed will be dispatched when the application is started again.
+--    * A persistent event queue manages the event in a similar way as the
+--      FIFO queue but saves them in the database.  If the application is
+--      stopped, events that have not yet been processed will be dispatched
+--      when the application is started again.
 --
 --  === Data Model ===
 --  [images/awa_events_model.png]
@@ -114,12 +124,14 @@ package AWA.Events is
 
    --  Find the event runtime index given the event name.
    --  Raises Not_Found exception if the event name is not recognized.
-   function Find_Event_Index (Name : in String) return Event_Index renames Event_Arrays.Find;
+   function Find_Event_Index (Name : in String)
+                              return Event_Index renames Event_Arrays.Find;
 
    --  ------------------------------
    --  Module event
    --  ------------------------------
-   type Module_Event is new Util.Events.Event and Util.Beans.Basic.Readonly_Bean with private;
+   type Module_Event is new Util.Events.Event
+     and Util.Beans.Basic.Readonly_Bean with private;
    type Module_Event_Access is access all Module_Event'Class;
 
    --  Set the event type which identifies the event.
@@ -138,6 +150,10 @@ package AWA.Events is
    function Get_Parameter (Event : in Module_Event;
                            Name  : in String) return String;
 
+   --  Set the parameters of the message.
+   procedure Set_Parameters (Event      : in out Module_Event;
+                             Parameters : in Util.Beans.Objects.Maps.Map);
+
    --  Get the value that corresponds to the parameter with the given name.
    overriding
    function Get_Value (Event : in Module_Event;
@@ -150,14 +166,19 @@ package AWA.Events is
    procedure Set_Entity_Identifier (Event : in out Module_Event;
                                     Id    : in ADO.Identifier);
 
+   --  Set the database entity associated with the event.
+   procedure Set_Entity (Event   : in out Module_Event;
+                         Entity  : in ADO.Objects.Object_Ref'Class;
+                         Session : in ADO.Sessions.Session'Class);
 
-   --  Copy the event properties to the map passed in <tt>Into</tt>.
+   --  Copy the event properties to the map passed in `Into`.
    procedure Copy (Event : in Module_Event;
                    Into  : in out Util.Beans.Objects.Maps.Map);
 
 private
 
-   type Module_Event is new Util.Events.Event and Util.Beans.Basic.Readonly_Bean with record
+   type Module_Event is new Util.Events.Event
+     and Util.Beans.Basic.Readonly_Bean with record
       Kind        : Event_Index := Event_Arrays.Invalid_Index;
       Props       : Util.Beans.Objects.Maps.Map;
       Entity      : ADO.Identifier  := ADO.NO_IDENTIFIER;
