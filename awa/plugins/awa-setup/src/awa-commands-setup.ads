d083e57f92d5d6706baa1d2509ace51e78d6f302
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-01 09:43:59 +0100

    New command to run the web server and setup the application

---
+++ awa/plugins/awa-setup/src/awa-commands-setup.ads
@@ -0,0 +1,43 @@
+-----------------------------------------------------------------------
+--  awa-commands-setup -- Setup command to start and configure the application
+--  Copyright (C) 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with GNAT.Command_Line;
+with AWA.Commands.Start;
+generic
+   with package Start_Command is new AWA.Commands.Start (<>);
+package AWA.Commands.Setup is
+
+   type Command_Type is new Start_Command.Command_Type with record
+      List_Sessions : aliased Boolean := False;
+   end record;
+
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type);
+
+   --  Setup the command before parsing the arguments and executing it.
+   overriding
+   procedure Setup (Command : in out Command_Type;
+                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
+                    Context : in out Context_Type);
+
+   Command       : aliased Command_Type;
+
+end AWA.Commands.Setup;
