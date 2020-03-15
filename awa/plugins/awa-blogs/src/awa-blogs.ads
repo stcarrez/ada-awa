9a970fbfba561c97950394853769e6287ade90ad
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-15 21:37:12 +0100

    Update the documentation

--- awa/plugins/awa-blogs/src/awa-blogs.ads
+++ awa/plugins/awa-blogs/src/awa-blogs.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-blogs -- Blogs module
---  Copyright (C) 2011, 2014, 2015, 2018 Stephane Carrez
+--  Copyright (C) 2011, 2014, 2015, 2018, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -17,16 +17,20 @@
 -----------------------------------------------------------------------
 
 --  = Blogs Module =
---  The `blogs` module is a small blog application which allows users to publish articles.
---  A user may own several blogs, each blog having a name and its own base URI.  Within a blog,
---  the user may write articles and publish them.  Once published, the articles are visible to
+--  The `blogs` module is a small blog application which allows users
+--  to publish articles.  A user may own several blogs, each blog having
+--  a name and its own base URI.  Within a blog, the user may write articles
+--  and publish them.  Once published, the articles are visible to
 --  anonymous users.
 --
---  The `blogs` module uses the [AWA_Tags] and [AWA_Comments] modules to allow to associate
---  tags to a post and allow users to comment on the articles.
+--  The `blogs` module uses several other modules:
+--
+--  * the [Counters Module] to track page display counter to a blog post,
+--  * the [Tags Module] to associate one or several tags to a blog post,
+--  * the [Comments Module] to allow users to write comments on a blog post,
+--  * the [Images Module] to easily add images in blog post.
 --
 --  @include awa-blogs-modules.ads
---
 --  @include awa-blogs-beans.ads
 --  @include-bean blogs.xml
 --  @include-bean blog-admin-post-list.xml
