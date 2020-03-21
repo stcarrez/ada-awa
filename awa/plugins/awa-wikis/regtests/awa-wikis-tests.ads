26ee6b1dd70478aa201c5a31437fa509c3607453
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-21 21:42:25 +0100

    Add Test_Page_With_Image unit test

--- awa/plugins/awa-wikis/regtests/awa-wikis-tests.ads
+++ awa/plugins/awa-wikis/regtests/awa-wikis-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-wikis-tests -- Unit tests for wikis module
---  Copyright (C) 2018 Stephane Carrez
+--  Copyright (C) 2018, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +18,8 @@
 
 with Util.Tests;
 with AWA.Tests;
+with Servlet.requests.Mockup;
+with Servlet.Responses.Mockup;
 with Ada.Strings.Unbounded;
 
 package AWA.Wikis.Tests is
@@ -25,9 +27,20 @@ package AWA.Wikis.Tests is
    procedure Add_Tests (Suite : in Util.Tests.Access_Test_Suite);
 
    type Test is new AWA.Tests.Test with record
-      Wiki_Ident : Ada.Strings.Unbounded.Unbounded_String;
-      Page_Ident : Ada.Strings.Unbounded.Unbounded_String;
+      Wiki_Ident  : Ada.Strings.Unbounded.Unbounded_String;
+      Page_Ident  : Ada.Strings.Unbounded.Unbounded_String;
+      Image_Ident : Ada.Strings.Unbounded.Unbounded_String;
    end record;
+
+   --  Setup an image for the wiki page.
+   procedure Make_Wiki_Image (T : in out Test);
+
+   --  Create a wiki page.
+   procedure Create_Page (T       : in out Test;
+                          Request : in out Servlet.Requests.Mockup.Request;
+                          Reply   : in out Servlet.Responses.Mockup.Response;
+                          Name    : in String;
+                          Title   : in String);
 
    --  Get some access on the wiki page as anonymous users.
    procedure Verify_Anonymous (T     : in out Test;
@@ -46,5 +59,8 @@ package AWA.Wikis.Tests is
 
    --  Test getting a wiki page which does not exist.
    procedure Test_Missing_Page (T : in out Test);
+
+   --  Test creation of wiki page with an image.
+   procedure Test_Page_With_Image (T : in out Test);
 
 end AWA.Wikis.Tests;
