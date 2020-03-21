fc06966f8496e4b0cb8c411b70304f195321c0dd
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-21 14:38:14 +0100

    Add Test_Get_Sizes procedure

--- awa/plugins/awa-images/regtests/awa-images-modules-tests.ads
+++ awa/plugins/awa-images/regtests/awa-images-modules-tests.ads
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-images-modules-tests -- Unit tests for image service
---  Copyright (C) 2012, 2013, 2018 Stephane Carrez
+--  Copyright (C) 2012, 2013, 2018, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,5 +30,8 @@ package AWA.Images.Modules.Tests is
 
    --  Test creation of a storage object
    procedure Test_Create_Image (T : in out Test);
+
+   --  Test the Get_Sizes operation.
+   procedure Test_Get_Sizes (T : in out TesT);
 
 end AWA.Images.Modules.Tests;
