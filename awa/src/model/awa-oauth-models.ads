108b18d3f3e0b4327378c047f3602cd714c63c65
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-03-21 17:55:56 +0100

    Rebuild the model files

--- awa/src/model/awa-oauth-models.ads
+++ awa/src/model/awa-oauth-models.ads
@@ -5,7 +5,7 @@
 --  Template used: templates/model/package-spec.xhtml
 --  Ada Generator: https://ada-gen.googlecode.com/svn/trunk Revision 1095
 -----------------------------------------------------------------------
---  Copyright (C) 2019 Stephane Carrez
+--  Copyright (C) 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
