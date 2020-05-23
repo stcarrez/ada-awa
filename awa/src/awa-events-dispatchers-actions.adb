9fa9beae9284951ad0f09f8cdc9783f01017b32b
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-05-23 15:19:12 +0200

    Fix compilation with GNAT 2020

--- awa/src/awa-events-dispatchers-actions.adb
+++ awa/src/awa-events-dispatchers-actions.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  awa-events-dispatchers-actions -- Event dispatcher to Ada bean actions
---  Copyright (C) 2012, 2015, 2017, 2018 Stephane Carrez
+--  Copyright (C) 2012, 2015, 2017, 2018, 2020 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -180,13 +180,15 @@ package body AWA.Events.Dispatchers.Actions is
          use Ada.Exceptions;
 
          Method : EL.Expressions.Method_Info;
+         Bean   : access Util.Beans.Basic.Readonly_Bean'Class;
       begin
          Method := Action.Action.Get_Method_Info (Context => ELContext);
 
-         if Method.Object.all in Util.Beans.Basic.Bean'Class then
+         Bean := Util.Beans.Objects.To_Bean (Method.Object);
+         if Bean /= null and then Bean.all in Util.Beans.Basic.Bean'Class then
             --  If we have a prepare method and the bean provides a Set_Value method,
             --  call the preparation method to fill the bean with some values.
-            EL.Beans.Initialize (Util.Beans.Basic.Bean'Class (Method.Object.all),
+            EL.Beans.Initialize (Util.Beans.Basic.Bean'Class (Bean.all),
                                  Action.Properties,
                                  ELContext);
          end if;
