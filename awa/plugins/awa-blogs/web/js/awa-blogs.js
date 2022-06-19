213fc80d06339aa1b3f032bdef8a9f3c33bf0ebb
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-06-19 20:43:57 +0200

    Update to use the Easy Markdown Editor when the post is using Markdown

--- awa/plugins/awa-blogs/web/js/awa-blogs.js
+++ awa/plugins/awa-blogs/web/js/awa-blogs.js
@@ -122,7 +122,7 @@
             if (self.options.syntax == 'FORMAT_MARKDOWN') {
                 syntax = 'markdown';
             }
-            if (syntax == 'dotclear' || syntax == 'mediawiki' || syntax == 'markdown') {
+            if (syntax == 'dotclear' || syntax == 'mediawiki') {
 
             // Create a MarkEdit editor on page load
                 this.element.find('textarea').markedit({
@@ -150,12 +150,66 @@
                         ]}).imageSelector("openSelector");
                     }
                 },
+                }).each(function () {
+                    this.setAttribute("style", "height:" + (this.scrollHeight) + "px;overflow-y:hidden;");
+                }).on("input", function () {
+                    this.style.height = "auto";
+                    this.style.height = (this.scrollHeight) + "px";
+                });
+            } else if (syntax == 'markdown') {
+
+                // Create a MarkEdit editor on page load
+                this.element.find('textarea').each(function() {
+                    const easyMDE = new EasyMDE({
+                        element: this,
+                        autoDownloadFontAwesome: false,
+                        spellChecker: false,
+                        indentWithTags: false,
+                        autofocus: true,
+                        tabSize: 4,
+                        lineNumbers: true,
+                        renderingConfig: {
+                            singleLineBreaks: false
+                        },
+                        toolbar: [
+                            'bold', 'italic', 'strikethrough', '|',
+                            'heading-1', 'heading-2', 'heading-3', '|',
+                            'code', 'quote', '|',
+                            'unordered-list', 'ordered-list', '|',
+                            'link', {
+                                name: 'image',
+                                action(e) {
+                                    const cm = e.codemirror;
+                                    $('#image-selector').imageSelector({
+                                        autoOpen: false,
+                                        show: "blind",
+                                        hide: "explode",
+                                        minWidth: 900,
+                                        minHeight: 400,
+                                        modal: true,
+                                        buttons: [
+                                            { "text": "Select",
+                                              "click": function() {
+                                                  var value = $('#image-selector').imageSelector("value");
+                                                  cm.replaceSelection('![' + value + '](' + value + ')');
+                                                  cm.focus();
+                                                  $(this).imageSelector("close");
+                                              }
+                                            }
+                                        ]}).imageSelector("openSelector");
+                                },
+                                className: 'fa fa-image',
+                                title: 'Add image'
+                            },
+                            'table', 'horizontal-rule', '|', 'clean-block'
+                        ]
+                    });
                 });
             } else {
                 this.element.find('textarea').trumbowyg({
                     minimalLinks: true,
                     imageWidthModalEdit: true,
-                    autogrow: false,
+                    autogrow: true,
                     resetCss: false,
                     tagsToRemove: ['script', 'link'],
                     btns: [['viewHTML'], ['undo', 'redo'],
