63a1a8bb7307b7f3f47aa270802f9b290c6888c8
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-08-21 10:41:21 +0200

    Feature #23: add new command migrate for the database migration

---
+++ awa/src/awa-commands-migrate.adb
@@ -0,0 +1,106 @@
+-----------------------------------------------------------------------
+--  awa-commands-migrate -- Database schema migration command
+--  Copyright (C) 2022 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Util.Strings.Vectors;
+with ADO.Sessions;
+with ADO.Schemas.Databases;
+package body AWA.Commands.Migrate is
+
+   --  ------------------------------
+   --  Add, disable, enable a user.
+   --  ------------------------------
+   overriding
+   procedure Execute (Command   : in out Command_Type;
+                      Name      : in String;
+                      Args      : in Argument_List'Class;
+                      Context   : in out Context_Type) is
+   begin
+      Command_Drivers.Application_Command_Type (Command).Execute (Name, Args, Context);
+   end Execute;
+
+   overriding
+   procedure Execute (Command     : in out Command_Type;
+                      Application : in out AWA.Applications.Application'Class;
+                      Args        : in Argument_List'Class;
+                      Context     : in out Context_Type) is
+      Session : ADO.Sessions.Master_Session;
+      List    : ADO.Schemas.Databases.Upgrade_List;
+      Files   : Util.Strings.Vectors.Vector;
+   begin
+      Application.Load_Bundle (Name   => "commands",
+                               Locale => "en",
+                               Bundle => Command.Bundle);
+      Session := Application.Get_Master_Session;
+
+      ADO.Schemas.Databases.Scan_Migration (Session, "db/migrate", List);
+      if List.Is_Empty then
+         Context.Console.Notice (N_INFO, "Database schema is up to date");
+         return;
+      end if;
+
+      ADO.Schemas.Databases.Sort_Migration (List);
+      if Command.Execute then
+         for Upgrade of List loop
+            ADO.Schemas.Databases.Run_Migration
+              (Session, Upgrade, Files, ADO.Sessions.Execute'Access);
+         end loop;
+      else
+         for Upgrade of List loop
+            ADO.Schemas.Databases.Prepare_Migration
+              (Session, Upgrade, Files);
+            for Path of Files loop
+               Context.Console.Notice (N_INFO, "Run SQL script " & Path);
+            end loop;
+         end loop;
+      end if;
+   end Execute;
+
+   --  ------------------------------
+   --  Setup the command before parsing the arguments and executing it.
+   --  ------------------------------
+   overriding
+   procedure Setup (Command : in out Command_Type;
+                    Config  : in out GNAT.Command_Line.Command_Line_Configuration;
+                    Context : in out Context_Type) is
+   begin
+      GC.Set_Usage (Config => Config,
+                    Usage  => Command.Get_Name & " [arguments]",
+                    Help   => Command.Get_Description);
+      Command_Drivers.Application_Command_Type (Command).Setup (Config, Context);
+      GC.Define_Switch (Config => Config,
+                        Output => Command.Execute'Access,
+                        Long_Switch => "--execute",
+                        Help   => -("Execute the SQL to upgrade the database"));
+   end Setup;
+
+   --  ------------------------------
+   --  Write the help associated with the command.
+   --  ------------------------------
+   overriding
+   procedure Help (Command   : in out Command_Type;
+                   Name      : in String;
+                   Context   : in out Context_Type) is
+      pragma Unreferenced (Command, Context);
+   begin
+      null;
+   end Help;
+
+begin
+   Command_Drivers.Driver.Add_Command ("migrate",
+                                       -("database schema upgrade"),
+                                       Command'Access);
+end AWA.Commands.Migrate;
