c8f6fb01509d48a0a3c08e8d9393c8c331d471d6
Stephane Carrez <Stephane.Carrez@gmail.com>
2022-08-21 20:15:30 +0200

    Feature #25: add the SQL schema for the unit tests

---
+++ awa/db/regtests/mysql/drop-regtests-mysql.sql
@@ -0,0 +1,76 @@
+/* Copied from awa-wikis-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_wiki_space`;
+DROP TABLE IF EXISTS `awa_wiki_page`;
+DROP TABLE IF EXISTS `awa_wiki_content`;
+/* Copied from awa-changelogs-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_changelog`;
+/* Copied from awa-settings-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_user_setting`;
+DROP TABLE IF EXISTS `awa_setting`;
+DROP TABLE IF EXISTS `awa_global_setting`;
+/* Copied from awa-questions-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_question`;
+DROP TABLE IF EXISTS `awa_answer`;
+/* Copied from awa-votes-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_vote`;
+DROP TABLE IF EXISTS `awa_rating`;
+/* Copied from awa-blogs-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_post`;
+DROP TABLE IF EXISTS `awa_blog`;
+/* Copied from awa-comments-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_comment`;
+/* Copied from awa-tags-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_tagged_entity`;
+DROP TABLE IF EXISTS `awa_tag`;
+/* Copied from awa-images-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_image`;
+/* Copied from awa-jobs-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_job`;
+/* Copied from awa-storages-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_store_local`;
+DROP TABLE IF EXISTS `awa_storage_folder`;
+DROP TABLE IF EXISTS `awa_storage_data`;
+DROP TABLE IF EXISTS `awa_storage`;
+/* Copied from awa-counters-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_visit`;
+DROP TABLE IF EXISTS `awa_counter_definition`;
+DROP TABLE IF EXISTS `awa_counter`;
+/* Copied from awa-workspaces-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_workspace_member`;
+DROP TABLE IF EXISTS `awa_workspace_feature`;
+DROP TABLE IF EXISTS `awa_workspace`;
+DROP TABLE IF EXISTS `awa_invitation`;
+/* Copied from awa-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `awa_user`;
+DROP TABLE IF EXISTS `awa_session`;
+DROP TABLE IF EXISTS `awa_email`;
+DROP TABLE IF EXISTS `awa_access_key`;
+DROP TABLE IF EXISTS `awa_permission`;
+DROP TABLE IF EXISTS `awa_acl`;
+DROP TABLE IF EXISTS `awa_oauth_session`;
+DROP TABLE IF EXISTS `awa_callback`;
+DROP TABLE IF EXISTS `awa_application`;
+DROP TABLE IF EXISTS `awa_queue`;
+DROP TABLE IF EXISTS `awa_message_type`;
+DROP TABLE IF EXISTS `awa_message`;
+DROP TABLE IF EXISTS `awa_audit_field`;
+DROP TABLE IF EXISTS `awa_audit`;
+/* Copied from ado-drop-mysql.sql*/
+/* File generated automatically by dynamo */
+DROP TABLE IF EXISTS `ado_version`;
+DROP TABLE IF EXISTS `ado_sequence`;
+DROP TABLE IF EXISTS `ado_entity_type`;
