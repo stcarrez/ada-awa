97eac7999e9eae25c1f85107c3ba711fbc5d56f7
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-08-14 08:59:20 +0200

    Fix alire setup to check if convert or magick commands are installed

--- alire-setup.sh
+++ alire-setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-sed -e 's,@USE_CONVERT@,,' \
-    -e 's,@USE_MAGICK@,-unused,' \
+CONVERT=`which convert`
+USE_CONVERT="-unused"
+USE_MAGICK="-unused"
+if test x"$CONVERT" != x; then
+  USE_CONVERT=""
+else
+  MAGICK=`which magick`
+  if test x"$MAGICK" != x; then
+    USE_MAGICK=""
+  fi
+fi
+sed -e "s,@USE_CONVERT@,$USE_CONVERT,g" \
+    -e "s,@USE_MAGICK@,$USE_MAGICK,g" \
     awa/plugins/awa-images/config/images.xml.in > awa/plugins/awa-images/config/images.xml
