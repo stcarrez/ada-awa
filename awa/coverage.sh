79f7facd1e4cc099a846f69d08ecb429a940087d
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-08-22 23:23:29 +0200

    Update coverage to run lcov from alr to get the gcov tool

--- awa/coverage.sh
+++ awa/coverage.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 NAME=awa.cov
-lcov --quiet --base-directory . --directory . -c -o $NAME
-lcov --quiet --remove $NAME "/usr*" -o $NAME
-lcov --quiet --remove $NAME "/build*" -o $NAME
-lcov --quiet --remove $NAME "/opt*" -o $NAME
-lcov --quiet --remove $NAME "*/adainclude*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests*" -o $NAME
-lcov --quiet --remove $NAME "*/b__*" -o $NAME
-lcov --quiet --remove $NAME "*/regtests/*" -o $NAME
-lcov --quiet --remove $NAME "*/awaunit/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-util/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-el/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-servlet/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-security/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-asf/*" -o $NAME
-lcov --quiet --remove $NAME "*/ada-ado/*" -o $NAME
-lcov --quiet --remove $NAME "*/openapi-ada/*" -o $NAME
-# Ignore generated code
-lcov --quiet --remove $NAME "*/model/*" -o $NAME
+alr exec -- lcov --quiet --base-directory . --directory . \
+   --no-external \
+   --exclude '*/b__*.adb' \
+   --exclude '*/samples/*' \
+   --exclude '*/awaunit/*' \
+   --exclude '*/ada-util/*' \
+   --exclude '*/ada-el/*' \
+   --exclude '*/ada-servlet/*' \
+   --exclude '*/ada-security/*' \
+   --exclude '*/ada-wiki/*' \
+   --exclude '*/ada-asf/*' \
+   --exclude '*/ada-keystore/*' \
+   --exclude '*/openapi-ada/*' \
+   --exclude '*/ada-ado/*' \
+   --exclude '*/model/*' \
+   --exclude '*/regtests*'  -c -o $NAME
 rm -rf cover
 genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
