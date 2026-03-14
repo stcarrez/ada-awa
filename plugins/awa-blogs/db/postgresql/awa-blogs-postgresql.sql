0c15b0205273b7e1b7823b496708ff3f42a28a35
Stephane Carrez <Stephane.Carrez@gmail.com>
2026-03-14 21:21:01 +0100

    Rebuild SQL files

--- plugins/awa-blogs/db/postgresql/awa-blogs-postgresql.sql
+++ plugins/awa-blogs/db/postgresql/awa-blogs-postgresql.sql
@@ -1,4 +1,5 @@
 /* File generated automatically by dynamo */
+SET client_min_messages = warning;
 /*  */
 CREATE TABLE IF NOT EXISTS awa_blog (
   /* the blog identifier */
@@ -97,5 +98,5 @@ INSERT INTO awa_audit_field (entity_type, name)
   VALUES ((SELECT id FROM ado_entity_type WHERE name = 'awa_post'), 'format')
   ON CONFLICT DO NOTHING;
 INSERT INTO ado_version (name, version)
-  VALUES ("awa-blogs", 1)
+  VALUES ('awa-blogs', 1)
   ON CONFLICT DO NOTHING;
